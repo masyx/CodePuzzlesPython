4f8bef72d0bc0e239c72e27d0e3c56018ed98c02
Sergey Matveev <rfmatveev@gmail.com>
2021-10-04 23:04:54 -0500

    cash cleanup

--- Cash/cash.py
+++ Cash/cash.py
@@ -5,18 +5,16 @@ while True:
         if change < 0: raise ValueError
         break;
     except ValueError:
-        print("This is not a number, pleae enter valid number") 
+        print("This is not a number, pleae enter valid number")
 
 coins = [25, 10, 5, 1]
 
-totalCoinsCount = 0
+coinsCount = 0
 
 for coin in coins:
-    coinsCount = 0
-    coinsCount = int(change / coin)
-    change = change % coin
-    totalCoinsCount += coinsCount
+    coinsCount += int(change / coin)
+    change %= coin
     if change == 0:
         break;
 
-print(f"Total amount of coins is: {totalCoinsCount}")
+print(coinsCount)
