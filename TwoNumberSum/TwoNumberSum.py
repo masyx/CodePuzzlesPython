e9da298ae1a846229c522fca53c48f7100c61cb4
Sergey Matveev <rfmatveev@gmail.com>
2022-06-12 16:53:28 -0500

    Two Number Sum

---
+++ TwoNumberSum/TwoNumberSum.py
@@ -0,0 +1,20 @@
+from random import randrange
+import sys
+
+
+def main():
+    arr = [randrange(0, 99) for i in range(50)]    
+    result = TwoNumberSum(arr, 10)
+    print(result)
+    
+    
+def TwoNumberSum(array, targetSum):
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i] + array[j] == targetSum:
+                return [array[i], array[j]]
+    return [] 
+
+
+if __name__ == "__main__":
+    main()
