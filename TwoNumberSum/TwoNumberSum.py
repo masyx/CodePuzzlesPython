95c79ac26502fbffe527dea6a77d399910d2cd76
Sergey Matveev <rfmatveev@gmail.com>
2022-06-13 21:26:25 -0500

    correct number of iteration in first loop

--- TwoNumberSum/TwoNumberSum.py
+++ TwoNumberSum/TwoNumberSum.py
@@ -9,7 +9,7 @@ def main():
     
     
 def TwoNumberSum(array, targetSum):
-    for i in range(len(array)):
+    for i in range(len(array) - 1):
         for j in range(i + 1, len(array)):
             if array[i] + array[j] == targetSum:
                 return [array[i], array[j]]
