4835d22c3f9be1ce34fe7c8344e1a5abc7069191
Sergey Matveev <rfmatveev@gmail.com>
2022-06-19 15:15:22 -0500

    IsValidSubsequence 2nd solution

--- ValidateSubsequence/ValidateSubsequence.py
+++ ValidateSubsequence/ValidateSubsequence.py
@@ -1,11 +1,11 @@
 def main():
     array = [1, 1, 6, 1]
     sequence = [1, 1, 1, 6]    
-    print(isValidSubsequence(array, sequence))
+    print(isValidSubsequence2(array, sequence))
     
 
 # O(n) time | O(1) space    
-def isValidSubsequence(array, sequence):
+def isValidSubsequence1(array, sequence):
     arrIdx, seqIdx = 0, 0
     while arrIdx < len(array) and seqIdx < len(sequence):
         if array[arrIdx] == sequence[seqIdx]:
@@ -13,6 +13,16 @@ def isValidSubsequence(array, sequence):
         arrIdx += 1
 
     return  seqIdx == len(sequence)
+
+def isValidSubsequence2(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx < len(sequence):
+            break
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
+    
             
     
 
