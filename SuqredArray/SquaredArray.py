15fffbbc3c69bfd6822dbfec96dd9087312b4131
Sergey Matveev <rfmatveev@gmail.com>
2022-06-19 17:03:36 -0500

    sortedSquaredArray optimal solution O(n)

--- SuqredArray/SquaredArray.py
+++ SuqredArray/SquaredArray.py
@@ -1,15 +1,35 @@
 
 
 def main():
-    print("hi")
+    inputArray1 = [-3, -2, -1, 5]
+
+    print(sortedSquaredArrayOptimal(inputArray))
     
 def sortedSquaredArray(array):
     squaredArray = []
     for i in range(len(array)):
-        squaredArray.append(array[i]**2)
+        squaredValue = array[i]**2   
+        squaredArray.append(squaredValue)
     
     squaredArray.sort()    
     return squaredArray
+
+def sortedSquaredArrayOptimal(array):
+    squaredArray = [0] * len(array)
+    left = 0
+    right = len(array) - 1
+    for i in range(len(array)):
+        val1 = abs(array[left])
+        val2 = abs(array[right])
+        if val1 > val2:
+            squaredArray[-i - 1] = val1**2
+            left += 1
+        else:
+            squaredArray[-i - 1] = val2**2
+            right -= 1
+    
+    return squaredArray    
+        
 
 
 
