3d3a6bd63d7ae0e8f8e0f2aed94b4030bcfd47f4
Sergey Matveev <rfmatveev@gmail.com>
2022-06-21 21:06:03 -0500

    Sorted squred array another approach

--- SuqredArray/SquaredArray.py
+++ SuqredArray/SquaredArray.py
@@ -14,6 +14,17 @@ def sortedSquaredArray(array):
     squaredArray.sort()    
     return squaredArray
 
+
+def sortedSquaredArray2(array):
+    sortedArray = [0 for _ in array]
+    
+    for idx in range(len(array)):
+        sortedArray[idx] = array[idx]**2
+        
+    sortedArray.sort()
+    return sortedArray
+
+
 def sortedSquaredArrayOptimal(array):
     squaredArray = [0] * len(array)
     left = 0
@@ -28,7 +39,24 @@ def sortedSquaredArrayOptimal(array):
             squaredArray[-i - 1] = val2**2
             right -= 1
     
-    return squaredArray    
+    return squaredArray
+
+
+def sortedSquaredArrayOptimal(array):
+    squaredArray = [0] * len(array)
+    left = 0
+    right = len(array) - 1
+    for i in reversed(range(len(array))):
+        val1 = abs(array[left])
+        val2 = abs(array[right])
+        if val1 > val2:
+            squaredArray[i] = val1**2
+            left += 1
+        else:
+            squaredArray[i] = val2**2
+            right -= 1
+    
+    return squaredArray 
         
 
 
