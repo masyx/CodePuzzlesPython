6aaeeb707231419be7a07890a63451d1d4a1f3ec
Sergey Matveev <rfmatveev@gmail.com>
2022-06-25 22:00:13 -0500

    Fibonacci my solutions

--- Fibonacci/Fibonacci.py
+++ Fibonacci/Fibonacci.py
@@ -1,13 +1,24 @@
 def main():
-    print(getNthFib(4))
+    print(getNthFib(1))
     
-def getNthFib(n):
+def getNthFibNaive(n):
     if n == 2:
         return 1
     if n == 1:
         return 0
     
-    return getNthFib(n - 2) + getNthFib(n - 1)
+    return getNthFibNaive(n - 2) + getNthFibNaive(n - 1)
+
+# O(n) time | O(n) space
+def getNthFib(n):
+    arr = [None] * (n + 2)
+    arr[1] = 0
+    arr[2] = 1
+    
+    for i in range(3, n + 1):
+        arr[i] = arr[i - 2] + arr[i - 1]
+        
+    return arr[n]
     
     
 
