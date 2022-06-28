3721d7a4f2815b1ed89726ae6d73b31ff557c22c
Sergey Matveev <rfmatveev@gmail.com>
2022-06-27 22:03:49 -0500

    NodeDepth my recursive solution

--- NodeDepth/NodeDepth.py
+++ NodeDepth/NodeDepth.py
@@ -1,6 +1,6 @@
 class BinaryTree:
     def __init__(self, root):
-        self.root = root
+        self.value = root
         self.left = None
         self.right = None
         
@@ -17,14 +17,35 @@ def initializeBinaryTree():
     
     tree.right.left = BinaryTree(6)
     tree.right.right = BinaryTree(7)
+    
+    return tree
 
 def main():
     tree = initializeBinaryTree()
     
-    print(nodeDepth(tree))
+    print(nodeDepths(tree))
     
     
-def nodeDepth(root):
+def nodeDepths(root):
+    depths = []
+    getNodesDepth(root, -1, depths)
+    return sum(depths)
+    
+    
+def getNodesDepth(node, currentDepth, depths):
+    if node is None:
+        return
+    
+    newDepth = currentDepth + 1
+    if newDepth > 0:
+        depths.append(newDepth)
+        print(f"The depth of the node with value {node.value} is: {newDepth}")
+    
+    if node.left is None and node.right is None:
+        return
+    
+    getNodesDepth(node.left, newDepth, depths)
+    getNodesDepth(node.right, newDepth, depths)
     
     
     
