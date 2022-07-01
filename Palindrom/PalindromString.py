df34a3a3735adf6cb79cf0bbad28b631bb03c145
Sergey Matveev <rfmatveev@gmail.com>
2022-06-30 22:11:11 -0500

    Palindrom string with join

--- Palindrom/PalindromString.py
+++ Palindrom/PalindromString.py
@@ -14,14 +14,18 @@ def isPalindromeStringIterative(str):
     return False
         
     
-
+def isPalindromeStringJoin(string):
+    reversedStr = "".join(reversed(string))
+    if reversedStr == string:
+        return True
+    return False
 
 
 
 
 def main():
     myString = "ABC"
-    print(isPalindromeStringIterative(myString))
+    print(isPalindromeStringJoin(myString))
     
 if __name__ == "__main__":
     main()
