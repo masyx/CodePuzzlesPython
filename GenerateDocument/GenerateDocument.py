7117c79c9ef4beee45bcd4359a8380c4b4753b87
Sergey Matveev <rfmatveev@gmail.com>
2022-07-02 12:54:41 -0500

    Generate Document my solution iptimized

--- GenerateDocument/GenerateDocument.py
+++ GenerateDocument/GenerateDocument.py
@@ -23,7 +23,44 @@ def generateDocument(characters, document):
         elif resultTableCharacters[key] < resultTableDocument[key]:
             return False
     return True
-                
+
+
+def generateDocument2(characters, document):
+    charactersTable = {}
+    for Idx, char in enumerate(characters):
+        count = 1
+        if char not in charactersTable:
+            for i in range(Idx + 1, len(characters)):
+                if char == characters[i]:
+                    count += 1
+            charactersTable[char] = count
+            
+    for char in document:
+        if char in charactersTable and charactersTable[char] > 0:
+            charactersTable[char] -= 1
+        else:
+            return False
+    
+    return True
+
+
+def generateDocument3(characters, document):
+    charactersTable = {}
+    for i in range(len(characters)):
+        count = 1
+        if characters[i] not in charactersTable:
+            for j in range(i + 1, len(characters)):
+                if characters[i] == characters[j]:
+                    count += 1
+            charactersTable[characters[i]] = count
+            
+    for char in document:
+        if char in charactersTable and charactersTable[char] > 0:
+            charactersTable[char] -= 1
+        else:
+            return False
+    
+    return True                
                 
     
     
@@ -31,9 +68,11 @@ def generateDocument(characters, document):
     
     
 def main():
-    characters = "helloworldO "
-    document = "hello wOrld"
-    generateDocument(characters, document)
+    characters2 = "helloworldO "
+    document2 = "hello wOrld"
+    characters = "abcabc"
+    document = "aabbccc"
+    generateDocument3(characters, document)
    
 if __name__ == "__main__":
     main() 
