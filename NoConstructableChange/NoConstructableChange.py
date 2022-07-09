1cf14c175f3aba08c565ca311646b7a06260ec21
Sergey Matveev <rfmatveev@gmail.com>
2022-07-09 17:03:06 -0500

    Non Constrictible Change solution

--- NoConstructableChange/NoConstructableChange.py
+++ NoConstructableChange/NoConstructableChange.py
@@ -1,8 +1,20 @@
-def main():
-    print(NoConstructableChange())
+from operator import contains
+from typing import List
 
-def NoConstructableChange():
-    i = 1
+
+def main():
+    coins = [1, 1, 1,1,1]#[5, 7, 1, 1, 2, 3, 22] # [1, 1, 2, 3, 5, 7, 22]
+    print(nonConstructibleChange(coins))
+
+def nonConstructibleChange(coins):
+    coins.sort()
+    change = 0
+    for coin in coins:
+        if coin > change + 1:
+            return change + 1
+        change += coin
+            
+    return change + 1
     
     
     
