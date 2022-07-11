84a221c549d23e63774b816d8d7b49b793037247
Sergey Matveev <rfmatveev@gmail.com>
2022-07-11 10:36:20 -0500

    Remove duplic from Linked list algo solution

--- RemoveDuplicatesFromLinkedList/RemoveDuplicatesFromLinkedList.py
+++ RemoveDuplicatesFromLinkedList/RemoveDuplicatesFromLinkedList.py
@@ -16,12 +16,26 @@ def removeDuplicatesFromLinkedListBruteForce(linkedList):
             used[currentNode.value] = True
         previous = currentNode
         currentNode = currentNode.next
-    return linkedList      
+    return linkedList
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+    currentNode = linkedList
+    while currentNode is not None:
+        nextNode = currentNode.next
+        while nextNode is not None and currentNode.value == nextNode.value:
+            nextNode = nextNode.next
+
+        currentNode.next = nextNode
+        currentNode = nextNode
+        
+    return linkedList
 
 
 
 
 def main():
+    # 1 -> 1 -> 3 -> 4 -> 4 -> 5 -> 6 -> 6
     head = LinkedList(1)
     head.next = LinkedList(1)
     head.next.next = LinkedList(3)
@@ -30,7 +44,9 @@ def main():
     head.next.next.next.next.next = LinkedList(5)
     head.next.next.next.next.next.next = LinkedList(6)
     head.next.next.next.next.next.next.next = LinkedList(6)
-    print(removeDuplicatesFromLinkedList(head))
+    head.next.next.next.next.next.next.next.next = LinkedList(6)
+    ll = removeDuplicatesFromLinkedList(head)
+    print()
     
 if __name__ == "__main__":
     main()
