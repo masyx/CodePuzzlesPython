bbee5f2b0ac68a5e806dff77ab3e61b068afcea8
Sergey Matveev <rfmatveev@gmail.com>
2022-07-10 21:21:43 -0500

    minimum waiting time algo solution

--- MinimumWaitingTime/MinimumWaitingTime.py
+++ MinimumWaitingTime/MinimumWaitingTime.py
@@ -12,7 +12,7 @@ def minimumWaitingTimeBrut(queries):
         times.append(currentWaitingTime)
     return sum(times)
 
-
+# O(n log(n)) time | O(1) space
 def minimumWaitingTime(queries):
     queries.sort()
     currentWaitingTime = 0
@@ -23,6 +23,13 @@ def minimumWaitingTime(queries):
 
     return totalWaitingTime
 
+# O(n log(n)) time | O(1) space
+def minimumWaitingTimeAlgo(queries):
+    queries.sort()
+    totalWT = 0
+    for i in range(len(queries)):
+        totalWT += queries[i] * (len(queries) - 1 - i)
+    return totalWT
 
 if __name__ == "__main__":
     main()
