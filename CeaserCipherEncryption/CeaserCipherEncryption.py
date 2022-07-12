78396aa86f9e204fdc68a8a0930ca54c45e6346c
Sergey Matveev <rfmatveev@gmail.com>
2022-07-12 13:09:19 -0500

    CeaserCipherEncryption second solution

--- CeaserCipherEncryption/CeaserCipherEncryption.py
+++ CeaserCipherEncryption/CeaserCipherEncryption.py
@@ -1,3 +1,4 @@
+# O(n) time | O(n) space
 def caesarCipherEncryptor(string, key):
     newWord = []
     for char in string:
@@ -9,8 +10,20 @@ def caesarCipherEncryptor(string, key):
     return "".join(newWord)
 
 
+def caesarCipherEncryptor(string, key):
+    newWord = []
+    newKey = key % 26
+    for char in string:
+        newLetterCode = ord(char) + newKey
+        if newLetterCode > 122:
+            newLetterCode = 96 + newLetterCode % 122
+        newWord.append(chr(newLetterCode))
+        
+    return "".join(newWord)
+
+
 def main():
-    print(caesarCipherEncryptor('xyz', 2)) 
+    print(caesarCipherEncryptor('xyz', 10000000)) 
     
     
 if __name__ == "__main__":
