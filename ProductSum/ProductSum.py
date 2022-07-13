80e383968d40f772a2663e0c3004a5286f7909fc
Sergey Matveev <rfmatveev@gmail.com>
2022-07-12 20:45:01 -0500

    ProductSum

---
+++ ProductSum/ProductSum.py
@@ -0,0 +1,14 @@
+# Tip: You can use the type(element) function to check whether an item
+# is a list or an integer.
+def productSum(array):
+    return
+
+
+
+def main():
+    print()
+    
+
+if __name__ == "__main__":
+    array = [5, 2, [7, 1], 3, [6, [-13, 8], 4]]
+    main(productSum(array)) 
