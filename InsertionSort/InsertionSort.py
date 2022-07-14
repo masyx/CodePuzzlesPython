75329d1dd1d440478cf8fdb5748755cb954bb8b5
Sergey Matveev <rfmatveev@gmail.com>
2022-07-14 10:12:49 -0500

    Insertion sort my 2nd solution

--- InsertionSort/InsertionSort.py
+++ InsertionSort/InsertionSort.py
@@ -1,4 +1,4 @@
-#O(n^2) time | O(n) space
+#O(n^2) time | O(1) space
 def insertionSort(array):
     for i in range(len(array) - 1):
         if array[i] > array[i + 1]:
@@ -11,9 +11,21 @@ def insertionSort(array):
     return array
 
 
+def insertionSort2(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            swap(i, i + 1, array)
+            for j in range(i, 0, -1):
+                if array[j] < array[j - 1]:
+                    swap(j, j - 1, array)
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+
 def main():
     array = [2,-3,99,0,1000,-1000,9,8,1,-1]
-    print(insertionSort(array))
+    print(insertionSort2(array))
     
 if __name__ == "__main__":
     main()
