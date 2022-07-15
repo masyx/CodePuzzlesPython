8016548d062b306c329e22ca1012fd8174353306
Sergey Matveev <rfmatveev@gmail.com>
2022-07-15 12:22:38 -0500

    Depth First Search my solution

--- DepthFirstSearch/DepthFirstSearch.py
+++ DepthFirstSearch/DepthFirstSearch.py
@@ -7,5 +7,35 @@ class Node:
         self.children.append(Node(name))
         return self
     
+    # O(v+e) time | O(v) space
     def depthFirstSearch(self, array):
-        pass
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+    
+
+def main():
+    root = Node('A')
+    root.addChild('B')
+    root.addChild('C')
+    root.addChild('D')
+    
+    root.children[0].addChild('E')
+    root.children[0].addChild('F')
+    root.children[0].children[1].addChild('I')
+    root.children[0].children[1].addChild('J')
+    
+    root.children[2].addChild('G')
+    root.children[2].addChild('H')
+    root.children[2].children[0].addChild('K')
+    
+    
+
+    
+    array = []
+    print(root.depthFirstSearch(array))
+    
+    
+if __name__ == "__main__":
+    main()
