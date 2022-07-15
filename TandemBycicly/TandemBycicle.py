b66323728411a03ceb2febddab1c2685ca558304
Sergey Matveev <rfmatveev@gmail.com>
2022-07-15 15:39:25 -0500

    TandemBicycles 2nd solution with array reversal

--- TandemBycicly/TandemBycicle.py
+++ TandemBycicly/TandemBycicle.py
@@ -14,10 +14,31 @@ def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
         fastestTotalSpeed += max(redShirtSpeeds[i], blueShirtSpeeds[i])
     return fastestTotalSpeed
 
+
+def tandemBicycle2(redShirtSpeeds, blueShirtSpeeds, fastest):
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+    
+    if fastest:
+        reverseArray(blueShirtSpeeds)
+    
+    totalSpeed = 0    
+    for i in range(len(redShirtSpeeds)):
+        totalSpeed += max(redShirtSpeeds[i], blueShirtSpeeds[i])
+    return totalSpeed    
+    
+def reverseArray(array):
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        array[left], array[right] = array[right], array[left]
+        left += 1
+        right -= 1
+    
 def main():
     redShirtSpeeds = [5, 5, 3, 9, 2]
     blueShirtSpeeds = [3, 6, 7, 2, 1]
-    print(tandemBicycle(redShirtSpeeds, blueShirtSpeeds, False))
+    print(tandemBicycle2(redShirtSpeeds, blueShirtSpeeds, True))
     
 if __name__ == "__main__":
     main()
