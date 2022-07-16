f961b8e43b86c3f50f264483181048e732b551ec
Sergey Matveev <rfmatveev@gmail.com>
2022-07-16 16:26:16 -0500

    runLengthEncoding my solution- cumbersome

--- RunLengthEncoding.py
+++ RunLengthEncoding.py
@@ -1,10 +1,34 @@
 def runLengthEncoding(string):
-    # Write your code here.
-    pass
+    resultArray = []
+    count = 0
+    if len(string) == 1:
+        resultArray.append(f"1{string[0]}")
+    for i in range(len(string) - 1):
+        if string[i] == string[i + 1]:
+            count += 1
+            if count == 9:
+                resultArray.append(f"{count}{string[i]}")
+                count = 0
+            if i == len(string) - 2:
+                count += 1
+                resultArray.append(f"{count}{string[i]}")
+                count = 0
+            
+        else:
+            count += 1
+            resultArray.append(f"{count}{string[i]}")
+            count = 0
+            if i == len(string) - 2:
+                count += 1
+                resultArray.append(f"{count}{string[i + 1]}") 
+        
+    return "".join(resultArray)
+            
 
 
 def main():
-    print()
+    string = "A" #'AAAAAAAAAAAAABBCCCCDF'
+    print(runLengthEncoding(string))
     
 if __name__ == "__main__":
     main()
