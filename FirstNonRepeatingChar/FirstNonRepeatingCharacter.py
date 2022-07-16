63e6682a7df0f61496039f06b8e2ac6730795559
Sergey Matveev <rfmatveev@gmail.com>
2022-07-15 19:55:58 -0500

    First non repeative char my brut force solution

--- FirstNonRepeatingChar/FirstNonRepeatingCharacter.py
+++ FirstNonRepeatingChar/FirstNonRepeatingCharacter.py
@@ -1,9 +1,24 @@
+# O(n) time | O(n) space
 def firstNonRepeatingCharacter(string):
-    return
+    used = {}
+    for char in string:
+        if char not in used:
+            used[char] =  1
+            continue
+        used[char] = used[char] + 1
+    
+    for element in used:
+        if used[element] == 1:
+            for i in range(len(string)):
+                if string[i] == element:
+                    return i
+    
+    return -1
 
 
 def main():
-    string = "zopazopa"
+    string = "abcdcaf"
+    print(firstNonRepeatingCharacter(string))
     
     
 if __name__ == "__main__":
