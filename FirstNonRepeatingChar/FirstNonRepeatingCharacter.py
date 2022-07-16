24316a97083534a45707e1f41305541eaae6fde1
Sergey Matveev <rfmatveev@gmail.com>
2022-07-15 20:29:10 -0500

    First non repeating char efficient my solution

--- FirstNonRepeatingChar/FirstNonRepeatingCharacter.py
+++ FirstNonRepeatingChar/FirstNonRepeatingCharacter.py
@@ -7,6 +7,21 @@ def firstNonRepeatingCharacter(string):
             continue
         used[char] = used[char] + 1
     
+    for i in range(len(string)):
+        if used[string[i]] == 1:
+            return i
+    
+    return -1
+
+# O(k^2) time where k is number of elements in hash table | O(n) space
+def firstNonRepeatingCharacter1(string):
+    used = {}
+    for char in string:
+        if char not in used:
+            used[char] =  1
+            continue
+        used[char] = used[char] + 1
+    
     for element in used:
         if used[element] == 1:
             for i in range(len(string)):
