03e3b4c4f015c908f35b306304cd61a60ab4149e
Sergey Matveev <rfmatveev@gmail.com>
2022-07-19 07:18:09 -0500

    Class photo my solution

--- ClassPhotos.py
+++ ClassPhotos.py
@@ -1,10 +1,24 @@
+# O(n log(n)) time | O(1) space
 def classPhotos(redShirtHeights, blueShirtHeights):
-    # Write your code here.
-    return False
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+    if redShirtHeights[-1] > blueShirtHeights[-1]:
+        return arrangePhoto(redShirtHeights, blueShirtHeights)
+    else:
+        return arrangePhoto(blueShirtHeights, redShirtHeights)
+        
+def arrangePhoto(tallerGroup, smallerGroup):
+    for i in range(len(tallerGroup)):
+        if tallerGroup[i] <= smallerGroup[i]:
+            return False
+    return True
+    
 
 
 def main():
-    print(classPhotos())
+    blueShirts = [5,8,1,3,4]
+    redShirts = [6,9,2,4,5]
+    print(classPhotos(redShirts, blueShirts))
     
     
 if __name__ == "__main__":
