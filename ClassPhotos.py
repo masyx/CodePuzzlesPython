26b3aafa6c2612456075f2f855e159980ccbd8e3
Sergey Matveev <rfmatveev@gmail.com>
2022-07-19 07:46:58 -0500

    Class Photos algo solution

--- ClassPhotos.py
+++ ClassPhotos.py
@@ -13,12 +13,27 @@ def arrangePhoto(tallerGroup, smallerGroup):
             return False
     return True
     
+    
+def classPhotosAlgo(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort(reverse=True)
+    blueShirtHeights.sort(reverse=True)
+    
+    groupInFirstRow = 'RED' if redShirtHeights[0] < blueShirtHeights[0] else 'BLUE'
+    
+    for idx in range(len(redShirtHeights)):
+        if groupInFirstRow == 'RED':
+            if redShirtHeights[idx] >= blueShirtHeights[idx]:
+                return False
+        else:
+            if blueShirtHeights[idx] >= redShirtHeights[idx]:
+                return False
+    return True
 
 
 def main():
     blueShirts = [5,8,1,3,4]
     redShirts = [6,9,2,4,5]
-    print(classPhotos(redShirts, blueShirts))
+    print(classPhotoAlgo(redShirts, blueShirts))
     
     
 if __name__ == "__main__":
