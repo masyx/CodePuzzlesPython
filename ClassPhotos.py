7149ddc87750908612434b311b01260625239262
Sergey Matveev <rfmatveev@gmail.com>
2022-07-19 06:19:39 -0500

    Class photos added

---
+++ ClassPhotos.py
@@ -0,0 +1,11 @@
+def classPhotos(redShirtHeights, blueShirtHeights):
+    # Write your code here.
+    return False
+
+
+def main():
+    print(classPhotos())
+    
+    
+if __name__ == "__main__":
+    main()
