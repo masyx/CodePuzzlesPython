556009a33713d67d5ae54c858f44a4e285766e03
Sergey Matveev <rfmatveev@gmail.com>
2022-07-20 20:43:34 -0500

    TwoNumberSum training

--- A_Training.py
+++ A_Training.py
@@ -1,3 +1,28 @@
-output = [float('-inf')] * 4
+# O(n) time | O(n) space
+def twoNumberSum(array, targetSum):
+    used = {}
+    for num in array:
+        possibleNumber = targetSum - num
+        if possibleNumber in used:
+            return [num, possibleNumber]
+        used[num] = True
+    return []
 
-print(type(output[0]))
+# O(n log(n)) time | O(1) space
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        if array[left] + array[right] == targetSum:
+            return [array[left], array[right]]
+        elif array[left] + array[right] > targetSum:
+            right -= 1
+        else:
+            left += 1
+    return []
+
+
+def main():
+    array = [12, 3, 1, 2, -6, 5, -8, 6]
+    print(twoNumberSum(array, 0))
