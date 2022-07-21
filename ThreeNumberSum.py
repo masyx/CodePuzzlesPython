284c0c3e940120f8321d229d326e0aedf4b54901
Sergey Matveev <rfmatveev@gmail.com>
2022-07-20 21:05:45 -0500

    Three number sum my solution

--- ThreeNumberSum.py
+++ ThreeNumberSum.py
@@ -1,10 +1,29 @@
 # medium
 
+# O(n^2) time | O(n) space, the max amount of triplets with numbers in it is bounded by n
 def threeNumberSum(array, targetSum):
-    # Write your code here.
-    pass
+    array.sort()
+    result = []
+    for i in range(len(array)):
+        left = i + 1
+        right = len(array) - 1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == targetSum:
+                result.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif currentSum > targetSum:
+                right -= 1
+            else:
+                left += 1
+    return result    
 
 
 def main():
-    array = [12,3,1,2,-6,5,-8,6]
-    print(threeNumberSum(array, 0))
+    array = [12, 3, 1, 2, -6, 5, -8, 6]
+    print(threeNumberSum(array, 9))
+    
+    
+if __name__ == "__main__":
+    main()
