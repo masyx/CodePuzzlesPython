caaae8a4f58f72573e135c39d3c36482c1ef0727
Sergey Matveev <rfmatveev@gmail.com>
2022-07-21 06:09:27 -0500

    Smallest Defference added

---
+++ SmallestDifference.py
@@ -0,0 +1,15 @@
+# medium
+
+def smallestDifference(array_one, array_two):
+                
+    return 
+
+
+def main():
+    array_one = [-1, 5, 10, 20, 28, 3]
+    array_two = [26, 134, 134, 15, 17]
+    print(smallestDifference(array_one, array_two))
+    
+    
+if __name__ == "__main__":
+    main()
