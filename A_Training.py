a846517f303958f1b38ff32ecc67810f080b12db
Sergey Matveev <rfmatveev@gmail.com>
2022-07-21 20:58:54 -0500

    Valid Subsequence training

--- A_Training.py
+++ A_Training.py
@@ -1,28 +1,20 @@
-# O(n) time | O(n) space
-def twoNumberSum(array, targetSum):
-    used = {}
-    for num in array:
-        possibleNumber = targetSum - num
-        if possibleNumber in used:
-            return [num, possibleNumber]
-        used[num] = True
-    return []
 
-# O(n log(n)) time | O(1) space
-def twoNumberSum(array, targetSum):
-    array.sort()
-    left = 0
-    right = len(array) - 1
-    while left < right:
-        if array[left] + array[right] == targetSum:
-            return [array[left], array[right]]
-        elif array[left] + array[right] > targetSum:
-            right -= 1
-        else:
-            left += 1
-    return []
+def isValidSubsequence(array, sequence):
+    seq_idx = 0
+    for num in array:
+        if seq_idx < len(sequence) and num == sequence[seq_idx]:
+            seq_idx += 1
+    is_valid = len(sequence) == seq_idx
+    return is_valid
+                
+        
 
 
 def main():
-    array = [12, 3, 1, 2, -6, 5, -8, 6]
-    print(twoNumberSum(array, 0))
+    array = [5, 1, 22, 25, 6, -1, 8, 10]
+    sequence = [1, 6, -1, 10]
+    print(isValidSubsequence(array, sequence))
+    
+    
+if __name__ == "__main__":
+    main()
