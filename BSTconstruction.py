3f47624e7c1a5db77efab411927ed6e9db6dc931
Sergey Matveev <rfmatveev@gmail.com>
2022-07-22 21:21:45 -0500

    BST insert method

--- BSTconstruction.py
+++ BSTconstruction.py
@@ -4,8 +4,17 @@ class BST:
         self.left = None
         self.right = None
         
-    def insert(self, value):
-
+    def insert(self, value): 
+        if value >= self.value:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                return BST.insert(self.right, value)
+        else:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                return BST.insert(self.left, value)
         return self
     
     def remove(self, value):
@@ -13,3 +22,23 @@ class BST:
     
     def contain(self, value):
         return self
+    
+    
+    
+def main():
+    tree = BST(10)
+    
+    tree.insert(5)
+    tree.insert(2)
+    tree.insert(5)
+    tree.insert(1)
+    
+    tree.insert(15)
+    tree.insert(13)
+    tree.insert(22)
+    tree.insert(14)
+    tree.insert(12)
+    print(tree)
+    
+if __name__ == "__main__":
+    main()
