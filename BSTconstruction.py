2c3737c854ff1860c9d36cedeafe4090238fdbae
Sergey Matveev <rfmatveev@gmail.com>
2022-07-23 13:00:59 -0500

    BST construction remove

--- BSTconstruction.py
+++ BSTconstruction.py
@@ -40,8 +40,28 @@ class BST:
                      # be able to run insert function like  testBst.insert(5).insert(10)...   
 
     
-    def remove(self, value):
-        return
+    def remove(root, key):
+        if not root: # if root doesn't exist, just return it
+            return root
+        if root.value > key: # if key value is less than root value, find the node in the left subtree
+            root.left = BST.remove(root.left, key)
+        elif root.value < key: # if key value is greater than root value, find the node in right subtree
+            root.right= BST.remove(root.right, key)
+        else: #if we found the node (root.value == key), start to delete it
+            if not root.right: # if it doesn't have right children, we delete the node then new root would be root.left
+                return root.left
+            if not root.left: # if it has no left children, we delete the node then new root would be root.right
+                return root.right
+                # if the node have both left and right children,  we replace its value with the minmimum value in the right subtree and then delete that minimum node in the right subtree
+            temp = root.right
+            mini = temp.value
+            while temp.left:
+                temp = temp.left
+                mini = temp.value
+            root.value = mini # replace value
+            root.right = BST.remove(root.right,root.value) # delete the minimum node in right subtree
+        return root
+            
             
     # O(log(n)) time average | O(log(n)) space
     # O(n) time worst 
@@ -85,7 +105,8 @@ def main():
     tree.insert(14)
     tree.insert(12)
     
-    what = tree.contain(15)
+    what = tree.contains(15)
+    hh = tree.remove(5)
     print(what)
     
 if __name__ == "__main__":
