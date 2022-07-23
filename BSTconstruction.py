4df046b6bb508c67768051230c683d6efe3d9031
Sergey Matveev <rfmatveev@gmail.com>
2022-07-22 19:47:41 -0500

    BST constrcuction added

---
+++ BSTconstruction.py
@@ -0,0 +1,15 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def insert(self, value):
+
+        return self
+    
+    def remove(self, value):
+        return self
+    
+    def contain(self, value):
+        return self
