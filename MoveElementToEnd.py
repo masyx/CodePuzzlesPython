75c3a3bd1563438146fc4457cbab5cd581ce8e6e
Sergey Matveev <rfmatveev@gmail.com>
2022-07-24 21:16:29 -0500

    Move element to end my solution

--- MoveElementToEnd.py
+++ MoveElementToEnd.py
@@ -1,13 +1,26 @@
+# O(n) time | O(1) space
 def moveElementToEnd(array, toMove):
-    # Write your code here.
-    pass
+    r = len(array) - 1
+    l = 0
+    while l < r:
+        if array[l] == toMove:
+            while array[r] == toMove:
+                r -= 1
+                if l >= r:
+                    break
+            array[l], array[r] = array[r], array[l]
+            r -= 1
+        l += 1           
+    return array
 
 
 
 
 
 def main():
-    print(what)
+    arr = [2, 1, 2, 2, 2, 3, 4, 2]
+    to_move = 2
+    print(moveElementToEnd(arr, to_move))
     
 if __name__ == "__main__":
     main()
