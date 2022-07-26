a3a4dc1187e736ff271e3e7ae53d218f9d308282
Sergey Matveev <rfmatveev@gmail.com>
2022-07-26 06:38:12 -0500

    First duplicate value brute force solution

--- FirstDuplicateValue.py
+++ FirstDuplicateValue.py
@@ -1,18 +1,31 @@
 class Solution:
+    # O(n) time | O(n) space
     def firstDuplicateValue(self, nums: list[int]) -> bool:
-        present = {}
+        present = set()
         for num in nums:
             if num in present:
-                return True
-            present[num] = True
-        return False
+                return num
+            present.add(num)
+        return -1
+    
+    # O(n^2) time | O(1) space
+    def firstDuplicateValueBruteForce(self, nums):
+        min_second_index = len(nums)
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    min_second_index = min(min_second_index, j)
+        if min_second_index == len(nums):
+            return - 1
+        else:
+            return nums[min_second_index]
     
 
 
 def main():
-    array = [1,2,3,1]
+    array = [2, 1, 5, 3, 3, 2, 4]
     s = Solution()
-    print(s.firstDuplicateValue(array))
+    print(s.firstDuplicateValueBruteForce(array))
     
     
 if __name__ == "__main__":
