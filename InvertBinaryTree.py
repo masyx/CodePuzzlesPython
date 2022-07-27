2abf707af9bf5da0d92d52f7a0b33d4e4ea643d3
Sergey Matveev <rfmatveev@gmail.com>
2022-07-27 08:41:06 -0500

    InvertBinaryTree queue solution

--- InvertBinaryTree.py
+++ InvertBinaryTree.py
@@ -24,6 +24,16 @@ def invertBinaryTree(tree):
     tree.left, tree.right = tree.right, tree.left
     invertBinaryTree(tree.left)
     invertBinaryTree(tree.right)
+    
+def invertBinaryTreeQueue(tree):
+    queue = [tree]
+    while len(queue):
+        currentNode = queue.pop(0)
+        if currentNode is None:
+            continue
+        currentNode.left, currentNode.right = currentNode.right, currentNode.left
+        queue.append(currentNode.left)
+        queue.append(currentNode.right)
 
 
 
@@ -32,10 +42,10 @@ def invertBinaryTree(tree):
 def initializeBinaryTree():
     tree = BinaryTree(1)
     tree.left = BinaryTree(2)
-    tree.right = BinaryTree(3)
+    # tree.right = BinaryTree(3)
     
-    tree.left.left = BinaryTree(4)
-    tree.left.right = BinaryTree(5)
+    # tree.left.left = BinaryTree(4)
+    # tree.left.right = BinaryTree(5)
     
     # tree.left.left.left = BinaryTree(8)
     # tree.left.left.right = BinaryTree(9)
@@ -48,7 +58,7 @@ def initializeBinaryTree():
 
 def main():
     tree = initializeBinaryTree()
-    print(invertBinaryTree(tree))
+    print(invertBinaryTreeQueue(tree))
     print
     
 if __name__ == "__main__":
