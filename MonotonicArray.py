2df277dc418bdb41f9012e51a17f40d06b462f33
Sergey Matveev <rfmatveev@gmail.com>
2022-07-28 06:23:47 -0500

    MonotonicArray added

---
+++ MonotonicArray.py
@@ -0,0 +1,12 @@
+def isMonotonic(array: list[int]) -> bool:
+    # Write your code here.
+    pass
+
+
+def main():
+    nums = [1,2,2,3]
+    print(isMonotonic(nums))
+    
+    
+if __name__ == '__main__':
+    main()
