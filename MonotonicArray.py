90ef2b865e2b76c8158cad48ea809b26e7bad78f
Sergey Matveev <rfmatveev@gmail.com>
2022-07-28 07:32:58 -0500

    MonotonicArray my first solution

--- MonotonicArray.py
+++ MonotonicArray.py
@@ -1,10 +1,38 @@
-def isMonotonic(array: list[int]) -> bool:
-    # Write your code here.
-    pass
+#O(n) time | O(1) space
+def isMonotonic(array) -> bool:
+    monotonic = True
+    increasing = False
+    if len(array) <= 1:
+        return monotonic
+    for i in range(1, len(array)):
+        if array[i] == array[i - 1]:
+            continue
+        elif array[i] > array[i - 1]:
+            increasing = True
+            break
+        else:
+            increasing = False
+            break
+    
+    if increasing:
+        for i in range(1, len(array)):
+            if array[i] >= array[i - 1]:
+                continue
+            else:
+                monotonic = False
+                break
+    else:
+        for i in range(1, len(array)):
+            if array[i] <= array[i - 1]:
+                continue
+            else:
+                monotonic = False
+                break
+    return monotonic
 
 
 def main():
-    nums = [1,2,2,3]
+    nums = [1,1,2,2,3]
     print(isMonotonic(nums))
     
     
