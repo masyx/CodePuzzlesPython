25a90be8611af94cbc5e18e2b58a97dea3ee5474
Sergey Matveev <rfmatveev@gmail.com>
2022-07-29 18:36:00 -0500

    SpiralTraverse added

---
+++ SpiralTraverse.py
@@ -0,0 +1,20 @@
+def spiralTraverse(array):
+    # Write your code here.
+    pass
+
+
+
+
+
+def main():
+    nums = [
+        [1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10, 9, 8, 7]
+        ]
+    print(spiralTraverse(nums))
+    
+    
+if __name__ == '__main__':
+    main()
