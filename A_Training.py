dd9a0d8c1c97b5dbe110855172585af96bb053e4
Sergey Matveev <rfmatveev@gmail.com>
2022-07-29 21:21:22 -0500

    SortedSquaredArray training

--- A_Training.py
+++ A_Training.py
@@ -1,24 +1,22 @@
-def getNthFibRec(n):
-    if n == 2:
-        return 1
-    if n == 1:
-        return 0
-    return getNthFib(n - 2) + getNthFib(n - 1)
-
-def getNthFib(n):
-    arr = [0, 1]
-    counter = 3
-    while counter <= n:
-        nextValue = sum(arr)
-        arr[0] = arr[1]
-        arr[1] = nextValue
-        counter += 1
-    return arr[1] if n > 1 else arr[0]
-
-
+def sortedSquaredArray(array):
+    result = [0] * len(array)
+    l = 0
+    r = len(array) - 1
+    for i in reversed(range(len(array))):
+        val_1 = abs(array[l])
+        val_2 = abs(array[r])
+        if val_1 > val_2:
+            result[i] = val_1**2
+            l += 1
+        else:
+            result[i] = val_2**2
+            r -= 1
+    return result
 
 def main():
-    print(getNthFib(5))
+    # [25]
+    arr = [-5, 2, 3, 5, 6, 8, 9]
+    print(sortedSquaredArray(arr))
     
 if __name__ == "__main__":
     main()
