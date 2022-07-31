aa36eb03e7cf970988f429fdc37a15d97d51104f
Sergey Matveev <rfmatveev@gmail.com>
2022-07-31 12:30:42 -0500

    MaxSubsetSumNoAdjacent added

---
+++ MaxSubsetSumNoAdjacent.py
@@ -0,0 +1,12 @@
+def maxSubsetSumNoAdjacent(array):
+    # Write your code here.
+    pass
+
+
+
+def main():
+    arr = [-5, 2, 3, 5, 6, 8, 9]
+    print()
+    
+if __name__ == "__main__":
+    main()
