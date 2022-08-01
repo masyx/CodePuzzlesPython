a346fbdacdb5fb544a270515dd22f9eda43f7037
Sergey Matveev <rfmatveev@gmail.com>
2022-08-01 07:44:35 -0500

    SingleCycleCheck added

---
+++ SingleCycleCheck.py
@@ -0,0 +1,13 @@
+def hasSingleCycle(array):
+    # Write your code here.
+    pass
+
+
+
+def main():
+
+    arr = [2, 3, 1, -4, -4, 2]
+    print(hasSingleCycle(arr, 5))
+    
+if __name__ == "__main__":
+    main()
