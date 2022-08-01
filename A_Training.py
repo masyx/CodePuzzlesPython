624c5fc2eaa8366cf4be30aee87d4bb7c8dd7bff
Sergey Matveev <rfmatveev@gmail.com>
2022-07-31 20:55:43 -0500

    Training BinarySearch iterative solution

--- A_Training.py
+++ A_Training.py
@@ -15,11 +15,24 @@ def binarySearchHelper(arr, target, left, right):
         return binarySearchHelper(arr, target, m + 1, right)
 
 
+def binarySearchIterative(arr, target):
+    l = 0
+    r = len(arr) - 1
+    
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] == target:
+            return m
+        elif target < arr[m]:
+            r = m - 1
+        else:
+            l = m + 1
+    return -1
 
 def main():
 
     arr = [5, 23, 111]
-    print(binarySearch(arr, 120))
+    print(binarySearchIterative(arr, 5))
     
 if __name__ == "__main__":
     main()
