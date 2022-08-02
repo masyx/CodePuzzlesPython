8b7a4ad893b21800caa6274bcce72b22a96c9fb7
Sergey Matveev <rfmatveev@gmail.com>
2022-08-01 20:36:52 -0500

    SingleCycleCheck my wrong solution fo

--- SingleCycleCheck.py
+++ SingleCycleCheck.py
@@ -1,13 +1,21 @@
-def hasSingleCycle(array):
-    # Write your code here.
-    pass
+# left it here for tracking purpose
+def hasSingleCycleWrong(array):
+    cyclic = False
+    idx = 0
+    visited = {idx}
+    for i in range(len(array) - 1):
+        idx = (idx + array[idx]) % len(array)        
+        visited.add(idx)
+    if len(array) == len(visited):
+        cyclic = True
+    return cyclic
 
 
 
 def main():
 
-    arr = [2, 3, 1, -4, -4, 2]
-    print(hasSingleCycle(arr, 5))
+    arr = [1, 1, 1, 1, 2]
+    print(hasSingleCycle(arr))
     
 if __name__ == "__main__":
     main()
