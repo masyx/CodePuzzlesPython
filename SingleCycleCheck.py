26995ba5152879c5b4d2f87e2d4196ea15482d02
Sergey Matveev <rfmatveev@gmail.com>
2022-08-02 07:46:44 -0500

    SingeCycleCheck algo solution

--- SingleCycleCheck.py
+++ SingleCycleCheck.py
@@ -13,12 +13,28 @@ def hasSingleCycleMy(array):
         cyclic = True
     return cyclic
 
+# O(n) time | O(1) space
+def hasSingleCycle(array):
+    num_of_visited_elements = 1
+    current_idx = 0
+    while num_of_visited_elements <= len(array):
+        if num_of_visited_elements > 1 and current_idx == 0:
+            return False
+        # In Python, however, "the modulo operator always yields a result 
+        #       with the same sign as its second operand (or zero)"
+        # More specifically, the modulo operator in Python behaves as follows when used with a negative first operand:
+        #       -x % y == -(x % y) + y
+        current_idx = (current_idx + array[current_idx]) % len(array)
+        num_of_visited_elements += 1
+    return current_idx == 0
+    
+
 
 
 def main():
 
-    arr = [2, 3, 1, -4, -4, 2]
-    print(hasSingleCycleMy(arr))
+    arr = [1,1,-3,-3]
+    print(hasSingleCycle(arr))
     
 if __name__ == "__main__":
     main()
