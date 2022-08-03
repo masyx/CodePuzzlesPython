5375bab3fa15676732b1cdfa61ee17f9b059fe1d
Sergey Matveev <rfmatveev@gmail.com>
2022-08-02 19:16:46 -0500

    MinNumberOfCoins created

---
+++ MinNumberOfCoins.py
@@ -0,0 +1,14 @@
+def minNumberOfCoinsForChange(n, denoms):
+    # Write your code here.
+    pass
+
+
+
+def main():
+    coins = [1,5,10]
+    change = 7
+    minNumberOfCoinsForChange(change, coins)
+    
+    
+if __name__ == "__main__":
+    main()
