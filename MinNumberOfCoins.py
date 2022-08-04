d69c65dc17755680352628cd296b5656199cd1f8
Sergey Matveev <rfmatveev@gmail.com>
2022-08-04 07:39:08 -0500

    MinNumberOfCoins repeating

--- MinNumberOfCoins.py
+++ MinNumberOfCoins.py
@@ -7,22 +7,25 @@ def min_number_of_coins_for_change_greedy(n, denoms):
             return num_of_coins
     return -1
 
+
 # O(nd) time, where n is target amount and d is number of denominations that we have
 # O(n) space
 def min_number_of_coins_for_change(n, denoms):
-    num_of_coins = [float('inf')] * (n + 1)
+    num_of_coins = [float("inf") for x in range(n + 1)]
     num_of_coins[0] = 0
     for denom in denoms:
-        for amount in range(len(num_of_coins)):
+        for amount in range(n + 1):
             if denom <= amount:
-                left = amount - denom
-                total_coins = 1 + num_of_coins[left]
-                num_of_coins[amount] = min(num_of_coins[amount], total_coins)
-    return num_of_coins[n] if num_of_coins[n] != float('inf') else -1
+                amount_left = amount - denom
+                coins_at_amount_left= num_of_coins[amount_left]
+                num_of_coins[amount] = min(num_of_coins[amount], 1 + coins_at_amount_left)
+    return num_of_coins[n] if num_of_coins[n] != float("inf") else -1
+
+
 
 def main():
-    coins = [1, 3, 4]
-    change = 10
+    coins = [2, 3]
+    change = 5
     print(min_number_of_coins_for_change(change, coins))
 
 
