a442ead98cfec25dd1d8a65c79c5f2db744d4137
Sergey Matveev <rfmatveev@gmail.com>
2022-08-08 08:07:03 -0500

    Remove Islands added

---
+++ RemoveIslands.py
@@ -0,0 +1,19 @@
+def removeIslands(matrix):
+    # Write your code here.
+    return []
+
+
+def main():
+    matrix = [
+    [1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1],
+    [0, 0, 1, 0, 1, 0],
+    [1, 1, 0, 0, 1, 0],
+    [1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0, 1]
+    ]
+    removeIslands(matrix)
+    
+    
+if __name__ == "__main__":
+    main()
