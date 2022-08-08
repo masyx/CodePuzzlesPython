b1ff0343f74a8f4e7adea4c8d46f92761332b8e6
Sergey Matveev <rfmatveev@gmail.com>
2022-08-07 19:45:09 -0500

    BST traversal solutions

--- BST_traversal.py
+++ BST_traversal.py
@@ -4,33 +4,50 @@ class BST():
         self.left = None
         self.right = None
 
-def inOrderTraverse(tree, array):
 
+# FOR ALL 3 FUNCTIONS: O(n) time | O(n) space, but if we didn't have to return an array
+# space complexity would be O(d), where d is the depth of the tree
+def inOrderTraverse(tree, array):
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
 
 
 def preOrderTraverse(tree, array):
-    # Write your code here.
-    pass
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
+    return array
 
 
 def postOrderTraverse(tree, array):
-    # Write your code here.
-    pass
+    if tree is not None:
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
 
 
 def main():
     tree = BST(3)
     tree.left = BST(1)
     tree.right = BST(5)
-    
+
     tree.left.left = BST(-5)
     tree.left.right = BST(2)
-    
+
     tree.left.left.left = BST(-10)
-    
-    result_in_order = []
-    inOrderTraverse(tree, result_in_order)
-    print(result_in_order)
+
+    result = []
+    print(inOrderTraverse(tree, result))
+    result.clear()
+    print(preOrderTraverse(tree, result))
+    result.clear()
+    print(postOrderTraverse(tree, result))
+
 
 if __name__ == '__main__':
     main()
