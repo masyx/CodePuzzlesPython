3c13164bf53c6460701e64b450b1a68a2603b5f7
Sergey Matveev <rfmatveev@gmail.com>
2022-08-10 20:39:50 -0500

    FindSuccessor simple solution

---
+++ FindSuccessor.py
@@ -0,0 +1,48 @@
+class BinaryTree():
+    def __init__(self, value, left = None, right = None, parent = None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+        
+        
+def find_successor(tree, node):
+    result = []
+    find_successor_helper(tree, node, result)
+    if len(result) <= 1:
+        return None
+    else:
+        return result[1]
+
+
+def find_successor_helper(tree, node, result):
+    if tree is not None:
+        find_successor_helper(tree.left, node, result)
+        if len(result) == 1:
+            result.append(tree)
+        if tree.value == node.value:
+            result.append(node)
+        find_successor_helper(tree.right, node, result)
+        
+        
+def main():
+    tree = BinaryTree(1)
+    tree.left = BinaryTree(2)
+    tree.right = BinaryTree(3)
+    
+    tree.left.left = BinaryTree(4)
+    tree.left.right = BinaryTree(5)
+    
+    tree.left.left.left = BinaryTree(6)
+
+    node = BinaryTree(22)
+
+    found_node = find_successor(tree, node)
+    if found_node:
+        print(node.value)
+    else:
+        print('Nothing')
+    
+    
+if __name__ == "__main__":
+    main()
