fc9858ecc1131a54ca90a88f67f7ead58f865e35
Sergey Matveev <rfmatveev@gmail.com>
2022-08-16 09:39:13 -0500

    SmallestSubarrayWithGreaterSum solution

--- SmallestSubarrayWithGreaterSum.py
+++ SmallestSubarrayWithGreaterSum.py
@@ -1,14 +1,27 @@
-# Given an array of positive integers and a number ‘S,’ find the length of the smallest contiguous 
+# Given an array of positive integers and a number ‘S’, find the length of the smallest contiguous 
 # subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+# Input: [2, 1, 5, 2, 3, 2], S=10
+# Output: 2
+# Explanation: The smallest subarray with a sum greater than or equal to ‘10’ is [5, 2, 3].
 
 
 def smallest_subarray_sum(s, arr):
-  # TODO: Write your code here
-  return -1
+  l = 0
+  _sum = 0
+  result = float('inf')
+  for r in range(len(arr)):
+      _sum += arr[r]
+      while _sum >= s:
+          result = min(result, r - l + 1)
+          _sum -= arr[l]
+          l += 1
+  
+  return 0 if result == float('inf') else result
 
 
 def main():
-    arr = []
+    arr = [2, 1, 5, 2, 3, 2]
+    print(smallest_subarray_sum(10, arr))
     
     
 if __name__ == "__main__":
