7b93f21a35273182485d1d08e20a50a4412c3976
Sergey Matveev <rfmatveev@gmail.com>
2022-08-15 21:42:20 -0500

    Added SmallestSubarrayWithGreaterSum

---
+++ SmallestSubarrayWithGreaterSum.py
@@ -0,0 +1,15 @@
+# Given an array of positive integers and a number ‘S,’ find the length of the smallest contiguous 
+# subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+
+
+def smallest_subarray_sum(s, arr):
+  # TODO: Write your code here
+  return -1
+
+
+def main():
+    arr = []
+    
+    
+if __name__ == "__main__":
+    main()
