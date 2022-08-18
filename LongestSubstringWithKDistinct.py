de8577627edc3ff759f8e00f3cc46d5b06d8d0a4
Sergey Matveev <rfmatveev@gmail.com>
2022-08-18 08:57:45 -0500

    LongestSubstringWithKDistinct deleted wrong solution with set

--- LongestSubstringWithKDistinct.py
+++ LongestSubstringWithKDistinct.py
@@ -19,24 +19,7 @@
 # Output: 6
 # Explanation: The longest substring with no more than '10' distinct characters is "cbbebi".
 
-
 def longest_substring_with_k_distinct(str1, k):
-    used = set()
-    l = 0
-    max_length = 0
-    curr_length = 0
-    for r in range(len(str1)):
-        while len(used) > k:
-            used.remove(str1[l])
-            l += 1
-        used.add(str1[r])
-        if len(used) <= k:
-            curr_length += 1
-        max_length = max(max_length, curr_length)
-    return max_length
-
-
-def longest_substring_with_k_distinct_2(str1, k):
     used = {}
     l = 0
     max_length = 0
@@ -55,9 +38,8 @@ def longest_substring_with_k_distinct_2(str1, k):
 
 
 def main():
-    string1 = 'araaci'
+    string1 = 'aakjjjio'
     print(longest_substring_with_k_distinct(string1, 2))
-    print(longest_substring_with_k_distinct_2(string1, 2))
 
 
 if __name__ == '__main__':
