256ae136eb84318680181c0d28698dc1ecf57786
Sergey Matveev <rfmatveev@gmail.com>
2022-08-19 08:44:32 -0500

    LongestPalindromicSubstring.py added

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,11 @@
+def longestPalindromicSubstring(string):
+    # Write your code here.
+    pass
+
+
+def main():
+    myString = "abcdcba"
+    print((myString))
+    
+if __name__ == "__main__":
+    main()
