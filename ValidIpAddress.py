f3199520a359334f080d356091d0c4567eb7aa97
Sergey Matveev <rfmatveev@gmail.com>
2022-08-21 20:00:10 -0500

    ValidIpAddress added

---
+++ ValidIpAddress.py
@@ -0,0 +1,12 @@
+def validIPAddresses(string):
+    # Write your code here.
+    return []
+
+
+def main():
+    ip = ""
+    print(validIPAddresses(ip))
+    
+    
+if __name__ == "__main__":
+    main()
