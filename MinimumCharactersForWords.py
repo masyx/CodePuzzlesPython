1f088160dbf25e875aba09d51b4046822cb19ef9
Sergey Matveev <rfmatveev@gmail.com>
2022-08-24 09:29:46 -0500

    MinimumCharactersForWords my cumbersome solution :)

--- MinimumCharactersForWords.py
+++ MinimumCharactersForWords.py
@@ -1,10 +1,34 @@
 def minimumCharactersForWords(words):
-    # Write your code here.
-    return []
+    total_chars = {}
+    for word in words:
+        curr_chars = {}
+        for char in word:
+            if char not in curr_chars:
+                curr_chars[char] = 0
+            curr_chars[char] += 1
+        compare_chars(curr_chars, total_chars)
+    return dict_to_array(total_chars)
 
+
+def compare_chars(curr_chars, total_chars):
+    for key in curr_chars:
+        if key in total_chars:
+            if curr_chars[key] > total_chars[key]:
+                total_chars[key] = curr_chars[key]
+        else:
+            total_chars[key] = curr_chars[key]
+            
+def dict_to_array(dict):
+    array = []
+    for key in dict:
+        value = dict[key]
+        for i in range(value):
+            array.append(key)
+    return array
 
 def main():
     words = ["this", "that", "did", "deed", "them!", "a"]
+    #words = ["did", "deed"]
     print(minimumCharactersForWords(words))
     
     
