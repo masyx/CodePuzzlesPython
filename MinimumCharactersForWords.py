b6cf6e5622e583cbed0be424f48029083fe6cf9b
Sergey Matveev <rfmatveev@gmail.com>
2022-08-24 08:56:31 -0500

    MinimumCharactersForWords added

---
+++ MinimumCharactersForWords.py
@@ -0,0 +1,12 @@
+def minimumCharactersForWords(words):
+    # Write your code here.
+    return []
+
+
+def main():
+    words = ["this", "that", "did", "deed", "them!", "a"]
+    print(minimumCharactersForWords(words))
+    
+    
+if __name__ == "__main__":
+    main()
