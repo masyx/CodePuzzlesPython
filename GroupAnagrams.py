93427ee9f14bea26c6030acba52a15d2ddb40323
Sergey Matveev <rfmatveev@gmail.com>
2022-08-26 20:23:11 -0500

    GroupAnagrams added

---
+++ GroupAnagrams.py
@@ -0,0 +1,11 @@
+def groupAnagrams(words):
+    return
+
+def main():
+    # ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+    words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+    print(groupAnagrams(words))
+    
+    
+if __name__ == "__main__":
+    main()
