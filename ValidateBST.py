a9b4c294878a23356198f70a5ff3401485312f47
Sergey Matveev <rfmatveev@gmail.com>
2022-08-27 21:12:23 -0500

    ValidateBST added

---
+++ ValidateBST.py
@@ -0,0 +1,19 @@
+class BST():
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def validateBst(tree):
+    # Write your code here.
+    pass
+
+
+def main():
+    tree = BST(2)
+    print(validateBst(tree))
+    
+    
+if __name__ == "__main__":
+    main()
