1ef056edc3d753255ff9947ed06495bc36dcdff5
Sergey Matveev <rfmatveev@gmail.com>
2022-08-29 08:37:31 -0500

    MinHeightBst added

---
+++ MinHeightBst.py
@@ -0,0 +1,30 @@
+def minHeightBst(array):
+    pass
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+                
+                
+def main():
+    array = []
+    minHeightBst(array)
+    
+    
+if __name__ == "__main__":
+    main()
