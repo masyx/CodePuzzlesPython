877c18aee4fa91600f822e5d8d2e9594309b5e5b
Sergey Matveev <rfmatveev@gmail.com>
2022-08-29 21:27:51 -0500

    MinHeightBst n*log(n) solution

--- MinHeightBst.py
+++ MinHeightBst.py
@@ -1,5 +1,29 @@
+# O(n*log(n)) time | O(n) space, where n is the length of the array
 def minHeightBst(array):
-    pass
+    return construct_bst(array, None, 0, len(array) - 1)
+
+
+def construct_bst(array, bst, left_idx, right_idx):
+    if left_idx > right_idx:
+        return None
+    mean_idx = (left_idx + right_idx) // 2
+    if bst is None:
+        bst = BST(array[mean_idx])
+    else:
+        bst.insert(array[mean_idx])
+    construct_bst(array, bst, left_idx, mean_idx - 1)
+    construct_bst(array, bst, mean_idx + 1, right_idx)
+    return bst
+
+def find_closest_int_to_mean(array, mean):
+    closest_to_mean_idx = 0
+    original_diff = abs(mean - array[0])
+    for i in range(1, len(array)):
+        curr_difference = abs(mean - array[i])
+        if curr_difference < original_diff:
+            closest_to_mean_idx = i
+            original_diff = curr_difference
+    return closest_to_mean_idx
 
 
 class BST:
@@ -22,8 +46,9 @@ class BST:
                 
                 
 def main():
-    array = []
-    minHeightBst(array)
+    array = [1, 2, 5, 7, 10, 13, 14, 15, 22]
+    bst = minHeightBst(array)
+    print(bst)
     
     
 if __name__ == "__main__":
