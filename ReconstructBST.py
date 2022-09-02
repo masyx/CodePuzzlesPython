9032d51fd6a488aff59170f1ed475158b8dfaaf4
Sergey Matveev <rfmatveev@gmail.com>
2022-09-02 09:34:09 -0500

    ReconstructBST optimal O(n) solution

--- ReconstructBST.py
+++ ReconstructBST.py
@@ -22,9 +22,35 @@ def reconstructBst(preOrderTraversalValues):
     return BST(current_value, left, right)
 
 
+class TreeInfo:
+    def __init__(self, root_idx):
+        self.root_idx = root_idx
+        
+
+# O(n) time | O(n) space, where n is the length of the array
+def reconstructBst_2(preOrderTraversalValues):
+    tree_info = TreeInfo(0)
+    return reconstruct_bst_from_range(float("-inf"), float("inf"), preOrderTraversalValues, tree_info)
+
+
+def reconstruct_bst_from_range(lower_bound, upper_bound, 
+                               pre_order_traversal_values, current_subtree_info: TreeInfo):
+    if current_subtree_info.root_idx == len(pre_order_traversal_values):
+        return None
+    
+    root_value = pre_order_traversal_values[current_subtree_info.root_idx]
+    if root_value < lower_bound or root_value > upper_bound:
+        return None
+    
+    current_subtree_info.root_idx += 1
+    left = reconstruct_bst_from_range(lower_bound, root_value, pre_order_traversal_values, current_subtree_info)
+    right = reconstruct_bst_from_range(root_value, upper_bound, pre_order_traversal_values, current_subtree_info)
+    return BST(root_value, left, right)
+
+
 def main():
     pre_order_traversal_values = [10, 4, 2, 5, 17, 19]
-    tree = reconstructBst(pre_order_traversal_values)
+    tree = reconstructBst_2(pre_order_traversal_values)
     print(tree)
     
     
