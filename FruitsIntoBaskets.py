77c50dc4d77befc01434735865cc23a213db9616
Sergey Matveev <rfmatveev@gmail.com>
2022-09-03 18:11:43 -0500

    FruitsIntoBaskets added

---
+++ FruitsIntoBaskets.py
@@ -0,0 +1,12 @@
+def fruits_into_baskets(fruits):
+
+  return -1
+
+
+def main():
+    fruits = ['A', 'C', 'C', 'C', 'C', 'B', 'C', 'W', 'W', 'W', 'W']
+    print(fruits_into_baskets(fruits))
+    
+    
+if __name__ == "__main__":
+    main()
