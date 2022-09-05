a041a0bbfbfc76508913dead9324bfebc10ac822
Sergey Matveev <rfmatveev@gmail.com>
2022-09-04 21:35:02 -0500

    LongestStringWIthDistinctChars added

---
+++ LongestSubstringWithDistinctChars.py
@@ -0,0 +1,12 @@
+def non_repeat_substring(str):
+  # TODO: Write your code here
+  return -1
+
+
+def main():
+    string = 'aabcdd'
+    print(non_repeat_substring(string))
+    
+    
+if __name__ == "__main__":
+    main()
