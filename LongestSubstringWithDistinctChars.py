d9b8c38ee099206781f022c6080b376a038fe8d3
Sergey Matveev <rfmatveev@gmail.com>
2022-09-05 21:26:36 -0500

    LongestSubstringWithoutDuplication  separate file

--- LongestSubstringWithDistinctChars.py
+++ LongestSubstringWithDistinctChars.py
@@ -40,25 +40,6 @@ def non_repeat_substring_2(str1):
     max_length = max(max_length, window_end - window_start + 1)
   return max_length
 
-
-def longestSubstringWithoutDuplication(string):
-    l = 0
-    longest = 0
-    used_chars = {}
-    result = ''
-    for r in range(len(string)):
-        right_char = string[r]
-        used_chars[right_char] = used_chars.get(right_char, 0) + 1
-        while used_chars[right_char] > 1:
-            left_char = string[l]
-            used_chars[left_char] -= 1
-            l += 1
-        if r - l + 1 > longest:
-            longest = r - l + 1
-            result = string[l:r + 1]
-    return result
-
-
 def main():
     string = 'aabcacde'
     print(non_repeat_substring_2(string))
