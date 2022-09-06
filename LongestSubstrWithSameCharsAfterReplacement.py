9dd5cb13c1b5bb9528c32aabdb8fd744c56e22c9
Sergey Matveev <rfmatveev@gmail.com>
2022-09-06 08:55:19 -0500

    LongestSubstrWithSameCharsAfterReplacement added

---
+++ LongestSubstrWithSameCharsAfterReplacement.py
@@ -0,0 +1,13 @@
+def length_of_longest_substring(str1, k):
+  # TODO: Write your code here
+  return -1
+
+
+def main():
+    # abbcb
+    string = "abccde"
+    print(length_of_longest_substring(string,))
+    
+    
+if __name__ == "__main__":
+    main()
