7b2d03f5917177cbb3552cc443950d3659cd5ad6
Sergey Matveev <rfmatveev@gmail.com>
2022-09-10 12:03:31 -0500

    RemoveDuplicates solutions

--- Remove_duplicates.py
+++ Remove_duplicates.py
@@ -1,5 +1,22 @@
+#O(n) time | O(1) space
 def remove_duplicates(arr):
-    return
+    none_duplicate_pointer = 0
+    for i in range(1, len(arr)):
+        if arr[none_duplicate_pointer] != arr[i]:
+            arr[none_duplicate_pointer + 1] = arr[i]
+            none_duplicate_pointer += 1
+    return none_duplicate_pointer + 1
+
+#O(n) time | O(1) space
+def remove_duplicates_2(arr):
+  next_non_duplicate = 1
+  i = 0
+  while(i < len(arr)):
+    if arr[next_non_duplicate - 1] != arr[i]:
+      arr[next_non_duplicate] = arr[i]
+      next_non_duplicate += 1
+    i += 1
+  return next_non_duplicate
 
 
 def main():
