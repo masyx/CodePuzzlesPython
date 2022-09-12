e1b7c349ed3ed8100f28619fa4ea86c81201a529
Sergey Matveev <rfmatveev@gmail.com>
2022-09-12 07:44:49 -0500

    ThreeNumberSum training

--- A_Training.py
+++ A_Training.py
@@ -1,19 +1,27 @@
-def make_squares(arr):
-    squares = [0] * len(arr)
-    l, r = 0, len(arr) - 1
-    for i in reversed(range(len(arr))):
-        if abs(arr[r]) >= abs(arr[l]):
-            squares[i] = arr[r] ** 2
-            r -= 1
-        else:
-            squares[i] = arr[l] ** 2
-            l += 1
-    return squares
+# O()
+def search_triplets(arr):
+    triplets = []
+    arr.sort()
+    for i in range(len(arr)):
+        l = i + 1
+        r = len(arr) - 1
+        while l < r:
+            curr_sum = arr[i] + arr[l] + arr[r]
+            if curr_sum == 0:
+                triplets.append([arr[i], arr[l], arr[r]])
+                l += 1
+                r -= 1
+            elif curr_sum > 0:
+                r -= 1
+            else:
+                l += 1
+    return triplets
+
 
 
 def main():
     array = [-2, -1, 0, 2, 3]
-    print(make_squares(array))
+    print(search_triplets(array))
     
 if __name__ == "__main__":
     main()
