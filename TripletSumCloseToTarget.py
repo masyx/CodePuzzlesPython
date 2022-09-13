f74d07cffd43d38e42f9b649b2e7c04f923aac32
Sergey Matveev <rfmatveev@gmail.com>
2022-09-13 09:10:47 -0500

    TripletSumCloseToTarget.py solution

--- TripletSumCloseToTarget.py
+++ TripletSumCloseToTarget.py
@@ -15,12 +15,33 @@ Output: 0
 Explanation: The triplet [-3, 1, 2] has the closest sum to the target. """
 
 
+# O(n^2) time | O(n) space, which is required for sorting
 def triplet_sum_close_to_target(arr, target_sum):
-  # TODO: Write your code here
-  return -1
+    result = - 1
+    arr.sort()
+    smallest_diff = float('inf')
+    for i in range(len(arr) - 2):
+        l = i + 1
+        r = len(arr) - 1
+        while l < r:
+            current_sum = arr[i] + arr[l] + arr[r]
+            if current_sum == target_sum:
+                return current_sum
+            
+            current_diff = target_sum - current_sum
+            if abs(current_diff) < smallest_diff:
+                smallest_diff = abs(current_diff)
+                result = current_sum
+                
+            if current_diff > 0:
+                l += 1    # we need a triplet with a bigger sum
+            else:
+                r -= 1    # we need a triplet with a smaller sum
+    return result
 
 
 def main():
+    # [1,2,3,4,5] 10
     arr = [-2, 0, 1, 2]
     print(triplet_sum_close_to_target(arr, 2))
     
