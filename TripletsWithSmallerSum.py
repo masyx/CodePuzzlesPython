a69cad9f22f43173c36fe2903efb22d2b768ff5c
Sergey Matveev <rfmatveev@gmail.com>
2022-09-15 08:38:35 -0500

    TripletsWithSmallerSum added

---
+++ TripletsWithSmallerSum.py
@@ -0,0 +1,13 @@
+def triplet_with_smaller_sum(arr, target):
+  count = -1
+  # TODO: Write your code here
+  return count
+
+
+def main():
+    arr = [-1, 0, 2, 3]
+    print(triplet_with_smaller_sum(arr, 3))
+    
+    
+if __name__ == "__main__":
+    main()
