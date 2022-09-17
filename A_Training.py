e05efc47edeec354cc11e4963e9c962ce6935d52
Sergey Matveev <rfmatveev@gmail.com>
2022-09-17 18:45:14 -0500

    BFS training again

--- A_Training.py
+++ A_Training.py
@@ -1,37 +1,31 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
-    
-    def add_child(self, value):
-        self.children.append(Node(value))
-        
-    def breadth_first_search(self, result: list):
-        queue = [self]
-        while queue:
-            current_node = queue.pop(0)
-            result.append(current_node.value)
-            for child in current_node.children:
-                queue.append(child)
-        return result
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def traverse(root):
+  result = []
+  queue = [root]
+  while queue:
+    current = queue.pop(0)
+    result.append(current.val)
+    if current.left:
+        queue.append(current.left)
+    if current.right:
+        queue.append(current.right)
+  return result
 
 
 def main():
-    root = Node('A')
-    root.add_child('B')
-    root.add_child('C')
-    root.add_child('D')
-
-    root.children[0].add_child('E')
-    root.children[0].add_child('F')
-    root.children[0].children[1].add_child('I')
-    root.children[0].children[1].add_child('J')
-
-    root.children[2].add_child('G')
-    root.children[2].add_child('H')
-    root.children[2].children[0].add_child('K')
-    result = []
-    print(root.breadth_first_search(result))
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  
+  print(traverse(root))
 
 if __name__ == '__main__':
     main()
