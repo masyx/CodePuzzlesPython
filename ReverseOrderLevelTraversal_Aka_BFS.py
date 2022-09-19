26535f4a01791a012cc98bd5b58c1a97ebcd177b
Sergey Matveev <rfmatveev@gmail.com>
2022-09-19 09:15:56 -0500

    Reverse Order Traversal solutions

--- ReverseOrderLevelTraversal_Aka_BFS.py
+++ ReverseOrderLevelTraversal_Aka_BFS.py
@@ -8,8 +8,40 @@ class TreeNode:
 
 def traverse(root):
     result = deque()
-    # TODO: Write your code here
+    if root is None:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        current = queue.popleft()
+        result.appendleft(current.val)
+        if current.right:
+            queue.append(current.right)
+        if current.left:
+            queue.append(current.left)
     return result
+
+# O(n) time 
+# O(n) space  as we need to return a list containing the level order traversal. We will also need O(N) space for the queue. 
+# Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level), 
+# therefore we will need O(N) space to store them in the queue.
+def traverse_2(root):
+    result = deque()
+    if root is None:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        for _ in range(level_size):
+            current_node = queue.popleft(queue)
+            current_level.append(current_node.val)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        result.appendleft(current_level)
 
 def main():
     root = TreeNode(12)
