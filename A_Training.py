dbb860ea5f1e4638bb81f8be46661f44b977dcb1
Sergey Matveev <rfmatveev@gmail.com>
2022-09-26 09:24:58 -0500

    CaesarCipherEncryptor training

--- A_Training.py
+++ A_Training.py
@@ -1,38 +1,15 @@
-from collections import deque
-
-
-class TreeNode:
-    def __init__(self, val):
-      self.val = val
-      self.left, self.right = None, None
-
-
-def traverse(root):
-    result = []
-    if root is None:
-        return result
-    queue = deque()
-    queue.append(root)
-    while queue:
-      current = queue.popleft()
-      result.append(current.val)
-      if current.left:
-          queue.append(current.left)
-      if current.right:
-          queue.append(current.right)
-    return result
-
+def caesarCipherEncryptor(string, key):
+    result_chars = []
+    key = key % 26
+    for char in string:
+        new_letter_code = ord(char) + key 
+        if new_letter_code > 122:
+            new_letter_code = 96 + new_letter_code % 122
+        result_chars.append(chr(new_letter_code))
+    return "".join(result_chars)
 
 def main():
-    #root = None
-    root = TreeNode(12)
-    root.left = TreeNode(7)
-    root.right = TreeNode(1)
-    root.left.left = TreeNode(9)
-    root.right.left = TreeNode(10)
-    root.right.right = TreeNode(5)
-  
-    print(traverse(root))
+    print(caesarCipherEncryptor("abc", 52))
 
 if __name__ == '__main__':
     main()
