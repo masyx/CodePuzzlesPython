a0f03a2ed6426b62e31679001649d0a07737e705
Sergey Matveev <rfmatveev@gmail.com>
2022-10-03 08:06:16 -0500

    BinaryTreePathSum.py second solution

--- BinaryTreePathSum.py
+++ BinaryTreePathSum.py
@@ -4,40 +4,25 @@ class TreeNode:
     self.left = left
     self.right = right
 
+# O(n) time | O(n) space
+def has_path(root, sum):
+    if root is None:
+        return False
+    if sum == root.val and root.left is None and root.right is None:
+        return True
+    return has_path(root.left, sum - root.val) or has_path(root.right, sum - root.val)
 
 # O(n) time | O(n) space
-def has_path(root, sum, current_sum = 0):
+def has_path_2(root, sum, current_sum = 0):
     if root is None:
         return False
     current_sum += root.val
-    if current_sum == sum:
+    if current_sum == sum and root.left is None and root.right is None:
         return True
     return has_path(root.left, sum, current_sum) or has_path(root.right, sum, current_sum)
 
 
-def DFS(root, result = []):
-    result.append(root.val)
-    if root.left is not None:
-        DFS(root.left, result)
-    if root.right is not None:
-        DFS(root.right, result)
-    return result
-
-def DFS_stack(root, result = []):
-    stack = []
-    stack.append(root)
-    while stack:
-        current = stack.pop()
-        result.append(current.val)
-        if current.right is not None:
-            stack.append(current.right)
-        if current.left is not None:
-            stack.append(current.left)
-    return result
-
-
 def main():
-
   root = TreeNode(12)
   root.left = TreeNode(7)
   root.right = TreeNode(1)
@@ -46,7 +31,6 @@ def main():
   root.right.right = TreeNode(5)
   print("Tree has path: " + str(has_path(root, 23)))
   print("Tree has path: " + str(has_path(root, 16)))
-  #print(DFS(root))
 
 
 if __name__ == "__main__":
