49914b8be6685547929cd9f7ed3796dbd5341938
Sergey Matveev <rfmatveev@gmail.com>
2022-10-03 08:14:18 -0500

    BinaryTreeAllPathSum added

---
+++ BinaryTreeAllPathSum.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def find_paths(root, sum):
+    allPaths = []
+    # TODO: Write your code here
+    return allPaths
+
+
+def main():
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    sum = 23
+    print("Tree paths with sum " + str(sum) +
+            ": " + str(find_paths(root, sum)))
+
+
+if __name__ == "__main__":
+    main()
