e9e55c5ba272f123837ce46833f445cf05a8cd3f
Sergey Matveev <rfmatveev@gmail.com>
2022-10-06 09:10:16 -0500

    SumOfPathNumbers more elegant recursive solution

--- SumOfPathNumbers.py
+++ SumOfPathNumbers.py
@@ -11,20 +11,34 @@ class TreeNode:
 """The space complexity of the above algorithm will be O(N) in the worst case. 
 This space will be used to store the recursion stack. 
 The worst case will happen when the given tree is a linked list (i.e., every node has only one child)."""
-def find_sum_of_path_numbers(root):
+def find_sum_of_path_numbers_2(root):
     all_paths = []
-    find_sum_of_path_numbers_helper(root, '', all_paths)
+    find_sum_of_path_numbers_helper_2(root, '', all_paths)
     return sum(all_paths)
 
-def find_sum_of_path_numbers_helper(root, current_number, all_paths):
+def find_sum_of_path_numbers_helper_2(root, current_number, all_paths):
     if root is None:
         return
     current_number += str(root.val)
     if root.left is None and root.right is None:
         all_paths.append(int(current_number))
-    find_sum_of_path_numbers_helper(root.left, current_number, all_paths)
-    find_sum_of_path_numbers_helper(root.right, current_number, all_paths)
+    find_sum_of_path_numbers_helper_2(root.left, current_number, all_paths)
+    find_sum_of_path_numbers_helper_2(root.right, current_number, all_paths)
 
+
+# O(n) time | O(n) space 
+def find_sum_of_path_numbers(root):
+    return find_sum_of_path_numbers_helper(root, 0)
+
+def find_sum_of_path_numbers_helper(root, path_sum):
+    if root is None:
+        return 0
+    path_sum = path_sum * 10 + root.val
+    if root.left is None and root.right is None:
+        return path_sum
+    left = find_sum_of_path_numbers_helper(root.left, path_sum)
+    right = find_sum_of_path_numbers_helper(root.right, path_sum)
+    return left + right
 
 def main():
     root = TreeNode(1)
@@ -34,6 +48,7 @@ def main():
     root.right.left = TreeNode(6)
     root.right.right = TreeNode(5)
     print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
+    print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers_2(root)))
 
 
 main()
