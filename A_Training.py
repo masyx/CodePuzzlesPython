a2a7578f8e11906841adf862a4855821abab3a84
Sergey Matveev <rfmatveev@gmail.com>
2022-10-12 09:49:48 -0500

    ClassPhotos training

--- A_Training.py
+++ A_Training.py
@@ -1,15 +1,35 @@
-def lll(queries):
-    queries.sort()
-    waiting_times = [0]
-    previous_time = 0
-    for i in range(1, len(queries)):
-        waiting_times.append(queries[i - 1] + waiting_times[i - 1])
+def classPhotos(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+    red_shirts_in_front = redShirtHeights[0] > blueShirtHeights[0]
+    for i in range(len(redShirtHeights)):
+        if red_shirts_in_front:
+            if redShirtHeights[i] <= blueShirtHeights[i]:
+                return False
+        else:
+            if redShirtHeights[i] >= blueShirtHeights[i]:
+                return False
+    return True
 
-    return sum(waiting_times)
+
+def classPhotos(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+    if redShirtHeights[0] > blueShirtHeights[0]:
+        return arrange_photo(redShirtHeights, blueShirtHeights)
+    else:
+        return arrange_photo(blueShirtHeights, redShirtHeights)
+    
+def arrange_photo(tallerGroup, smallerGroup):
+    for i in range(len(tallerGroup)):
+        if tallerGroup[i] <= smallerGroup[i]:
+            return False
+    return True
 
 def main():
-    queries = [3, 2, 1, 2, 6] # 1, 2, 2, 3,6
-    print(lll(queries))
+    redShirts = [5, 8, 1, 3, 4]
+    blueShirts = [6, 9, 2, 4, 5]
+    print(classPhotos(redShirts, blueShirts))
 
 if __name__ == '__main__':
     main()
