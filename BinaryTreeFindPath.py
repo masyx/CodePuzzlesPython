d054947d21aabe9e1472fbd8702a7417bf72815b
Sergey Matveev <rfmatveev@gmail.com>
2022-10-12 21:39:43 -0500

    BinaryTreeFindPath my cumbersome solution

--- BinaryTreeFindPath.py
+++ BinaryTreeFindPath.py
@@ -1,13 +1,31 @@
+'''Given a binary tree and a number sequence, find if the sequence is present as 
+a root-to-leaf path in the given tree.'''
+
+import collections
+
+
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-
 def find_path(root, sequence):
-    # TODO: Write your code here
-    return False  
+    result = []
+    find_path_recursive(root, sequence, result)
+    if result:
+        return True
+    else:
+        return False
+
+def find_path_recursive(root, sequence, result, current = []):
+    if root is not None:
+        current.append(root.val)
+        find_path_recursive(root.left, sequence, result)
+        find_path_recursive(root.right, sequence, result)
+        if current == sequence:
+            result.append(f"Found sequence: {sequence}")
+        del current[-1]
 
 
 def main():
@@ -17,7 +35,7 @@ def main():
     root.left.left = TreeNode(1)
     root.right.left = TreeNode(6)
     root.right.right = TreeNode(5)
-
+    print("Tree has path sequence: " + str(find_path(root, [1, 0, 1])))
     print("Tree has path sequence: " + str(find_path(root, [1, 0, 7])))
     print("Tree has path sequence: " + str(find_path(root, [1, 1, 6])))
 
