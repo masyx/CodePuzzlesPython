f2c52b63ea048ff676359fa205e9da769292facf
Sergey Matveev <rfmatveev@gmail.com>
2022-10-14 09:23:50 -0500

    Training BinaryTreePathSum started

--- A_Training.py
+++ A_Training.py
@@ -1,48 +1,23 @@
-'''Given a binary tree and a number sequence, find if the sequence is present as 
-a root-to-leaf path in the given tree.'''
-
-import collections
-
-
 class TreeNode:
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, val, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
 
-
-def DFS_pre_order_traversal(root, bfs):
-    if root is None:
-        return
-    bfs.append(root.val)
-    DFS_pre_order_traversal(root.left, bfs)
-    DFS_pre_order_traversal(root.right, bfs)
-    
-def pre_order_traversal_iterative(root):
-    result = []
-    if root is None:
-        return result
-    stack = [root]
-    while stack:
-        current = stack.pop()
-        if current is None:
-            continue
-        result.append(current.val)
-        stack.append(current.right)
-        stack.append(current.left)
-    return result
+def has_path(root, s):
+    return
 
 
 def main():
-    root = TreeNode(1)
-    root.left = TreeNode(0)
-    root.right = TreeNode(1)
-    root.left.left = TreeNode(1)
-    root.right.left = TreeNode(6)
-    root.right.right = TreeNode(5)
-    bfs = []
-    DFS_pre_order_traversal(root, bfs)
-    print(bfs)
-    print(pre_order_traversal_iterative(root))
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Tree has path: " + str(has_path(root, 23)))
+  print("Tree has path: " + str(has_path(root, 16)))
+
 
 main()
