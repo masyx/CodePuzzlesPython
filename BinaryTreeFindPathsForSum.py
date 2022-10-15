0a3386bc6e0c1e48bb7cacbc3465441615fbf21c
Sergey Matveev <rfmatveev@gmail.com>
2022-10-14 21:33:41 -0500

    BinaryTreeFindPathsForSum added

---
+++ BinaryTreeFindPathsForSum.py
@@ -0,0 +1,28 @@
+"""Given a binary tree and a number 'S', find all paths in the tree 
+such that the sum of all the node values of each path equals 'S'. 
+Please note that the paths can start or end at any node but all paths 
+must follow direction from parent to child (top to bottom)."""
+
+class TreeNode:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+def count_paths(root, S):
+    return
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree has paths: " + str(count_paths(root, 11)))
+    
+    
+if __name__ == "__main__":
+    main()
