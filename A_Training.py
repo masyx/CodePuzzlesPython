bd743f2cd5bb6b21ec0185e300a139c7895c512e
Sergey Matveev <rfmatveev@gmail.com>
2022-10-14 21:49:11 -0500

    BinaryTreePathSum training done

--- A_Training.py
+++ A_Training.py
@@ -4,20 +4,24 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def has_path(root, s):
-    return
+
+def has_path(node, sum):
+    if node is None:
+      return False
+    if node.val == sum and node.left is None and node.right is None:
+      return True
+    return has_path(node.left, sum - node.val) or has_path(node.right, sum - node.val)
 
 
 def main():
-
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  print("Tree has path: " + str(has_path(root, 23)))
-  print("Tree has path: " + str(has_path(root, 16)))
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree has path: " + str(has_path(root, 23)))
+    print("Tree has path: " + str(has_path(root, 16)))
 
 
 main()
