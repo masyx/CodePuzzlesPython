525d96030d987659d2a16e0f5115e655bc56ece8
Sergey Matveev <rfmatveev@gmail.com>
2022-10-17 08:17:30 -0500

    BinaryTreeFindPathsForSum removed my wrong solution

--- BinaryTreeFindPathsForSum.py
+++ BinaryTreeFindPathsForSum.py
@@ -9,34 +9,17 @@ class TreeNode:
         self.left = left
         self.right = right
         
-# My solution: probably will be incorrect if tested intensively, just here for learning purpose
-# O(n^2) time | O(d) space
-def count_paths(root, S, result = []):
-    if root is None:
-        return
-    has_path(root, S, [], result)
-    count_paths(root.left, S, result)
-    count_paths(root.right, S, result)
-    return result
 
-def has_path(node, sum, curr_path, result):
-    if node is None:
-      return
-    curr_path.append(node.val)
-    if node.val == sum and node.left is None and node.right is None:
-      result.append(curr_path[:])
-    return has_path(node.left, sum - node.val, curr_path, result) \
-        or has_path(node.right, sum - node.val, curr_path, result)
 
 
 def main():
     root = TreeNode(12)
-    root.left = TreeNode(7)
+    root.left = TreeNode(6)
     root.right = TreeNode(1)
     root.left.left = TreeNode(4)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
-    print("Tree has paths: " + str(count_paths(root, 11)))
+    print("Tree has paths: " + str(count_paths(root, 18)))
     
     
 if __name__ == "__main__":
