e598378e5e02f5545858cbf34f957cbb186a7f36
Sergey Matveev <rfmatveev@gmail.com>
2022-10-19 08:51:36 -0500

    BinaryTreeFindPathsForSum optimal solution

--- BinaryTreeFindPathsForSum.py
+++ BinaryTreeFindPathsForSum.py
@@ -15,7 +15,7 @@ but for every node, we iterate the current path. The current path, in the worst 
 (in the case of a skewed tree). But, if the tree is balanced, then the current path will be 
 equal to the height of the tree, i.e., O(logN). So the best case of our algorithm will be O(N*logN)."""
 # O(n^2) time worse, O(n*log(n)) average| O(n) space
-def count_paths(root, S):
+def count_paths_2(root, S):
     return count_paths_recursive(root, S, [])
     
 def count_paths_recursive(node, S, current_path):
@@ -33,6 +33,27 @@ def count_paths_recursive(node, S, current_path):
     del current_path[-1]
     return paths_count
 
+# O(n) time | O(n) space
+def count_paths(root, target_sum):
+    # hash table to store prefix sum
+    map = {}
+    return count_paths_prefix_sum(root, target_sum, map, 0)
+
+def count_paths_prefix_sum(current_node, target_sum, prefixSums_map, current_path_sum):
+    if current_node is None:
+        return 0
+    path_count = 0
+    current_path_sum += current_node.val
+    if current_path_sum == target_sum:
+        path_count += 1
+    prefixSums_map[current_path_sum] = prefixSums_map.get(current_path_sum, 0) + 1
+    path_count += prefixSums_map.get(current_path_sum - target_sum, 0)
+    
+    path_count += count_paths_prefix_sum(current_node.left, target_sum, prefixSums_map, current_path_sum)
+    path_count += count_paths_prefix_sum(current_node.right, target_sum, prefixSums_map, current_path_sum)
+    
+    prefixSums_map[current_path_sum] = prefixSums_map[current_path_sum] - 1
+    return path_count
 
 def main():
     root = TreeNode(12)
