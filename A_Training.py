3573099952770308912da22b3cc8b327b664948c
Sergey Matveev <rfmatveev@gmail.com>
2022-10-24 08:37:00 -0500

    LinkedListCycleLength training

--- A_Training.py
+++ A_Training.py
@@ -1,48 +1,40 @@
-import collections
-
-
-class BinaryTree:
-    def __init__(self, value, left = None, right = None):
+class Node:
+    def __init__(self, value, next = None):
         self.value = value
-        self.left = left
-        self.right = right
+        self.next = next
 
-
-def branchSums(root):
-    result = []
-    branch_sums_recursive(root, 0, result)
-    return result
-
-def branch_sums_recursive(node, current_sum, array):
-    if node is None:
-        return
-    current_sum += node.value
-    if not node.left and not node.right:
-        array.append(current_sum)
-    branch_sums_recursive(node.left, current_sum, array)
-    branch_sums_recursive(node.right, current_sum, array)
-
-
-def initializeBinaryTree():
-    tree = BinaryTree(1)
-    tree.left = BinaryTree(2)
-    tree.right = BinaryTree(3)
-    
-    tree.left.left = BinaryTree(4)
-    tree.left.right = BinaryTree(5)
-    
-    tree.left.left.left = BinaryTree(8)
-    tree.left.left.right = BinaryTree(9)
-    
-    tree.right.left = BinaryTree(6)
-    tree.right.right = BinaryTree(7)
-    
-    return tree
+# Find where the slow pointer meets the fast pointer and loop again to find its length.
+# O(n) time, O(n) space
+def find_cycle_length(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            current = slow.next
+            cycle_length = 1
+            while current is not slow:
+                current = current.next
+                cycle_length += 1
+            return cycle_length
+    return 0
 
 
 def main():
-    tree = initializeBinaryTree()
-    print(branchSums(tree))
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList cycle length: " + str(find_cycle_length(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList cycle length: " + str(find_cycle_length(head)))
+    
+    head.next.next.next.next.next.next = head.next.next.next.next.next
+    print("LinkedList cycle length: " + str(find_cycle_length(head)))
 
 
 main()
