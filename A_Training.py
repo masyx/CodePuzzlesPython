3b15c5ac57723ea2d1a3f25b5a1f8f6fe9a456f9
Sergey Matveev <rfmatveev@gmail.com>
2022-10-25 07:46:52 -0500

    LinkedListStartOfCycle training

--- A_Training.py
+++ A_Training.py
@@ -1,16 +1,38 @@
+from __future__ import print_function
+
+
 class Node:
-    def __init__(self, value, next = None):
+    def __init__(self, value, next=None):
         self.value = value
         self.next = next
+  
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end='')
+            temp = temp.next
+        print()
 
-# Find where the slow pointer meets the fast pointer and loop again to find its length.
-# O(n) time, O(n) space
+
+def find_cycle_start(head):
+    slow = fast = head
+    cycle_length = find_cycle_length(head)
+    if cycle_length > 0:
+        for _ in range(cycle_length):
+            fast = fast.next
+        while True:
+            if slow == fast:
+                return slow
+            slow = slow.next
+            fast = fast.next
+    return None
+
 def find_cycle_length(head):
     slow = fast = head
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-        if slow == fast:
+        if fast == slow:
             current = slow.next
             cycle_length = 1
             while current is not slow:
@@ -27,14 +49,15 @@ def main():
     head.next.next.next = Node(4)
     head.next.next.next.next = Node(5)
     head.next.next.next.next.next = Node(6)
+
     head.next.next.next.next.next.next = head.next.next
-    print("LinkedList cycle length: " + str(find_cycle_length(head)))
+    print("LinkedList cycle start: " + str(find_cycle_start(head).value))
 
     head.next.next.next.next.next.next = head.next.next.next
-    print("LinkedList cycle length: " + str(find_cycle_length(head)))
-    
-    head.next.next.next.next.next.next = head.next.next.next.next.next
-    print("LinkedList cycle length: " + str(find_cycle_length(head)))
+    print("LinkedList cycle start: " + str(find_cycle_start(head).value))
+
+    head.next.next.next.next.next.next = head
+    print("LinkedList cycle start: " + str(find_cycle_start(head).value))
 
 
 main()
