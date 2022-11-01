fc58d30dcda352929cfe1005e21967e4a2901504
Sergey Matveev <rfmatveev@gmail.com>
2022-11-01 09:00:54 -0500

    InsertInterval added

---
+++ InsertInterval.py
@@ -0,0 +1,13 @@
+def insert(intervals, new_interval):
+  merged = []
+  # TODO: Write your code here
+  return merged
+
+
+def main():
+  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
+  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
+  print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
+
+
+main()
