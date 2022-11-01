57ff1cd3868451e3ebfc847b71c8c039b8aad557
Sergey Matveev <rfmatveev@gmail.com>
2022-11-01 07:59:57 -0500

    MergeIntervals more concise solution

--- MergeIntervals.py
+++ MergeIntervals.py
@@ -10,26 +10,34 @@ class Interval:
         print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
 
 # O(n log(n)) time | O(n) space
-def merge(intervals):
+def merge_2(intervals):
     intervals.sort(key=lambda interval: interval.start)
-    merged_intervals_counter = 0
-    merged = [intervals[merged_intervals_counter]]
+    merged = [intervals[0]]
     for idx in range(1, len(intervals)):
-        if intervals[idx].start <= merged[merged_intervals_counter].end:
-            if intervals[idx].end > merged[merged_intervals_counter].end:
-                merged[merged_intervals_counter].end = intervals[idx].end
-                continue
+        if intervals[idx].start <= merged[-1].end:
+            merged[-1].end = max(intervals[idx].end, merged[-1].end)
         else:
             merged.append(intervals[idx])
-            merged_intervals_counter += 1
     return merged
+
+# O(n log(n)) time | O(n) space
+def merge(intervals):
+    intervals.sort(key=lambda x: x.start)
+    merged = [intervals[0]]
+    for interval in intervals[1:]:
+        if interval.start <= merged[-1].end:
+            merged[-1].end = max(interval.end, merged[-1].end)
+        else:
+            merged.append(interval)
+    return merged
+
 
 # [[1,4], [2,5], [7,9]]
 def main():
-    # print("Merged intervals: ", end='')
-    # for i in merge([Interval(1, 4), Interval(2, 5), Interval(7, 9)]):
-    #     i.print_interval()
-    # print()
+    print("Merged intervals: ", end='')
+    for i in merge([Interval(1, 4), Interval(2, 5), Interval(7, 9)]):
+        i.print_interval()
+    print()
 
     print("Merged intervals: ", end='')
     for i in merge([Interval(6, 7), Interval(2, 4), Interval(5, 9)]):
