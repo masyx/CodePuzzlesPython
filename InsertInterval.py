d47dc23ca76fc1bb031d4592ea1aca6e868b8907
Sergey Matveev <rfmatveev@gmail.com>
2022-11-03 08:57:46 -0500

    InsertInterval training

--- InsertInterval.py
+++ InsertInterval.py
@@ -24,34 +24,32 @@ def insert_my(intervals, new_interval):
 
 # O(n) time | O(n) space
 def insert(intervals, new_interval):
-  merged = []
-  i, start, end = 0, 0, 1
-
-  # skip (and add to output) all intervals which end before the start of the new interval
-  while i < len(intervals) and intervals[i][end] < new_interval[start]:
-    merged.append(intervals[i])
-    i += 1
-
-  # merge all intervals that overlap with 'new_interval'
-  while i < len(intervals) and intervals[i][start] <= new_interval[end]:
-    new_interval[start] = min(intervals[i][start], new_interval[start])
-    new_interval[end] = max(intervals[i][end], new_interval[end])
-    i += 1
-
-  # insert the new_interval
-  merged.append(new_interval)
-
-  # add all the remaining intervals to the output
-  while i < len(intervals):
-    merged.append(intervals[i])
-    i += 1
-  return merged
+    merged = []
+    i, start, end = 0, 0, 1
+    
+    # skip all intervals that end before new interval
+    while i < len(intervals) and intervals[i][end] < new_interval[start]:
+        merged.append(intervals[i])
+        i += 1
+    
+    # merge new interval with intervals if end of new interval is more than start of current interval
+    while i < len(intervals) and new_interval[end] >= intervals[i][start]:
+        new_interval[start] = min(new_interval[start], intervals[i][start])
+        new_interval[end] = max(new_interval[end], intervals[i][end])
+        i += 1
+    
+    merged.append(new_interval)
+    
+    while i < len(intervals):
+        merged.append(intervals[i])
+        i += 1
+    return merged
 
 
 def main():
-    # print("Intervals after inserting the new interval: " + str(insert([[1, 2], [3, 4], [5, 8], [9, 15]] , [16, 17])))
-    # print("Intervals after inserting the new interval: " + str(insert([[1, 2], [3, 4], [5, 8], [9, 15]] , [2, 5])))
-    # print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
+    print("Intervals after inserting the new interval: " + str(insert([[1, 2], [3, 4], [5, 8], [9, 15]] , [16, 17])))
+    print("Intervals after inserting the new interval: " + str(insert([[1, 2], [3, 4], [5, 8], [9, 15]] , [2, 5])))
+    print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
     print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
     print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
 
