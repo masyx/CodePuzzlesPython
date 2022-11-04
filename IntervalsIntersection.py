00fc41c83cfa60b5e0fd3839751c330bbca6e8d6
Sergey Matveev <rfmatveev@gmail.com>
2022-11-04 08:47:16 -0500

    IntervalsIntersection added

---
+++ IntervalsIntersection.py
@@ -0,0 +1,12 @@
+def merge(intervals_a, intervals_b):
+  result = []
+  # TODO: Write your code here
+  return result
+
+
+def main():
+  print("Intervals Intersection: " + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]])))
+  print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
+
+
+main()
