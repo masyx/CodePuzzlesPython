1bd0dc186cec4d0ec5b591cb19e54b85e8123733
Sergey Matveev <rfmatveev@gmail.com>
2022-11-08 09:00:32 -0600

    ReverseList training

--- A_Training.py
+++ A_Training.py
@@ -1,17 +1,43 @@
-def nonConstructibleChange(coins):
-    coins.sort()
-    minimum_change = 0
-    for coin in coins:
-        if coin > minimum_change + 1:
-            break
-        minimum_change += coin
-    return minimum_change + 1
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+# O(n) time | O(n) space
+def reverse(head):
+    new_next = None
+    current = head
+    while current:
+        traverse_next = current.next
+        current.next = new_next
+        new_next = current
+        current = traverse_next
+    return new_next
 
 def main():
-    # 1 1 2 3 5 7 22
-    coins = [5, 7, 1, 1, 2, 3, 22]
-    print(nonConstructibleChange(coins))
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    
+    new_head = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end='')
+    new_head.print_list()
+    
+    reverse(new_head)
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
 
 
-if __name__ == "__main__":
-    main()
+main()
