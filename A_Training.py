a3b4740f56f07bcb243391dd70776609a0d0acf1
Sergey Matveev <rfmatveev@gmail.com>
2022-11-10 22:23:34 -0600

    FindClosestValueInBST training in progress

--- A_Training.py
+++ A_Training.py
@@ -1,43 +1,28 @@
-class Node:
-    def __init__(self, value, next=None):
+class BST:
+    def __init__(self, value, left=None, right=None):
         self.value = value
-        self.next = next
+        self.left = left
+        self.right = right
 
-    def print_list(self):
-        temp = self
-        while temp is not None:
-            print(temp.value, end=" ")
-            temp = temp.next
-        print()
-
-# O(n) time | O(n) space
-def reverse(head):
-    new_next = None
-    current = head
-    while current:
-        traverse_next = current.next
-        current.next = new_next
-        new_next = current
-        current = traverse_next
-    return new_next
+def findClosestValueInBst(tree, target):
+    return 
 
 def main():
-    head = Node(2)
-    head.next = Node(4)
-    head.next.next = Node(6)
-    head.next.next.next = Node(8)
-    head.next.next.next.next = Node(10)
-
-    print("Nodes of original LinkedList are: ", end='')
-    head.print_list()
+    tree = BST(10)
+    tree.left = BST(5)
+    tree.right = BST(13)
     
-    new_head = reverse(head)
-    print("Nodes of reversed LinkedList are: ", end='')
-    new_head.print_list()
+    # tree.left.left = BST(2)
+    # tree.left.right = BST(5)
     
-    reverse(new_head)
-    print("Nodes of original LinkedList are: ", end='')
-    head.print_list()
-
-
-main()
+    # tree.left.left.left = BST(1)
+    
+    # tree.right.left = BST(13)
+    # tree.right.right = BST(22)
+    
+    # tree.right.left.right = BST(14)
+    print(findClosestValueInBst(tree, 12))
+    
+    
+if __name__ == "__main__":
+    main()
