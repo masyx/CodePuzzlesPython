b752b7ce678415af2dc3edbc3ab55848d21b7b6e
Sergey Matveev <rfmatveev@gmail.com>
2022-11-18 21:27:06 -0600

    BST_Traversal recursive & iterative approaches

--- BST_traversal.py
+++ BST_traversal.py
@@ -7,41 +7,71 @@ class BST():
 
 # FOR ALL 3 FUNCTIONS: O(n) time | O(n) space, but if we didn't have to return an array
 # space complexity would be O(d), where d is the depth of the tree
-def dfs_in_order(root, visited):
-    if root:
-        visited.append(root.value)
-        dfs_in_order(root.left, visited)
-        dfs_in_order(root.right, visited)
+
+# Pre-Order Traversals
+def dfs_pre_order(node, visited: list):
+    if node:
+        visited.append(node.value)
+        dfs_pre_order(node.left, visited)
+        dfs_pre_order(node.right, visited)
     return visited
 
-
-def dfs_pre_order(root, visited: list): 
-    if root:
-        visited.append(root.value)
-        dfs_pre_order(root.left, visited)
-        dfs_pre_order(root.right, visited)
-    return visited
-
-def dfs_pre_order_iterative(root, visited: list):
-    stack = [root]
+def dfs_pre_order_iterative(node):
+    visited = []
+    stack = [node]
     while stack:
         current_node = stack.pop()
-        if current_node is None:
-            continue
-        visited.append(current_node.value)
-        stack.append(current_node.right)
-        stack.append(current_node.left)
+        if current_node:
+            visited.append(current_node.value)
+            stack.append(current_node.right) # add right child first
+            stack.append(current_node.left)
     return visited
 
-def dfs_post_order(tree, array):
-    if tree is not None:
-        dfs_post_order(tree.left, array)
-        dfs_post_order(tree.right, array)
-        array.append(tree.value)
-    return array
+
+# In-Order Traversals
+def dfs_in_order(node, visited: list):
+    if node:
+        dfs_in_order(node.left, visited)
+        visited.append(node.value)
+        dfs_in_order(node.right, visited)
+    return visited
+
+def dfs_in_order_iterative(node):
+    visited = []
+    stack = []
+    current_node = node
+    while stack or current_node:
+        while current_node:
+            stack.append(current_node)
+            current_node = current_node.left
+        current_node = stack.pop()
+        visited.append(current_node.value)
+        current_node = current_node.right
+    return visited
+
+
+# Post-Order Traversal
+def dfs_post_order(node, visited: list):
+    if node:
+        dfs_post_order(node.left, visited)
+        dfs_post_order(node.right, visited)
+        visited.append(node.value)
+    return visited
+
+def dfs_post_order_iterative(node):
+    visited = []
+    stack = [node]
+    while stack:
+        current_node = stack.pop()
+        if current_node:
+            visited.append(current_node.value)
+            stack.append(current_node.left)
+            stack.append(current_node.right)
+    return visited[::-1]
 
 
 def main():
+    # tree = None
     tree = BST(0)
     
     tree.left = BST(1)
@@ -53,14 +83,14 @@ def main():
     tree.right.left = BST(5)
     tree.right.right = BST(6)
 
-    print(f"In-order traversal recursive: {dfs_pre_order(tree, [])}")
+    print(f"Pre-order traversal recursive: {dfs_pre_order(tree, [])}")
+    print(f"Pre-order traversal iterative: {dfs_pre_order_iterative(tree)}\n")
+
+    print(f"In-order traversal recursive: {dfs_in_order(tree, [])}")
+    print(f"In-order traversal iterative: {dfs_in_order_iterative(tree)}\n")
     
-    # print(f"Pre-order traversal recursive: {dfs_pre_order(tree, [])}")
-    # print(f"Pre-order traversal iterative: {dfs_pre_order_iterative(tree, [])}")
-    # result.clear()
-    
-    # print(f"Post-order traversal recursive: {dfs_pre_order(tree, [])}")
-    # print(dfs_post_order(tree, result))
+    print(f"Post-order traversal recursive: {dfs_post_order(tree, [])}")
+    print(f"Post-order traversal iterative: {dfs_post_order_iterative(tree)}")
 
 
 if __name__ == '__main__':
