c651033c1eef428ce46ce8c7822f295b0453c6ec
Sergey Matveev <rfmatveev@gmail.com>
2022-12-10 16:58:29 -0600

    Training TreeNumberSum

--- A_Training.py
+++ A_Training.py
@@ -1,16 +1,26 @@
-def twoNumberSum(array, targetSum):
-    seen_numbers = {}
-    for idx, number in enumerate(array):
-        possible_number = targetSum - number
-        if possible_number in seen_numbers:
-            return [number, possible_number]
-        seen_numbers[number] = idx
-    return []
+# O(log n) time | O(1) space
+def threeNumberSum(array, targetSum):
+    array.sort()
+    result = []
+    for i in range(len(array)):
+        l = i + 1
+        r = len(array) - 1
+        while l < r:
+            curr_sum = array[i] + array[l] + array[r]
+            if curr_sum == targetSum:
+                result.append([array[i], array[l], array[r]])
+                l += 1
+                r -= 1
+            elif curr_sum > targetSum:
+                r -= 1
+            else:
+                l += 1
+    return [] 
 
 
 def main():
     nums = [2, 3, 5, 11, 32]
-    print(twoNumberSum(nums, 37))
+    print(threeNumberSum(nums, 19))
     
     
 if __name__ == "__main__":
