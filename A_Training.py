439f9740f8dc057ed2c54e6c9ea552c0f717162c
Sergey Matveev <rfmatveev@gmail.com>
2023-01-02 21:57:16 -0600

    InvertBinaryTree training

--- A_Training.py
+++ A_Training.py
@@ -1,3 +1,4 @@
+import collections
 class BinaryTree:
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -6,19 +7,35 @@ class BinaryTree:
 
 # O(n) time | O(h) space, where n is the number of the nodes
 # and 'h' is the hight of the tree
-class Solution:
-    def mirror_binary_tree(self, root):
-        def dfs_post_order(node):
-            if not node:
-                return
-            
-            dfs_post_order(node.left)
-            dfs_post_order(node.right)
-            
-            node.left, node.right = node.right, node.left
+def mirror_binary_tree_recursive(node):
+    if not node:
+        return
+    
+    node.left, node.right = node.right, node.left
+    
+    mirror_binary_tree_recursive(node.left)
+    mirror_binary_tree_recursive(node.right)
+    
+    return node
+
+def mirror_binary_tree_iterative(node):
+    if not node:
+        return
+    
+    queue = collections.deque([node])
+
+    while queue:
+        current_node = queue.pop()
+
+        current_node.left, current_node.right = \
+            current_node.right, current_node.left
         
-        dfs_post_order(root)
-        return root
+        if current_node.left:
+            queue.append(current_node.left)
+        if current_node.right:
+            queue.append(current_node.right)
+            
+    return node
 
 def main():
     tree = BinaryTree(1)
@@ -30,8 +47,9 @@ def main():
     tree.left.right = BinaryTree(4)
     
     
-    sol = Solution()
-    sol.mirror_binary_tree(tree)
+    #tree_1 = mirror_binary_tree_recursive(tree)
+    tree_2 = mirror_binary_tree_iterative(tree)
+    print()
     
     
 if __name__ == "__main__":
