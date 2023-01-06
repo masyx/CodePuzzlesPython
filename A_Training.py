d72a54ad5acc63d554e0b59f0a953a5a4c4a12b5
Sergey Matveev <rfmatveev@gmail.com>
2023-01-05 18:46:58 -0600

    linked_list module modifications

--- A_Training.py
+++ A_Training.py
@@ -1,12 +1,15 @@
-import LinkedList
-from LinkedList import Node
-from LinkedList import LinkedListClass
+import linked_list
+from linked_list import Node
+from linked_list import LinkedList
 
 def main():
-    ll = LinkedListClass()
+    ll = LinkedList()
     ll.head_node = Node(1)
-    LinkedList.insert_at_tail(ll, Node(2))
-    print()
+    ll.insert_at_tail(2)
+    ll.insert_at_tail(3)
+    ll.insert_at_tail(4)
+    ll.insert_at_tail(5)
+    print(ll.__str__())
     
     
 if __name__ == "__main__":
--- linked_list.py
+++ linked_list.py
@@ -3,9 +3,22 @@ class Node:
         self.value = value
         self.next = None
 
-class LinkedListClass:
+class LinkedList:
     def __init__(self):
         self.head_node = None
+        
+    def __str__(self):
+        head = self.get_head()
+        # linkedList = ""
+        # while head:
+        #     linkedList += f"{head.value} -> "
+        #     #linkedList.join(head) 
+        #     head = head.next
+        linkedList = []
+        while head:
+            linkedList.append(str(head.value))
+            head = head.next
+        return " -> ".join(linkedList)
         
     def get_head(self):
         return self.head_node
@@ -20,6 +33,13 @@ class LinkedListClass:
         new_node.next = self.head_node
         self.head_node = new_node
         return self.head_node
+    
+    def insert_at_tail(self, value):
+        current_node = self.get_head()
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = Node(value)
+
 
 @staticmethod
 def insert_at_tail(lst, value):
