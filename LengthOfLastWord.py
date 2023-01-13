c1b2fd8198349400713d220652f4de2def565d85
Sergey Matveev <rfmatveev@gmail.com>
2023-01-12 18:21:22 -0600

    LengthOfLastWord algorithm time & space complexity

--- LengthOfLastWord.py
+++ LengthOfLastWord.py
@@ -3,6 +3,9 @@ class Solution:
         list_of_strings = s.split()
         return len(list_of_strings[-1])
     
+    # O(n) time, where n is the length of the input string. In the worst case, the input string might contain only a single word
+    #       which implies that we would need to iterate through the entire string to obtain the result.
+    # O(1) space, constant memory is consumed, regardless of the input
     def lengthOfLastWord(self, s: str) -> int:
         # trimming the trailing spaces
         idx = len(s) - 1
