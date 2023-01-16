6d7d3b5bd655ba13a0ac1dcea8940d09b3d6f2e7
Sergey Matveev <rfmatveev@gmail.com>
2023-01-16 12:19:15 -0600

    MergeTwoSortedLists improvement and time complexity correction

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -1,21 +1,27 @@
-# O(n) time, where n is length of the longest list
+# O(n + m ) time
 # O(n + m) space
 def merge_lists(lst1, lst2):
-    idx_1 = idx_2 = 0
-    result = []
+    idx_1 = idx_2 = idx_res = 0
+    
+    result = [i for i in range(len(lst1) + len(lst2))]
+    
     while idx_1 < len(lst1) and idx_2 < len(lst2):
         if lst1[idx_1] < lst2[idx_2]:
-            result.append(lst1[idx_1])
+            result[idx_res] = lst1[idx_1]
             idx_1 += 1
+            idx_res += 1
         else:
-            result.append(lst2[idx_2])
+            result[idx_res] = lst2[idx_2]
             idx_2 += 1
+            idx_res += 1
     while idx_1 < len(lst1):
-        result.append(lst1[idx_1])
+        result[idx_res] = lst1[idx_1]
         idx_1 += 1
+        idx_res += 1
     while idx_2 < len(lst2):
-        result.append(lst2[idx_2])
+        result[idx_res] = lst2[idx_2]
         idx_2 += 1
+        idx_res += 1
     return result
 
 
