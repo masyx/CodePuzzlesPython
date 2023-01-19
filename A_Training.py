f8686239dbd0467fbf50fd6bf14339f517fa6cb2
Sergey Matveev <rfmatveev@gmail.com>
2023-01-18 19:06:52 -0600

    TwoNumberSum training

--- A_Training.py
+++ A_Training.py
@@ -1,18 +1,32 @@
-import linked_list
-from linked_list import Node
-from linked_list import LinkedList
+# O(n) time | O(n) space
+def find_sum_best(lst, sum):
+    seen = set()
+    for number in lst:
+        possible_number = sum - number
+        if possible_number in seen:
+            return [number, possible_number]
+        seen.add(number)
+    return []
+
+# O(n log(n)) time | O(1) space
+def find_sum(lst: list, sum):
+    lst.sort()
+    l = 0
+    r = len(lst) - 1
+    while l < r:
+        current_sum = lst[l] + lst[r]
+        if current_sum == sum:
+            return [lst[l], lst[r]]
+        if current_sum > sum:
+            r -= 1
+        else:
+            l += 1
+    return []
 
 def main():
-    ll = LinkedList()
-    ll.head_node = Node(1)
-    ll.insert_at_tail(2)
-    ll.insert_at_tail(3)
-    ll.insert_at_tail(4)
-    ll.insert_at_tail(5)
-    ll.insert_at_tail(6)
-    ll.insert_at_tail(7)
-
-    print(ll.middle())
+    lst = [1,21,3,14,5,60,7,6]
+    k = 81
+    print(find_sum_best(lst, k))
     
     
     
