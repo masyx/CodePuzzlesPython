6c95f4fa014558304d7fbae38293c35cf2910732
Sergey Matveev <rfmatveev@gmail.com>
2023-01-27 21:11:48 -0600

    FirstNonRepeatingInteger optimal solution

--- FirstNonRepeatingInteger.py
+++ FirstNonRepeatingInteger.py
@@ -1,11 +1,12 @@
-def find_first_unique(lst: list):
+# O(n^2) time | O(1) space
+def find_first_unique_BF_1(lst: list):
     for elem in lst:
         if lst.count(elem) == 1:
             return elem
     return None
 
-
-def first_unique_int(lst):
+# O(n^2) time | O(1) space
+def find_first_unique_BF_2(lst):
     for i in range(len(lst)):
         is_unique = True
         for j in range(len(lst)):
@@ -16,17 +17,29 @@ def first_unique_int(lst):
             return lst[i]
     return None
 
+# O(n) time | O(n) space
+def find_first_unique(lst: list):
+    counts = {}
+    for elem in lst:
+        counts[elem] = counts.get(elem, 0) + 1
+    # for key, value in counts.items():
+    #     if value == 1:
+    #         return key
+    for elem in lst:
+        if counts[elem] == 1:
+            return elem
+    return None
 
 def main():
+    print(find_first_unique_BF_1([9,2,3,3,9,6]))
+    print(find_first_unique_BF_1([9,2,3,2,6,6]))
+    print(find_first_unique_BF_1([4,5,1,5,4]))
+    print(find_first_unique_BF_1([0,0]))
+    print()
     print(find_first_unique([9,2,3,3,9,6]))
     print(find_first_unique([9,2,3,2,6,6]))
     print(find_first_unique([4,5,1,5,4]))
     print(find_first_unique([0,0]))
-    print()
-    print(first_unique_int([9,2,3,3,9,6]))
-    print(first_unique_int([9,2,3,2,6,6]))
-    print(first_unique_int([4,5,1,5,4]))
-    print(first_unique_int([0,0]))
     
     
     
