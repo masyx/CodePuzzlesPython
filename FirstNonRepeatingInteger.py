3c492ecaa8139fb682784ffa21bdbd9e28f45b15
Sergey Matveev <rfmatveev@gmail.com>
2023-01-27 20:09:14 -0600

    FirstNonRepeatingInteger correct brute force solutions

--- FirstNonRepeatingInteger.py
+++ FirstNonRepeatingInteger.py
@@ -1,21 +1,32 @@
-def find_first_unique(lst):
+def find_first_unique(lst: list):
+    for elem in lst:
+        if lst.count(elem) == 1:
+            return elem
+    return None
+
+
+def first_unique_int(lst):
     for i in range(len(lst)):
-        j = i + 1
-        while j < len(lst):
-            if lst[i] == lst[j]:
+        is_unique = True
+        for j in range(len(lst)):
+            if i != j and lst[i] == lst[j]:
+                is_unique = False
                 break
-            j += 1
-        if j == len(lst):
+        if is_unique:
             return lst[i]
-    return -1
+    return None
 
 
 def main():
     print(find_first_unique([9,2,3,3,9,6]))
-    
     print(find_first_unique([9,2,3,2,6,6]))
     print(find_first_unique([4,5,1,5,4]))
-    print(find_first_unique([0,0,1]))
+    print(find_first_unique([0,0]))
+    print()
+    print(first_unique_int([9,2,3,3,9,6]))
+    print(first_unique_int([9,2,3,2,6,6]))
+    print(first_unique_int([4,5,1,5,4]))
+    print(first_unique_int([0,0]))
     
     
     
