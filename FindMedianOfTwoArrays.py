7c5f81cb67e57484559fa9770de3205489ed9a63
Sergey Matveev <rfmatveev@gmail.com>
2023-01-29 22:45:12 -0600

    FindMedianOfTwoArrays added

---
+++ FindMedianOfTwoArrays.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def findMedianOfTwoArrays(num1: List[str], num2: List[int]) -> List[int]:
+    return 'Lol'
+
+
+def main():
+    l1 = [1, 3]
+    l2 = [2]
+    print(findMedianOfTwoArrays(l1, l2))
+    
+    
+if __name__ == "__main__":
+    main()
