14a87bde6673d6e654ce427a3aa80c00027f0336
Sergey Matveev <rfmatveev@gmail.com>
2023-01-30 23:37:56 -0600

    FindMedianOfTwoArrays linear solution

--- FindMedianOfTwoArrays.py
+++ FindMedianOfTwoArrays.py
@@ -1,12 +1,28 @@
 from typing import List
 
+# O(n) time | O(n + m) space
 def findMedianOfTwoArrays(num1: List[str], num2: List[int]) -> List[int]:
-    return 'Lol'
+    resultList = [0] * (len(num1) + len(num2))
+    i, j, k = 0, 0, 0
+    while i != len(num1) or j != len(num2):
+        if j == len(num2) or (i < len(num1) and num1[i] < num2[j]):
+            resultList[k] = num1[i]
+            i += 1
+        else:
+            resultList[k] = num2[j]
+            j += 1
+        k += 1
+
+    median_idx = (len(resultList) - 1) // 2
+    if len(resultList) & 1 == 1:
+        return  resultList[median_idx]
+    else:
+        return (resultList[median_idx] + resultList[median_idx + 1]) / 2.0
 
 
 def main():
-    l1 = [1, 3]
-    l2 = [2]
+    l1 = [1, 3, 4]
+    l2 = [2, 5]
     print(findMedianOfTwoArrays(l1, l2))
     
     
