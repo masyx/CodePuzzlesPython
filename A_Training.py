90973f15acb2c48964bef38dfb90ba6e96775d1c
Sergey Matveev <rfmatveev@gmail.com>
2023-10-01 23:07:42 -0500

    is_valid_parenthesis training

--- A_Training.py
+++ A_Training.py
@@ -1,15 +1,20 @@
-def main():
-    return
+class Solution(object):
+    def is_valid_parenthesis(string):
+        # The stack to keep the track of opening brackets
+        stack = []
+        mapping = {")": "(", "]": "[", "}": "{"}
+        
+        for char in string:
+            if stack and char in mapping:
+                top_element = stack.pop()
+                if top_element != mapping[char]:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
 
-# [2,3,8,10]
-def two_sum(nums, target):
-    seen = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
-    return []
+def main():
+    print(Solution.is_valid_parenthesis("())(){}"))
 
 
 if __name__ == "__main__":
