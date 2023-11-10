05d16a7a1a0d78bdf433b343b2c2a17a0b03ffbd
Sergey Matveev <rfmatveev@gmail.com>
2023-11-10 16:39:33 -0600

    ThreeSum training

--- A_Training.py
+++ A_Training.py
@@ -1,18 +1,25 @@
-class Solution(object):
-    def is_valid_parenthesis(string):
-        stack = []
-        mapping = {")": "(", "}": "{", "]": "["}
-        for char in string:
-            if char not in mapping:
-                stack.append(char)
-            else:
-                top_element = stack.pop() if stack else '#'
-                if mapping[char] != top_element:
-                    return False
-        return not stack
-
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int], target) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(0, len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == target:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                if nums[i] + nums[j] + nums[k] > target:
+                    k -= 1
+                if nums[i] + nums[j] + nums[k] < target:
+                    j += 1
+        return result if result else None
+            
 def main():
-    print(Solution.is_valid_parenthesis("()[(]){}"))
+    list1 = [8, 1, 10, 1, 2, 4]
+    print(Solution().threeSum(list1, 11))
 
 
 if __name__ == "__main__":
