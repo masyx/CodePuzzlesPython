c654e5293f45a549e9bcb04729e8987d73204f1b
Sergey Matveev <rfmatveev@gmail.com>
2024-01-04 00:24:51 -0600

    MyStack my custom data structure

---
+++ MyCustomDataStructures/MyStack.py
@@ -0,0 +1,28 @@
+# The operation on the stack: push, peek, pop, is_empty, stack_size
+
+class MyStack:
+    def __init__(self):
+        self.stack_size = 0
+        self.elements = []
+        
+    def is_empty(self):
+        return self.stack_size == 0
+    
+    def push(self, element):
+        self.stack_size += 1
+        self.elements.append(element)
+        
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.elements[-1]
+    
+    def pop(self):
+        if self.is_empty():
+            return None
+        self.stack_size -= 1
+        return self.elements.pop()
+    
+    @property
+    def size(self):
+        return self.stack_size
