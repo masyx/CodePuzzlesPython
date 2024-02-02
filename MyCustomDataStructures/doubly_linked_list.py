88e19606f38c1205387c1e080fe28380ac1652cb
Sergey Matveev <rfmatveev@gmail.com>
2024-02-02 17:10:15 -0600

    Added insert_tail & remove_head methods to doubly_linked type

--- MyCustomDataStructures/doubly_linked_list.py
+++ MyCustomDataStructures/doubly_linked_list.py
@@ -8,16 +8,53 @@ class DoublyLinkedList:
         
     def get_head(self):
         return self.head if self.head else None
-        
-    def is_empty(self):
-        return self.head is None
 
-    def get_head(self):
-        return self.head
+    def get_tail(self):
+        return self.tail if self.tail else None
     
     def is_empty(self):
-        return self.size == 0
+        return self.head is None
+    
+    def insert_tail(self, element):
+        new_node = Node(element)
+        if self.is_empty():
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+            
+        self.length += 1
+        return self.tail.data
+    
+    def remove_head(self):
+        if self.is_empty():
+            return None
         
-    def append(self, element):
-        self.tail.next = Node(element)
-        self.tail = self.tail.next
+        node_to_remove = self.head
+        
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.previous = None
+            node_to_remove.next = None
+        
+        self.length -= 1
+        return node_to_remove.data
+    
+    def __str__(self):
+        val=""
+        if(self.is_empty()):
+            return ""
+        temp = self.head
+        val = "[" + str(temp.data) + ", "
+        temp = temp.next_element
+
+        while (temp.next_element):
+            val = val + str(temp.data) + ", "
+            temp = temp.next_element
+        val = val + str(temp.data) + "]"
+        return val
+
