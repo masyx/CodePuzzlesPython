4e4aa0017f984b0b6630f75a5534b6d5533daf2c
Sergey Matveev <rfmatveev@gmail.com>
2024-02-13 18:50:16 -0600

    BFS traversal study

---
+++ DataStructuresCourse_Educative/Problems/BfsTraversal.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('DataStructuresCourse_Educative\MyCustomDataStructures')
+from my_stack import MyStack
+from my_queue import MyQueue
+
+
+def bfs_traversal(g, source):
+
+    # Replace this placeholder return statement with your code
+    return ""
