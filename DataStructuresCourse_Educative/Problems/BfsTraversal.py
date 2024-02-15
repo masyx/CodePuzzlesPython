fc8d9b35a631c688cc77596c66ad5d16cd307a14
Sergey Matveev <rfmatveev@gmail.com>
2024-02-14 23:46:11 -0600

    Learning Graph BFS, for tracking purpose

--- DataStructuresCourse_Educative/Problems/BfsTraversal.py
+++ DataStructuresCourse_Educative/Problems/BfsTraversal.py
@@ -2,9 +2,62 @@ import sys
 sys.path.append('DataStructuresCourse_Educative\MyCustomDataStructures')
 from my_stack import MyStack
 from my_queue import MyQueue
+from graph import Graph
 
+
+def bfs_traversal_helper(g, source, visited):
+    result = ""
+    # Create Queue(implemented in previous lesson) for Breadth First Traversal
+    # and enqueue source in it
+    queue = MyQueue()
+    queue.enqueue(source)
+    visited[source] = True # Mark as visited
+    # Traverse while queue is not empty
+    while not queue.is_empty():
+        # Dequeue a vertex/node from queue and add it to result
+        current_node = queue.dequeue()
+        result += str(current_node)
+        # Get adjacent vertices to the current_node from the list,
+        # and if they are not already visited then enqueue them in the Queue
+        temp = g.array[current_node].head_node
+        while temp is not None:
+            if not visited[temp.data]:
+                queue.enqueue(temp.data)
+                visited[temp.data] = True  # Visit the current Node
+            temp = temp.next
+    return result, visited
 
 def bfs_traversal(g, source):
+    result = ""
+    num_of_vertices = g.vertices
+    if num_of_vertices == 0:
+        return result
+    # A list to hold the history of visited nodes
+    # Make a node visited whenever you enqueue it into queue
+    visited = []
+    for i in range(num_of_vertices):
+        visited.append(False)
+    # Start from source
+    result, visited = bfs_traversal_helper(g, source, visited)
+    # visit remaining nodes
+    for i in range(num_of_vertices):
+        if not visited[i]:
+            result_new, visited = bfs_traversal_helper(g, i, visited)
+            result += result_new
+    return result
 
-    # Replace this placeholder return statement with your code
-    return ""
+
+
+if __name__ == "__main__" :
+    g = Graph(4)
+    num_of_vertices = g.vertices
+    if num_of_vertices == 0:
+        print("Graph is empty")
+    elif num_of_vertices < 0:
+        print("Graph cannot have negative vertices")
+    else:
+        g.add_edge(0, 1)
+        g.add_edge(0, 2)
+        g.add_edge(1, 3)
+        g.add_edge(2, 3)
+        print(bfs_traversal(g, 0))
