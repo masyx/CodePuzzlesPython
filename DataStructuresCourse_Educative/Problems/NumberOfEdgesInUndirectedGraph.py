b6db5e5555854fde5d968e16c17fb8acfa73a16e
Sergey Matveev <rfmatveev@gmail.com>
2024-03-31 23:49:11 -0500

    Number of edges in an undirected graph

--- DataStructuresCourse_Educative/Problems/NumberOfEdgesInUndirectedGraph.py
+++ DataStructuresCourse_Educative/Problems/NumberOfEdgesInUndirectedGraph.py
@@ -4,6 +4,13 @@ sys.path.append('DataStructuresCourse_Educative\MyCustomDataStructures')
 from graph import Graph
 
 
+def num_edges(g: Graph):
+    count = 0
+    for i in range(g.vertices):
+        count += g.adj_list[i].length() 
+    return count // 2
+
+
 
 if __name__ == "__main__" :
     g = Graph(8)
@@ -14,12 +21,15 @@ if __name__ == "__main__" :
         print("Graph cannot have negative vertices")
     else:
         g.add_edge(0, 2)
-        g.add_edge(1, 2)
-        g.add_edge(3, 0)
-        g.add_edge(3, 1)
-        g.add_edge(1, 4)
-        g.add_edge(1, 5)
-        g.add_edge(4, 5)
+        g.add_edge(0, 5)
+        g.add_edge(2, 3)
+        g.add_edge(2, 4)
+        g.add_edge(5, 3)
+        g.add_edge(5, 6)
         g.add_edge(3, 6)
-        g.add_edge(2, 7)
-        g.add_edge(5, 8)
+        g.add_edge(6, 7)
+        g.add_edge(6, 8)
+        g.add_edge(6, 4)
+        g.add_edge(7, 8)
+        
+    print(num_edges(g))
