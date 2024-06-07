a2e53517af4b6b957ae25d93b3b6b3859de21f2b
Sergey Matveev <rfmatveev@gmail.com>
2024-06-07 11:56:35 -0500

    SortColors generic counting sorting

--- SortColors.py
+++ SortColors.py
@@ -15,10 +15,24 @@ def sort_colors_counting(colors):
             index += 1
     return colors
 
+def sort_colors_counting_generic(nums):
+    counts = {}
+
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+    
+    counts = dict(sorted(counts.items()))
+    index = 0
+    for key in counts.keys():
+        for _ in range(counts[key]):
+            nums[index] = key
+            index += 1
+    return nums
+
 def main():
-    colors = [2, 1, 0]
-    #colors = [2, 1, 0, 2, 1, 0, 0, 1, 1, 2, 2, 1, 1, 0]
-    print(sort_colors_counting(colors))
+    #colors = [2, 1, 0, 3]
+    colors = [2, 1, 4, 2, 1, 4, 4, 1, 1, 2, 2, 1, 1, 4]
+    print(sort_colors_counting_generic(colors))
     
 
 if __name__ == "__main__":
