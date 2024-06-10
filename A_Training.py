e25cb21ca7ba9fde21b98e341d66a3ec83801c4c
Sergey Matveev <rfmatveev@gmail.com>
2024-06-10 09:40:29 -0500

    IsPalindrome started

--- A_Training.py
+++ A_Training.py
@@ -1,77 +1,16 @@
-from collections import deque
+def is_palindrome(word):
+    start = 0
+    end = len(word) - 1
+    removed_chars = 0
+    while start < end:
+        if word[start] != word[end]:
+            if removed_chars > 1:
+                return False
+            else:
+                
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.length = 0
-        
-    def append(self, value):
-        if not self.head:
-            self.head = Node(value)
-            self.length += 1
-            return
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = Node(value)
-        self.length += 1
-        
-        
-    def add_at_head(self, value):
-        new_node = Node(value)
-        new_node.next = self.head
-        self.head = new_node
-        self.length += 1
-        
-    def __str__(self) -> str:
-        array = [None for _ in range(self.length)]
-        curr = self.head
-        counter = 0
-        while curr:
-            array[counter] = str(curr.data)
-            counter += 1
-            curr = curr.next
-        return ", ".join(array)
-
-class Graph:
-    def __init__(self, vertices: int):
-        self.vertices = vertices
-        self.adjacency_list = [LinkedList() for _ in range(vertices)]
-        
-    def add_edge(self, source, destination):
-        if 0 <= source < self.vertices and 0 <= destination < self.vertices:
-            self.adjacency_list[source].add_at_head(destination)
-            
-    def __str__(self):
-        result = []
-        for i in range(self.vertices):
-            line = f"Vertex {i}: {self.adjacency_list[i]}"
-            result.append(line)
-        return "\n".join(result)
+    return True
 
 if __name__ == "__main__":
-    ll = LinkedList()
-    ll.append(5)
-    ll.append(10)
-    ll.add_at_head(3)
-    print(ll)
-    
-    graph = Graph(6)
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 5)
-    graph.add_edge(1, 2)
-    graph.add_edge(1, 3)
-    graph.add_edge(2, 0)
-    graph.add_edge(2, 5)
-    graph.add_edge(2, 4)
-    print(graph)
-    
-    
-    
-    
- 
+    word = "abcbma"
+    print(is_palindrome(word))
