22d40da77b46ec0085a8266992c49fa149ad3e65
Sergey Matveev <rfmatveev@gmail.com>
2024-06-10 09:34:07 -0500

    Minor but important fix for ReverseWordsInString.py

--- ReverseWordsInString_2.py
+++ ReverseWordsInString_2.py
@@ -5,7 +5,7 @@ def reverse_words(sentence):
         start = 0
         end = len(lst) - 1
         while start < end:
-            lst[start], lst[end] == lst[end], lst[start]
+            lst[start], lst[end] = lst[end], lst[start]
             start += 1
             end -= 1
         return lst
