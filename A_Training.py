b3e809be133082f7aba31bbe6e4414da41b74376
Sergey Matveev <rfmatveev@gmail.com>
2024-06-12 07:51:49 -0500

    HappyNumber training

--- A_Training.py
+++ A_Training.py
@@ -1,26 +1,11 @@
-def is_palindrome(word):
-    start = 0
-    end = len(word) - 1
-    removed_chars = 0
-    while start < end:
-        if word[start] != word[end]:
-            if removed_chars > 0:
-                return False
-            else:
-                if word[start] == word[end - 1]:
-                    end -= 1
-                    removed_chars += 1
-                elif word[start + 1] == word[end]:
-                    start += 1
-                    removed_chars += 1
-                else:
-                    return False
-
-        start += 1
-        end -= 1
-
+def is_happy_number(n):
+    
+    
+    
     return True
 
+
+
 if __name__ == "__main__":
-    word = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVgVUTSRQPONMLKLJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba"
-    print(is_palindrome(word))
+    number = 2
+    print(f"Is {number} a happy number? - {is_happy_number(number)}")
