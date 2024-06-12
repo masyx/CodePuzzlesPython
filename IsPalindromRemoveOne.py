90436b5999b7503fdd54a8670f7dfdd200c565aa
Sergey Matveev <rfmatveev@gmail.com>
2024-06-12 07:09:39 -0500

    IsPalindromeRemoveOne solved

--- IsPalindromRemoveOne.py
+++ IsPalindromRemoveOne.py
@@ -1,24 +1,28 @@
+'''
+Write a function that takes a string as input and checks whether it can be a valid palindrome 
+by removing at most one character from it.
+'''
+
+# O(n) time | O(1) space
 def is_palindrome(word):
+    def is_palindrome_helper(word, start, end):
+        while start < end:
+            if word[start] != word[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
+    
     start = 0
     end = len(word) - 1
-    removed_chars = 0
+    
     while start < end:
         if word[start] != word[end]:
-            if removed_chars > 0:
-                return False
-            else:
-                if word[start] == word[end - 1]:
-                    end -= 1
-                    removed_chars += 1
-                elif word[start + 1] == word[end]:
-                    start += 1
-                    removed_chars += 1
-                else:
-                    return False
-
+            return is_palindrome_helper(word, start, end - 1) or \
+                is_palindrome_helper(word, start + 1, end)
         start += 1
         end -= 1
-
+    
     return True
 
 if __name__ == "__main__":
