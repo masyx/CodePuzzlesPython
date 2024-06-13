5a3466be90e0662bb300dbc1fd4556dd598174a4
Sergey Matveev <rfmatveev@gmail.com>
2024-06-13 08:53:44 -0500

    IsHappyNumber training my solution

--- A_Training.py
+++ A_Training.py
@@ -1,9 +1,21 @@
 def is_happy_number(n):
-    
-    
-    
-    return True
-
+    numbers_seen = set()
+    def is_happy_number_helper(n):
+        numbers_seen.add(n)
+        
+        result = 0
+        while n > 0:
+            number = n % 10
+            result += number**2
+            n //= 10
+        
+        if result == 1:
+            return True
+        elif result in numbers_seen:
+            return False
+        else:
+            return is_happy_number_helper(result)
+    return is_happy_number_helper(n)
 
 
 if __name__ == "__main__":
