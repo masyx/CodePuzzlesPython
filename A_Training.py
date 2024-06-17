2e667aaae99d20a92512345797f6762ac84b0faf
Sergey Matveev <rfmatveev@gmail.com>
2024-06-17 07:32:43 -0500

    LinkedList middle training

--- A_Training.py
+++ A_Training.py
@@ -1,17 +1,17 @@
-class LinkedListNode:
-    def __init__(self, value):
+class Node:
+    def __init__(self, value) -> None:
         self.value = value
         self.next = None
 class LinkedList:
     def __init__(self, head):
         self.length = 0
-        self.head = LinkedListNode(head)
+        self.head = Node(head)
         
     def insert_at_tail(self, value):
         current = self.head
         while current.next:
             current = current.next
-        current.next = LinkedListNode(value)
+        current.next = Node(value)
         
     def __str__(self) -> str:
         current = self.head
@@ -22,16 +22,14 @@ class LinkedList:
         return " -> ".join(visited)
 
 
-def detect_cycle(head):
+def get_middle_node(head: Node):
     if head is None:
-        return False
+        return -1
     slow = fast = head
-    while fast.next:
+    while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-        if slow == fast:
-            return True
-    return False
+    return slow
 
 
 if __name__ == "__main__":
@@ -41,8 +39,7 @@ if __name__ == "__main__":
     ll.insert_at_tail(4)
     ll.insert_at_tail(5)
     print(ll)
-    print(f"Does the linked list has a cycle: {detect_cycle(ll.head)}")
-    print("Added cycle from 5 to 1.")
-    ll.head.next.next.next.next.next = ll.head
-    #print(ll) infinite cycle, cannot print
-    print(f"Does the linked list has a cycle: {detect_cycle(ll.head)}")
+    print(f"Middle of the linked list is node: {get_middle_node(ll.head).value}")
+    ll.insert_at_tail(6)
+    print("Added node 6")
+    print(f"Middle of the linked list is node: {get_middle_node(ll.head).value}")
