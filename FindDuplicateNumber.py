b804d0635f2cc0377dcb4167efb48a9028fc26a6
Sergey Matveev <rfmatveev@gmail.com>
2024-06-18 08:07:12 -0500

    FindDuplicateNumber created

---
+++ FindDuplicateNumber.py
@@ -0,0 +1,14 @@
+'''
+Given an array of positive numbers, nums, such that the values lie in the range [1,n], inclusive, 
+and that there are n+1 numbers in the array, find and return the duplicate number present in nums. 
+There is only one repeated number in nums.
+
+Note: You cannot modify the given array nums. You have to solve the problem using only constant extra space.
+'''
+
+def find_duplicate(nums):
+    pass
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 2]
