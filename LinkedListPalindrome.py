43a7cf7d15248129e09a1c5a85817492724b0df6
Sergey Matveev <rfmatveev@gmail.com>
2024-06-26 17:53:37 -0500

    LinkedListPalindrome training

--- LinkedListPalindrome.py
+++ LinkedListPalindrome.py
@@ -3,48 +3,45 @@ class Node:
         self.value = value
         self.next = next
 
-
 def is_palindromic_linked_list(head):
-    middle_node = find_middle_node(head)
-    reversed_second_half_head = reverse_linked_list(middle_node)
-    is_palindrome = compare_linked_lists(head, reversed_second_half_head)
-    second_half = reverse_linked_list(reversed_second_half_head)
-    return is_palindrome
+    middle = find_middle_node(head)
+    reversed_second_half = reverse_linked_list(middle)
+    
+    while head and reversed_second_half:
+        if head.value != reversed_second_half.value:
+            return False
+        head = head.next
+        reversed_second_half = reversed_second_half.next
+    return True
 
 def find_middle_node(head):
-    slow = fast = head
+    slow = head
+    fast = head
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
     return slow
 
-def compare_linked_lists(head_1, head_2):
-    while head_1 and head_2:
-        if head_1.value != head_2.value:
-            return False
-        head_1 = head_1.next
-        head_2 = head_2.next
-    return True
-
-def reverse_linked_list(node):
-    temp = None
-    while node is not None:
-        next = node.next
-        node.next = temp
-        temp = node
-        node = next
-    return temp
-
+def reverse_linked_list(head):
+    current = head
+    previous = None
+    while current:
+        follow = current.next
+        current.next = previous
+        previous = current
+        current = follow
+    return previous
+    
 def main():
     head = Node(2)
-    head.next = Node(4)
-    head.next.next = Node(6)
-    head.next.next.next = Node(7)
-    head.next.next.next.next = Node(4)
+    head.next = Node(1)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(2)
 
     #print("Is palindrome: " + str(is_palindromic_linked_list(head)))
 
-    head.next.next.next.next.next = Node(2)
+    #head.next.next.next.next.next = Node(2)
     print("Is palindrome: " + str(is_palindromic_linked_list(head)))
 
 if __name__ == "__main__":
