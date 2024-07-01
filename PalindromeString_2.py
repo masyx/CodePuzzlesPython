22d155c281525cb435632070c4ba6baaace041da
Sergey Matveev <rfmatveev@gmail.com>
2024-06-30 22:01:59 -0500

    PalindromeString_2 solution my

--- PalindromeString_2.py
+++ PalindromeString_2.py
@@ -1,3 +1,15 @@
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase
+letters and removing all non-alphanumeric characters, it reads the same forward
+and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+'''
+
 # O(n) time | O(n) space
 def is_palindrome_2(s):
     arr = [char.lower() for char in s if char.isalnum()]
