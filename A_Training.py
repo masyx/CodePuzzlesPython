cc479e8b78749c3f2e890ca8ecb6ace1a4af4fb2
Sergey Matveev <rfmatveev@gmail.com>
2024-07-03 23:50:08 -0500

    FizzBuzz training

--- A_Training.py
+++ A_Training.py
@@ -1,27 +1,29 @@
-def maxProfit(prices) -> int:
-    if not prices:
-        return 0
-    smallest = prices[0]
-    max_profit = 0
-    for price in prices[1:]:
-        max_profit = max(max_profit, price - smallest)
-        smallest = min(smallest, price)
-    return max_profit
+def fizz_buzz(n):
+    result = [i for i in range(1, n + 1)]
+    for i in range(1, n + 1):
+        current = ["", ""]
+        if i % 3 == 0:
+            current[0] = "Fizz"
+        if i % 5 == 0:
+            current[1] = "Buzz"
+        if current[0] or current[1]:
+            result[i - 1] = "".join(current)
+    return result
 
+def fizz_buzz_2(n):
+    result = [str(i + 1) for i in range(n)]
+    for i in range(1, n + 1):
+        curr = ""
+        if i % 3 == 0:
+            curr += "Fizz"
+        if i % 5 == 0:
+            curr += "Buzz"
+        if curr:
+            result[i - 1] = curr
+    return result
 
-def maxProfit_2(prices):
-    if not prices:
-        return 0
-    smallest = prices[0]
-    max_profit = 0
-    for price in prices[1:]:
-        current_profit = price - smallest
-        if current_profit > max_profit:
-            max_profit = current_profit
-        if price < smallest:
-            smallest = price
-    return max_profit
+            
 
 if __name__ == "__main__":
     prices = [1]
-    print(maxProfit_2(prices))
+    print(fizz_buzz_2(15))
--- FizzBuzz.py
+++ FizzBuzz.py
@@ -36,6 +36,32 @@ class Solution:
             answer.append(temp)
         return answer
     
+        
+    def fizz_buzz_5(n):
+        result = [i for i in range(1, n + 1)]
+        for i in range(1, n + 1):
+            current = ["", ""]
+            if i % 3 == 0:
+                current[0] = "Fizz"
+            if i % 5 == 0:
+                current[1] = "Buzz"
+            if current[0] or current[1]:
+                result[i - 1] = "".join(current)
+        return result
+
+    def fizz_buzz_6(n):
+        result = [str(i + 1) for i in range(n)]
+        for i in range(1, n + 1):
+            curr = ""
+            if i % 3 == 0:
+                curr += "Fizz"
+            if i % 5 == 0:
+                curr += "Buzz"
+            if curr:
+                result[i - 1] = curr
+        return result
+
+    
     
 def main():
     sol = Solution()
