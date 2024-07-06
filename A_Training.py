c7befd3b2572390c4be6cc0b8a8e9203130e4e4f
Sergey Matveev <rfmatveev@gmail.com>
2024-07-06 17:30:47 -0500

    IsAnagram training my og solution

--- A_Training.py
+++ A_Training.py
@@ -1,34 +1,18 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-
-def printLinkedList(head: Node):
-    current = head
-    result = []
-    seen = set()
-    while current:
-        if current not in seen:
-            result.append(str(current.val))
-            seen.add(current)
-            current = current.next
-        else:
-            break
-    return " -> ".join(result)
-
-def hasCycle(head):
-    slow = fast = head
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-        if slow == fast:
-            return True
-    return False
+def isAnagram(s: str, t: str) -> bool:
+    chars = {}
+    for char in s:
+        chars[char] = chars.get(char, 0) + 1
+    
+    for char in t:
+        if char not in chars:
+            return False
+        chars[char] -= 1
+        if chars[char] == 0:
+            chars.pop(char)
+    return not chars
 
 if __name__ == "__main__":
-    head = Node(1)
-    head.next = Node(2)
-    head.next.next = Node(99)
-    head.next.next.next = head
+    s = "anagram"
+    t = "nagaram"
     
-    print(f"Does LinkedList [{printLinkedList(head)}] has cycle: {'Yes' if hasCycle(head) else 'No'}")
+    print(f"Is '{t}' an anagram of '{s}': {'Yes' if isAnagram(s, t) else 'No'}")
