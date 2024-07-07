81d50945ec0792f9aabff4e5106abd60922c83ba
Sergey Matveev <rfmatveev@gmail.com>
2024-07-07 18:39:08 -0500

    MaxSlidingWindow training

--- A_Training.py
+++ A_Training.py
@@ -1,45 +1,24 @@
-# O(n + m) time | O(1) space complexity because number of letters in alphabet is 26
-def isAnagram(s: str, t: str) -> bool:
-    chars = {}
-    for char in s:
-        chars[char] = chars.get(char, 0) + 1
-    
-    for char in t:
-        if char not in chars:
-            return False
-        chars[char] -= 1
-        if chars[char] == 0:
-            chars.pop(char)
-    return not chars
+from collections import deque
 
-def is_anagram_sort(s, t):
-    s_arr = sorted([char for char in s])
-    t_arr = sorted([char for char in t])
-    if len(s_arr) != len(t_arr):
-        return False
-    for i in range(len(s_arr)):
-        if s_arr[i] != t_arr[i]:
-            return False
-    return True
-
-def is_anagram_sort_2(s, t):
-    s_arr = sorted(s)
-    t_arr = sorted(t)
-    if len(s_arr) != len(t_arr):
-        return False
-    for i in range(len(s_arr)):
-        if s_arr[i] != t_arr[i]:
-            return False
-    return True
-
-def is_anagram_sort_3(s, t):
-    return sorted(s) == sorted(t)
-
+def maxSlidingWindow(nums, w):
+    deq = deque()
+    result = []
+    for i in range(len(nums)):
+        # remove indices that are out of the current window
+        while deq and deq[0] <= i - w:
+            deq.popleft()
+            
+        # remove elements that are smaller than the current value
+        while deq and nums[deq[-1]] < nums[i]:
+            deq.pop()
+        
+        # add the current element's index to the deq
+        deq.append(i)
+        
+        # If the window is completely withing the list add the biggest value to the result
+        if i >= w - 1:
+            result.append(nums[deq[0]])
+    return result
 if __name__ == "__main__":
-    s = "anagram"
-    t = "nagaram"
-    
-    print(f"Is '{t}' an anagram of '{s}': {'Yes' if isAnagram(s, t) else 'No'}")
-    print(f"Is '{t}' an anagram of '{s}': {'Yes' if is_anagram_sort(s, t) else 'No'}")
-    print(f"Is '{t}' an anagram of '{s}': {'Yes' if is_anagram_sort_2(s, t) else 'No'}")
-    print(f"Is '{t}' an anagram of '{s}': {'Yes' if is_anagram_sort_3(s, t) else 'No'}")
+    arr = [2,4,1,5,8]
+    print(maxSlidingWindow(arr, 3))
