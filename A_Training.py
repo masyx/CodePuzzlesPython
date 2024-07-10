9885b2fe981c20c01390a9cedb3c291c6850de13
Sergey Matveev <rfmatveev@gmail.com>
2024-07-09 23:53:29 -0500

    Merge_sorted_arrays training

--- A_Training.py
+++ A_Training.py
@@ -1,16 +1,5 @@
 def merge_sorted_array(arr1, arr2):
-    if not arr1:
-        return arr2
-    if not arr2:
-        return arr1
-    if arr1[0] < arr2[0]:
-        res = merge_sorted_array(arr1[1:], arr2)
-        merged_arr = [arr1[0]] + res
-        return merged_arr
-    else:
-        res = merge_sorted_array(arr1, arr2[1:])
-        merged_arr = [arr2[0]] + res
-        return merged_arr
+
     
 class Node:
     def __init__(self, value):
