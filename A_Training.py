1fc55d9aa0a868eaa06562ead4510635732b58ec
Sergey Matveev <rfmatveev@gmail.com>
2024-07-13 23:40:48 -0500

    SlidingWindowMaximum training

--- A_Training.py
+++ A_Training.py
@@ -1,81 +1,20 @@
-class Node:
-    def __init__(self, val) -> None:
-        self.val = val
-        self.next = None
-class LinkedList:
-    def __init__(self) -> None:
-        self.head = None
-        
-    def add_at_tail(self, val):
-        new_node = Node(val)
-        if not self.head:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-    
-    def __str__(self) -> str:
-        result = []
-        current = self.head
-        while current:
-            result.append(str(current.val))
-            current = current.next
-        return " -> ".join(result)
-    
-    
-def isPalindrome(head: Node) -> bool:
-    middle_node = get_middle_node(head)
-    reversed_head = reverse_linked_list(middle_node)
-    isPal = isPalindromeHelper(head, reversed_head)
-    reverse_linked_list(reversed_head)
-    return isPal
+from collections import deque
 
-def get_middle_node(head: Node):
-    slow = fast = head
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-    return slow
-
-# 1 -> 2
-def reverse_linked_list(head: Node):
-    previous = None
-    current = head
-    while current:
-        following = current.next # None
-        current.next = previous # 1
-        previous = current # 2
-        current = following # None
-    return previous
+def find_max_sliding_window(nums, w):
+    deq = deque()
+    for i in range(len(nums)):
+        while deq and deq[0] <= i - w:
+            deq.popleft()
+            
+        while deq and nums[deq[-1]] < nums[i]:
+            deq.pop()
         
-def isPalindromeHelper(head1, head2):
-    while head1 and head2:
-        if head1.val != head2.val:
-            return False
-        head1 = head1.next
-        head2 = head2.next
-    return True
+        deq.append(i)
+        
+        if i >= w - 1:
+            result.append(nums[deq[0]])
+    return result
 
 if __name__ == "__main__":
-    ll = LinkedList()
-    ll.add_at_tail(1)
-    ll.add_at_tail(2)
-    ll.add_at_tail(3)
-    ll.add_at_tail(4)
-    ll.add_at_tail(5)
-    
-    print(ll)
-    print(isPalindrome(ll.head))
-    
-    
-    ll_2 = LinkedList()
-    ll_2.add_at_tail(1)
-    ll_2.add_at_tail(2)
-    ll_2.add_at_tail(2)
-    ll_2.add_at_tail(1)
-    
-    print(ll_2)
-    print(isPalindrome(ll_2.head))
-    
+    nums = [1,2,5,5,5,8,6]
+    print(find_max_sliding_window(nums, 4))
