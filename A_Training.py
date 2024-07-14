52a6a94083b2f844b5fef0180ed262e58fdde7ff
Sergey Matveev <rfmatveev@gmail.com>
2024-07-14 15:44:49 -0500

    PalindromeLinkedList training

--- A_Training.py
+++ A_Training.py
@@ -1,24 +1,81 @@
-# O(n log n) time | O(1) space
-def containsDuplicate(nums):
-    nums.sort()
-    for i in range(1, len(nums)):
-        if nums[i] == nums[i - 1]:
-            return True
-    return False
-
-# O(n) time | O(n) space
-def containsDuplicate_2(nums):
-    seen = set()
-    for num in nums:
-        if num in seen:
-            return True
-        seen.add(num)
-    return False
-
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        
+    def add_at_tail(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+    
+    def __str__(self) -> str:
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.val))
+            current = current.next
+        return " -> ".join(result)
     
     
+def isPalindrome(head: Node) -> bool:
+    middle_node = get_middle_node(head)
+    reversed_head = reverse_linked_list(middle_node)
+    isPal = isPalindromeHelper(head, reversed_head)
+    reverse_linked_list(reversed_head)
+    return isPal
+
+def get_middle_node(head: Node):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+# 1 -> 2
+def reverse_linked_list(head: Node):
+    previous = None
+    current = head
+    while current:
+        following = current.next # None
+        current.next = previous # 1
+        previous = current # 2
+        current = following # None
+    return previous
+        
+def isPalindromeHelper(head1, head2):
+    while head1 and head2:
+        if head1.val != head2.val:
+            return False
+        head1 = head1.next
+        head2 = head2.next
+    return True
+
 if __name__ == "__main__":
-    nums = [11, 3, 50, 11]
-    print(containsDuplicate(nums))
-    print(containsDuplicate_2(nums))
+    ll = LinkedList()
+    ll.add_at_tail(1)
+    ll.add_at_tail(2)
+    ll.add_at_tail(3)
+    ll.add_at_tail(4)
+    ll.add_at_tail(5)
+    
+    print(ll)
+    print(isPalindrome(ll.head))
+    
+    
+    ll_2 = LinkedList()
+    ll_2.add_at_tail(1)
+    ll_2.add_at_tail(2)
+    ll_2.add_at_tail(2)
+    ll_2.add_at_tail(1)
+    
+    print(ll_2)
+    print(isPalindrome(ll_2.head))
     
