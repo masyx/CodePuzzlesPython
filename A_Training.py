799b7810c2199565f1a9cf5a15aedf22fdb10998
Sergey Matveev <rfmatveev@gmail.com>
2024-07-26 23:58:48 -0500

    InvertBinaryTree learning/training

--- A_Training.py
+++ A_Training.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, value, left = None, right = None) -> None:
         self.value = value
@@ -20,8 +22,9 @@ Inverted:
 7  6 5  4
 
 '''
-
-def invert_binary_tree(root):
+# O(n) time | space for call stack O(log n) balanced binary tree
+#               O(n) for unbalanced tree (completely skewed, LinkedList) 
+def invert_binary_tree1(root):
     if not root:
         return
     invert_binary_tree(root.left)
@@ -29,6 +32,31 @@ def invert_binary_tree(root):
     root.left, root.right = root.right, root.left
     return root
 
+# O(n) time | extra space for queue: O(log n) balanced binary tree
+# O(n)  unbalanced binary tree (completely skewed, LinkedList) 
+def invert_binary_tree_iterative_queue(root: TreeNode):
+    queue = deque([root])
+    while queue:
+        node = queue.pop()
+        node.left, node.right = node.right, node.left
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return root
+
+# O(n) time | extra space for stack: O(log n) balanced binary tree
+# O(n)  unbalanced binary tree (completely skewed, LinkedList)
+def invert_binary_tree_iterative_stack(root: TreeNode):
+    stack = [root]
+    while stack:
+        current_node = stack.pop()
+        if current_node.left:
+            stack.append(current_node.left)
+        if current_node.right:
+            stack.append(current_node.right)
+        current_node.left, current_node.right = current_node.right, current_node.left
+    return root
 
 def print_in_order(node):
     if not node:
@@ -51,3 +79,12 @@ if __name__ == "__main__":
     
     print("\nInverted: ", )
     print_in_order(invert_binary_tree(root))
+    
+    # Checking iterative solution
+    print("\n\nIterative solution")
+    invert_binary_tree(root)
+    print("Original: ")
+    print_in_order(root)
+    
+    print("\nInverted: ", )
+    print_in_order(invert_binary_tree_iterative_stack(root))
