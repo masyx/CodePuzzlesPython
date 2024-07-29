e85dd0953cd3765a750ddc6f7cd5764c69cf1ce9
Sergey Matveev <rfmatveev@gmail.com>
2024-07-29 17:42:56 -0500

    LongestSubstringWithoutRepeatingChars my solution

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -29,21 +29,28 @@ s consists of English letters, digits, symbols and spaces.
 # Check later if this is a duplicate 
 # "LongestSubstringWithDistinctChars.py', this is mihailescu
 
+# O(n) time | O(1) space, there 26 letters in alphabet
 def lengthOfLongestSubstring(s):
     longest = 0
     left = 0
-    observed_chars = set()
+    observed_chars = {}
     for right in range(len(s)):
-        if s[right] in observed_chars:
-            observed_chars.remove(s[right])
+        while s[right] in observed_chars:
+            observed_chars[s[left]] -= 1
+            if observed_chars[s[left]] == 0:
+                del observed_chars[s[left]]
             left += 1
-        observed_chars.add(s[right])
+            
+        observed_chars[s[right]] = observed_chars.get(s[right], 0) + 1
         
         longest = max(longest, right - left + 1)
     return longest
 
 if __name__ == "__main__":
     s = "pwwkew"
+    #s ="dvdf"
+    #s = "nfpdmpi"
+    s = "abcabcbb"
     print("Length of the longest substring without "
         f"repeating char is: {lengthOfLongestSubstring(s)}")
         
