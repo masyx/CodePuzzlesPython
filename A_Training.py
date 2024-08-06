108010418e13debf1b63744c8ad2f0da2d0b605e
Sergey Matveev <rfmatveev@gmail.com>
2024-08-05 23:58:02 -0500

    BestTimeToBuyAndSellStock training

--- A_Training.py
+++ A_Training.py
@@ -1,19 +1,8 @@
-def valid_anagram_array(s, t):
-    if len(s) != len(t):
-        return
-    
-    seen = [0] * 128
-    for i in range(len(s)):
-        seen[ord(s[i])] += 1
-        seen[ord(t[i])] -= 1
-    
-    for char in s:
-        if seen[ord(char)] != 0:
-            return False
-    return True
+def maxProfit(self, prices) -> int:
+    pass
 
 if __name__ == "__main__":
     s = "abb"
     t = "baa"
     print(f"Is '{t}' an anagram of '{s}': "
-          f"{'Yes' if valid_anagram_array(s, t) else 'No'}")
+          f"{'Yes' if maxProfit(s, t) else 'No'}")
