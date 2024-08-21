468752dfe02726da8cb61b9e161e1b1378ba59b9
Sergey Matveev <rfmatveev@gmail.com>
2024-08-20 22:39:36 -0700

    EncodeDecodeString final solution

--- EncodeDecodeString.py
+++ EncodeDecodeString.py
@@ -57,7 +57,25 @@ import random
 import string
 from typing import List
 
+
 class Codec:
+    def encode(self, strs: List[str]) -> str:
+        return "".join(f"{len(s)}/:{s}" for s in strs)
+    
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        result = []
+        
+        while i < len(s):
+            delim = s.find('/:', i)
+            length = int(s[i:delim])
+            i = delim + 2
+            result.append(s[i:i + length])
+            i += length
+        
+        return result
+
+class Codec_My:
     def encode(self, strs: List[str]) -> str:
         # result = ""
         # for s in strs:
