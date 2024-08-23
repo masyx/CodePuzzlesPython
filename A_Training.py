7b5c0c77f5b831164bf4fed333037648027d464d
Sergey Matveev <rfmatveev@gmail.com>
2024-08-23 08:26:43 -0500

    IsPalindrome training completed

--- A_Training.py
+++ A_Training.py
@@ -3,11 +3,11 @@ from collections import defaultdict
 def isPalindrome(s: str) -> bool:
     l, r = 0, len(s) - 1
     while l < r:
-        while l < len(s) - 1 and not s[l].isalnum():
+        while l < r and not s[l].isalnum():
             l += 1
-        while r > 0 and not s[r].isalnum():
+        while r > l and not s[r].isalnum():
             r -= 1
-        if s[l].lower() != s[r].lower():
+        if l < r and s[l].lower() != s[r].lower():
             return False
         l += 1
         r -= 1
@@ -15,5 +15,5 @@ def isPalindrome(s: str) -> bool:
        
 
 if __name__ == "__main__":
-    s=""
+    s=",."
     print(isPalindrome(s))
