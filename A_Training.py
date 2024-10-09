87dc5b4e509f1d108897650a416f08029e0a2a5e
Sergey Matveev <rfmatveev@gmail.com>
2024-10-08 23:37:45 -0500

    LongestSubstring training

--- A_Training.py
+++ A_Training.py
@@ -2,31 +2,27 @@ from collections import defaultdict
 #
 # abcbwa
 
+# 012345
+#    i
+# abaabc
+#      j
+# seen = a b
+# longest = 2
 
-class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        def validPalindromeHelper(s, l, r):
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        
-        if not s or len(s) <= 2:
-            return True
-        
-        l = 0
-        r = len(s) - 1
-        while l < r:
-            if s[l] != s[r]:
-                return validPalindromeHelper(s , l + 1, r) or validPalindromeHelper(s, l, r - 1)
-            l += 1
-            r -= 1
-        return True
+# O(n) time | O(1) space
+def longest_substring(s):
+    seen = {}
+    longest = 0
+    i = 0
+    for j in range(len(s)):
+        while s[j] in seen:
+            del seen[s[i]]
+            i += 1
+        seen[s[j]] = True
+        longest = max(longest, j - i + 1)
+    return longest
 
 
 if __name__ == "__main__":
-    s="abaasba"
-    palindromeChecker = Solution()
-    print(palindromeChecker.validPalindrome(s))
+    s="dvdf"
+    print(longest_substring(s))
