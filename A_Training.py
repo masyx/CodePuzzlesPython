d0f1964e0622353ecd91cc188d10ed072fe8adb6
Sergey Matveev <rfmatveev@gmail.com>
2024-10-15 23:53:53 -0500

    TwoNumberSumSort training

--- A_Training.py
+++ A_Training.py
@@ -1,36 +1,21 @@
-# O(n) time | O(1) space
-def isAnagram(s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
-    seen = {}
-    for char in s:
-        seen[char] = seen.get(char, 0) + 1
-    
-    for char in t:
-        if char not in seen:
-            return False
-        seen[char] -= 1
-        if seen[char] == 0:
-            del seen[char]
-    return len(seen) == 0
-
-def isAnagramArray(s, t):
-    if len(s) != len(t):
-        return False
-    chars_count = [0] * 26
-    for char in s:
-        chars_count[ord(char) - ord('a')] += 1
-    for char in t:
-        chars_count[ord(char) - ord('a')] -= 1
-    for char_count in chars_count:
-        if char_count != 0:
-            return False
-    return True
+def TwoNumberSumSort(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum > targetSum:
+            right -= 1
+        else:
+            left += 1
+    return []    
 
 
 
 
 if __name__ == "__main__":
-    nums = [3, 10, 4, 1, 99] # [1,3,4,10,99]
+    nums = [3, 10, 4, 1, 99] 
     target = 11
-    print(isAnagramArray("tar", "rac"))
+    print(TwoNumberSumSort(nums, target))
