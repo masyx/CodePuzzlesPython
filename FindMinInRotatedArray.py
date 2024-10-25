ba6c34299ff80365ea4bb4a8a855084ea7ca7842
Sergey Matveev <rfmatveev@gmail.com>
2024-10-24 22:08:20 -0700

    FindMinInRotatedArray concise solution

--- FindMinInRotatedArray.py
+++ FindMinInRotatedArray.py
@@ -41,6 +41,8 @@ nums is sorted and rotated between 1 and n times.
 #  0  1  2  3  4  5
 # [5, 6, 7, 8, 2, 3]
 #
+
+# o(log n) time, O(1) space
 def find_min_in_rotated_arr(nums):
     if not nums:
         return -1
@@ -58,7 +60,17 @@ def find_min_in_rotated_arr(nums):
             l = mid + 1
         else:
             r = mid - 1
-            
+
+def find_min(nums):
+    l, r = 0, len(nums) - 1 
+    while l < r:
+        mid = (l + r) // 2
+        if nums[mid] < nums[r]:
+            r = mid
+        else:
+            l = mid + 1
+    return nums[l]
+
             
 if __name__  == "__main__":
     nums = [2]
