248d7035a4e4221236f0a97463c6925e5473762e
Sergey Matveev <rfmatveev@gmail.com>
2024-10-25 23:57:35 -0500

    FindMinInRotatedArray training

--- FindMinInRotatedArray.py
+++ FindMinInRotatedArray.py
@@ -65,10 +65,10 @@ def find_min(nums):
     l, r = 0, len(nums) - 1 
     while l < r:
         mid = (l + r) // 2
-        if nums[mid] < nums[r]:
-            r = mid
-        else:
+        if nums[mid] > nums[r]:
             l = mid + 1
+        else:
+            r = mid
     return nums[l]
 
             
