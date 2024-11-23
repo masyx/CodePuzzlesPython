6ef7de2f585696c40027eb2009ef596b51f5a31c
Sergey Matveev <rfmatveev@gmail.com>
2024-11-22 23:19:49 -0600

    KokoEatingBananas solved finished

--- KokoEatingBananas.py
+++ KokoEatingBananas.py
@@ -4,11 +4,11 @@ Medium
 
 Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. 
 The guards have gone and will come back in h hours.
-Koko can decide her bananas-per-hour eating speed of curr_speed. Each hour, she chooses some pile of bananas 
-and eats curr_speed bananas from that pile. If the pile has less than curr_speed bananas, she eats all of them 
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas 
+and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them 
 instead and will not eat any more bananas during this hour.
 Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
-Return the minimum integer curr_speed such that she can eat all the bananas within h hours.
+Return the minimum integer k such that she can eat all the bananas within h hours.
 
 Example 1:
 Input: piles = [3,6,7,11], h = 8
@@ -33,24 +33,28 @@ import math
 from typing import List
 
 
-def min_eating_bananas(piles: List[int], h: int):
-    l, r = 1, max(piles)
-    res = r
+#i 0  1  2   3
+# [5, 3, 1, 4] h = 4
+# speed min = 5, max = 5, medium = 4 
+# result = 5
 
-    while l <= r:
-        curr_speed = (l + r) // 2
-
-        totalTime = 0
-        for p in piles:
-            totalTime += math.ceil(float(p) / curr_speed)
-        if totalTime <= h:
-            res = curr_speed
-            r = curr_speed - 1
+def min_eating_speed(piles: List[int], h):
+    min_speed, max_speed = 1, max(piles)
+    result = max_speed
+    while min_speed <= max_speed:
+        medium_speed = (max_speed + min_speed) // 2
+        hours_spent = 0
+        for pile in piles:
+            hours_spent += math.ceil(pile / medium_speed)
+        if hours_spent > h:
+            min_speed = medium_speed + 1
         else:
-            l = curr_speed + 1
-    return res
+            max_speed = medium_speed - 1
+    return min_eating_speed
 
 
 if __name__ == "__main__":
-    piles = [312884470]
-    print(min_eating_bananas(piles, 312884469))
+    piles = [30,11,23,4,20]
+    h = 6
+    
+    print(min_eating_speed(piles, h))
