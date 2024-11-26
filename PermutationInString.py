1c9fa8e42caa2194a5cca326882a4483f8ba81d0
Sergey Matveev <rfmatveev@gmail.com>
2024-11-26 11:19:30 -0600

    PermutationInString improved solution with array

--- PermutationInString.py
+++ PermutationInString.py
@@ -27,6 +27,8 @@ from collections import Counter
 # s2="koabjyuu"
 # l = 0, r = 1
 # r - l + 1 = x ? > 2
+
+# O(n + m) time | O(1) space
 def check_inclusion(s1, s2):
     #s1_dict = Counter(s1)
     s1_dict = {}
@@ -45,8 +47,24 @@ def check_inclusion(s1, s2):
             return True
     return False
 
+# O(n + m) time | O(1) space
+def check_inclusion_array(s1, s2):
+    count1 = [0] * 26
+    count2 = [0] * 26
+    for char in s1:
+        count1[ord(char) - ord('a')] += 1
+    l = 0
+    for r in range(len(s2)):
+        count2[ord(s2[r]) - ord('a')] += 1
+        if r - l + 1 > len(s1):
+            count2[ord(s2[l]) - ord('a')] -= 1
+            l += 1
+        if count1 == count2:
+            return True
+    return False
 
 if __name__ == "__main__":
-    s1 = "abc"
-    s2="cba"
+    s1 = "ba"
+    s2="koabjyuu"
     print(check_inclusion(s1, s2))
+    print(check_inclusion_array(s1, s2))
