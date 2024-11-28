0783c8648094a829fdc83617028a5aa4b9d6ea6f
Sergey Matveev <rfmatveev@gmail.com>
2024-11-28 16:29:13 -0600

    SearchMatrix practicing

--- A_Training.py
+++ A_Training.py
@@ -1,36 +1,25 @@
-#  0 1 2 3 4 5 6 7
-# [4,5,6,7,8,1,2,3] target = 2
-# l=0, r=7, m=3
-
-#  0 1 2 3 4 5 6
-# [4,5,6,7,0,1,2] target = 0
-# l=4, r=6, m=5
-
 from typing import List
 
-def search(nums, target):
+
+def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+    rows = len(matrix)
+    cols = len(matrix[0])
     l = 0
-    r = len(nums) - 1
+    r = rows * cols - 1
     while l <= r:
         mid = (l + r) // 2
-        if target == nums[mid]:
-            return mid
-        # left subarray is sorted
-        if nums[l] <= nums[mid]:
-            if target >= nums[l] and target < nums[mid]:
-                r = mid - 1
-            else:
-                l = mid + 1
-        # right subarray is sorted
+        row = mid // cols
+        col = mid % cols
+        if matrix[row][col] == target:
+            return True
+        elif target > matrix[row][col]:
+            l = mid + 1
         else:
-            if target >= nums[mid] and target < nums[r]:
-                l = mid + 1
-            else:
-                r = mid - 1
-    return -1
+            r = mid - 1
+    return False
 
 
 if __name__ == "__main__":
-    nums = [4,5,6,7,8,1,2,3]
-    target = 2
-    print(search(nums, target))
+    matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    target = 3
+    print(searchMatrix(matrix, target))
