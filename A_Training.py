c65d438ee7c9c4ceefd8a49968227636f77b536c
Sergey Matveev <rfmatveev@gmail.com>
2024-11-30 14:52:20 -0600

    SearchInRotatedArray practicing

--- A_Training.py
+++ A_Training.py
@@ -1,25 +1,29 @@
 from typing import List
 
-
-def searchMatrix(matrix: List[List[int]], target: int) -> bool:
-    rows = len(matrix)
-    cols = len(matrix[0])
+#  0 1 2 3 4 5 6 7
+# [5,6,7,8,9,1,2,3]
+# l = 0, r = 7, mid = 3
+def search_array(nums, target):
     l = 0
-    r = rows * cols - 1
+    r = len(nums) - 1
     while l <= r:
         mid = (l + r) // 2
-        row = mid // cols
-        col = mid % cols
-        if matrix[row][col] == target:
-            return True
-        elif target > matrix[row][col]:
-            l = mid + 1
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] >= nums[l]:
+            if nums[l] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
         else:
-            r = mid - 1
-    return False
-
+            if nums[mid] < target <= nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return -1
+    
 
 if __name__ == "__main__":
-    matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-    target = 3
-    print(searchMatrix(matrix, target))
+    nums = [5,6,7,8,9,1,2,3]
+    target = 1
+    print(search_array(nums, target))
