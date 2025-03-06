1967714456fbfb7b78590043df25d3d867c45874
Sergey Matveev <rfmatveev@gmail.com>
2025-03-06 09:10:16 -0600

    GroupAnagrams training

--- A_Training.py
+++ A_Training.py
@@ -1,68 +1,32 @@
-import math
+from collections import defaultdict
 from typing import List
 
-#  0  1  2  3   4   5
-# [0, 4, 8, 10, 22, 29], target 29
-# l = 5, r = 5, mid = 4
-def binary_search(nums, target):
-    l = 0
-    r = len(nums) - 1
-    while l <= r:
-        mid = (l + r) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            r = mid - 1
-        else:
-            l = mid + 1
-    return None            
 
-def search_rotated(nums: List[int], target: int) -> int:
-    l = 0
-    r = len(nums) - 1
-    while l <= r:
-        mid = (l + r) // 2
-        if nums[mid] == target:
-            return mid
-        # left subarray is sorted
-        elif nums[mid] >= nums[l]:
-            if nums[l] <= target < nums[mid]:
-                r = mid - 1
-            else:
-                l = mid + 1
-        # right subarray is sorted
-        else:
-            if nums[mid] < target <= nums[r]:
-                l = mid + 1
-            else:
-                r = mid - 1
-    return None
+# O(n * m *log(m)) time | O(n) space
+def groupAnagrams_bf(strs: List[str]) -> List[List[str]]:
+    groups = {}
+    for word in strs:
+        key = str(sorted(word))
+        groups.setdefault(key, []).append(word)
+    return list(groups.values())
 
-
-# 123
-def reverse_int(x: int) -> int:
-    MIN = -2147483648  # -2^31,
-    MAX = 2147483647  #  2^31 - 1
-
-    res = 0
-    while x:
-        pop = int(math.fmod(x, 10))
-        x = int(x / 10)
-
-        if res > MAX // 10 or (res == MAX // 10 and pop > MAX % 10): # edge case number for second condition is 8463847412
-            return 0
-        if res < MIN // 10 or (res == MIN // 10 and pop < MIN % 10):
-            return 0
-        res = (res * 10) + pop
-
-    return res
+# O(n * m) time | O(n) space
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    #groups = defaultdict(list)
+    groups = {} 
+    for word in strs:
+        count = [0] * 26
+        for char in word:
+            count[ord(char) - ord('a')] += 1
+        #groups[tuple(count)].append(word)
+        groups.setdefault(tuple(count), []).append(word)
+    return list(groups.values())
 
 
 def main():
-    nums = [3,0]
-    #print(binary_search(nums, 1))
-    #print(search_rotated(nums, 0))
-    print(reverse_int(-1563847412))
-
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    print(groupAnagrams(strs))
+    
+    
 if __name__ == "__main__":
     main()
