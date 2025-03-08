6f7e1c9adb2205c8190c77b31466335b69862aeb
Sergey Matveev <rfmatveev@gmail.com>
2025-03-07 22:57:57 -0600

    EncodeDecode string training

--- A_Training.py
+++ A_Training.py
@@ -2,23 +2,55 @@ from collections import defaultdict
 from typing import List
 
 
-def valid_anagram(s, t) -> bool:
-    if len(s) != len(t):
-        return False
+class Codec:
+    # ["", "W" "Hello","SuperDuper"]
     
-    counter = [0] * 26
-    for i in range(len(s)):
-        counter[ord(s[i]) - ord('a')] += 1
-        counter[ord(t[i]) - ord('a')] -= 1
-    
-    for count in counter:
-        if count != 0:
-            return False
-    return True
 
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        if not strs:
+            return ""
+        res = ""
+        for word in strs:
+            res += f"{len(word):03}"
+            res += word
+        return res
+        
+    #  012345678901234567890
+    #     i
+    # "000001W005Hello010SuperDuper"
+    #        j
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        if not s:
+            return []
+        res = []
+  
+        i = 0
+        while i < len(s):
+            j = i + 3
+            length = int(s[i:j])
+            if length == 0:
+                res.append("")
+                i = j
+            else:
+                res.append(s[j:j + length])
+                i = j + length 
+        return res
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
 
 def main():
-    print(valid_anagram("rat", "car"))    
+    codec = Codec()
+    arr = ["", "W", "Hello","SuperDuper"]
+    s = codec.encode(arr)
+    print(s)
+    print(codec.decode(s))    
     
 if __name__ == "__main__":
     main()
