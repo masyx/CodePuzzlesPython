ac8961b9c5cd9cdf1bc28cddd6b6fc8f7bf658aa
Sergey Matveev <rfmatveev@gmail.com>
2025-03-17 07:11:36 -0500

    ThreeSum training

--- A_Training.py
+++ A_Training.py
@@ -1,27 +1,62 @@
 
+# nums = [-1,0,1,2,-1,-4] [-1,-1,-1,10,-9,0,1,2] -> [-1, -1, 2],skip [-1, -1, 2], [-1, 10, -9]
 
-def isPalindrome(s: str) -> bool:
-    l = 0
-    r = len(s) - 1
+# O(n log n) time | O(n) space
+from typing import List
 
-    while l < r:
-        while l < r and not s[l].isalnum():
-            l += 1
-        while l < r and not s[r].isalnum():
-            r -= 1
-        if s[l].lower() != s[r].lower():
-            return False
-        l += 1
-        r -= 1
 
-    return True
+def three_sum(nums):
+    result = set()
+    nums.sort()
+    for i in range(len(nums)):
+        l = i + 1
+        r = len(nums) - 1
+        while l < r:
+            curr_sum = nums[i] + nums[l] + nums[r]
+            if curr_sum == 0:
+                if (nums[i], nums[l], nums[r]) not in result:
+                    result.add((nums[i], nums[l], nums[r]))
+                l += 1
+                r -= 1
+            elif curr_sum > 0:
+                r -= 1
+            else:
+                l += 1
+    return [list(i) for i in result]
 
-        
+def three_sum_neetcode(nums: List[int]) -> List[List[int]]:
+    res = []
+    nums.sort()
 
+    for i, num in enumerate(nums):
+        if num > 0:
+            break
+
+        if i > 0 and num == nums[i - 1]:
+            continue
+
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = num + nums[l] + nums[r]
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
+            else:
+                res.append([num, nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+                    
+    return res
+            
+    
 
 def main():
-    s = "    "
-    print(isPalindrome(s))
+    nums = [2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10]
+    #nums = [0,0,0]
+    print(three_sum_neetcode(nums))
     
      
 if __name__ == "__main__":
