03cf8239b22c15f197ef9f1d8c0d95a81c672b1a
Sergey Matveev <rfmatveev@gmail.com>
2025-03-16 21:46:18 -0500

    ValidPalindrome training

--- A_Training.py
+++ A_Training.py
@@ -1,32 +1,27 @@
-#  i    0  1  2   3
-# arr  [2, 5,  3, 4]
-# pre  [1, 2, 10,30]
-# post [60,12, 4, 1]
-# res  [60,24,40,30]
 
-# O(n) time | O(n) space
-def product_except_self(nums):
-    answer = [1] * len(nums)
-    pre_prod = [1] * len(nums)
-    post_prod = [1] * len(nums)
-    
-    for i in range(1, len(nums)):
-        pre_prod[i] = pre_prod[i - 1] * nums[i - 1]
-    
-    for i in range(len(nums) - 2, -1, -1):
-        post_prod[i] = post_prod[i + 1] * nums[i + 1]
-        
-    for i in range(len(answer)):
-        answer[i] = pre_prod[i] * post_prod[i]
-    
-    return answer
+
+def isPalindrome(s: str) -> bool:
+    l = 0
+    r = len(s) - 1
+
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l < r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+
+    return True
 
         
 
 
 def main():
-    nums = [2, 5, 3, 4]
-    print(product_except_self(nums))
+    s = "    "
+    print(isPalindrome(s))
     
      
 if __name__ == "__main__":
