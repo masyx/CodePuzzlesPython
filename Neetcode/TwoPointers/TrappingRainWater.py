cac20cff8442a9c41350aaca96b78c0dc571316a
Sergey Matveev <rfmatveev@gmail.com>
2025-03-22 09:55:06 -0500

    TrappingRainWater solution two pointers

--- Neetcode/TwoPointers/TrappingRainWater.py
+++ Neetcode/TwoPointers/TrappingRainWater.py
@@ -29,7 +29,7 @@ This is central to the trapping rain water problem. It ensures that the height o
 doesn’t exceed the smaller boundary.
 '''
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap_bf(self, height: List[int]) -> int:
         if not height:
             return 0
         
@@ -45,12 +45,16 @@ class Solution:
                 
             res += min(left_max, right_max) - height[i]
         return res
-    
-    #      i:     0 1 2 3 5 6
-    # height:    [3,2,0,5,2,4]
-    # left_max:  [3,3,3,5,5,5]
-    # right_max: [5,5,5,5,4,4]
-    def trap_dp_neet(self, height: List[int]) -> int:
+
+    """Drawings
+        
+             i:     0 1 2 3 5 6
+        height:    [3,2,0,5,2,4]
+        left_max:  [3,3,3,5,5,5]
+        right_max: [5,5,5,5,4,4]
+        
+    """
+    def trap_dp(self, height: List[int]) -> int:
         n = len(height)
         if n == 0:
             return 0
@@ -70,24 +74,49 @@ class Solution:
         for i in range(n):
             res += min(leftMax[i], rightMax[i]) - height[i]
         return res
+
+    """Key Point
+    The core trick is that at any point, you only need to focus on the side (left or right) that has a smaller "max height so far."
+    Why? Because if, for example, the left side has a smaller “max bar” than the right side, the right side is tall enough to ensure 
+    that the left side’s water trapping capacity depends only on the left side’s height. You move the pointer inward from the side 
+    with the smaller max height, update that side’s max height, and calculate how much water is trapped based on how far below that 
+    max height the current bar is.
+    """
+    def trap_two_pointers(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
             
-        
             
-            
-    
 def main():
     #  5        #    
-    #  4        #   #
-    #  3  #     #   # 
+    #  4        #   
+    #  3  #     #    
     #  2  # #   # # #
     #  1  # #   # # #
     #  0  -----------
     #     0 1 2 3 4 5
   
-    height = [3,2,0,5,2,4]
-    print(Solution().trap(height))
+    height = [3,2,0,5,2,2]
+    #height = [1,3,5,4]
+    print(Solution().trap_bf(height))
     print(Solution().trap_dp(height))
-    print(Solution().trap_dp_neet(height))
+    print(Solution().trap_two_pointers(height))
+
+
     
 
 if __name__ == "__main__":
