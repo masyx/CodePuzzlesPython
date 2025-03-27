5c2bd1beb708f74380e1b91b8522d94c06e16411
Sergey Matveev <rfmatveev@gmail.com>
2025-03-26 22:25:56 -0500

    MinStack training

--- A_Training.py
+++ A_Training.py
@@ -1,54 +1,61 @@
-from collections import defaultdict
+class MinStack:
 
-class Solution:
-    #   r
-    #  l
-    #i 0123456
-    # "AABCBBA" k=2
-    # w = 
-    def character_replacement(self, s: str, k: int) -> int:
-        l = 0
-        counts = defaultdict(int)
-        max_freq = 0
-        res = 0
-        for r in range(len(s)):
-            counts[s[r]] += 1
-            max_freq = max(max_freq, counts[s[r]])
-            
-            if (r - l + 1) - max_freq > k:
-                counts[s[l]] -= 1
-                l += 1
-            
-            res = max(res, r - l + 1)
-        return res
-            
-        #       r
-    #   l
-    #  0123456
-    # "AAABABB"
-    # A = 3, B = 2
-    # max_freq = 4
-    # res = 5
-    def characterReplacement(self, s: str, k: int) -> int:
-        res = 0
-        counts = [0] * 26
-        l = 0
-        max_freq = 0
-        for r, char in enumerate(s):
-            counts[ord(char) - ord('A')] += 1
-            max_freq = max(max_freq, counts[ord(char) - ord('A')])
-            if (r - l + 1) - max_freq > k:
-                counts[ord(s[l]) - ord('A')] -= 1
-                l += 1
-            res = max(res, r - l + 1)
+    def __init__(self):
+        self.stack = []
+        self.min_values = []
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_values or val <= self.min_values[-1]:
+            self.min_values.append(val)
 
-        return res
+    def pop(self) -> None:
+        if self.stack:
+            val = self.stack.pop()
+            if val == self.min_values[-1]:
+                self.min_values.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        return None
+
+    def getMin(self) -> int:
+        if self.min_values:
+            return self.min_values[-1]
+        return None
     
 def main():
-    s = "AAABABBBB"
-    sol = Solution()
-    print(sol.character_replacement(s, 1))
-    print(sol.characterReplacement(s, 1))
+    stack = MinStack()
+    # stack.push(5)
+    # stack.push(6)
+    # stack.push(-1)
+    # stack.push(8)
+    # stack.push(-88)
+    # print(stack.getMin())
+    # print(stack.top())
+    # print(stack.pop())
+    # print(stack.top())
+    # stack.pop()
+    # stack.pop()
+    # stack.pop()
+    # stack.pop()
+    # stack.pop()
+    # stack.pop()
+    # print(stack.getMin())
+    #["MinStack","push","push","push","getMin","pop","getMin"]
+    #[[],[0],[1],[0],[],[],[]]
+    stack.push(0)
+    stack.push(1)
+    stack.push(0)
+    stack.push(0)
+    stack.push(0)
+    stack.push(0)
+    stack.push(0)
+    
+    print(stack.getMin())
+    stack.pop()
+    print(stack.getMin())
 
     
      
