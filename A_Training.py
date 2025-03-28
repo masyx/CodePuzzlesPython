c9a2226cf2e1a402971d99d617728f70dd8fb47c
Sergey Matveev <rfmatveev@gmail.com>
2025-03-28 12:10:32 -0500

    ValidParentheses training

--- A_Training.py
+++ A_Training.py
@@ -1,97 +1,24 @@
-class MinStack1:
-
-    def __init__(self):
-        self.stack = []
-        self.min_values = []
+class Solution:
+    
+    # stack: [")"]
+    def isValid(self, s: str) -> bool:
+        stack = []
+        map = {")":"(", "]":"[", "}":"{"}
+        for char in s:
+            if char not in map:
+                stack.append(char)
+            elif stack and map[char] == stack[-1]:
+                    stack.pop()
+            else:
+                return False
+        return len(stack) == 0
         
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.min_values or val <= self.min_values[-1]:
-            self.min_values.append(val)
-
-    def pop(self) -> None:
-        if self.stack:
-            val = self.stack.pop()
-            if val == self.min_values[-1]:
-                self.min_values.pop()
-
-    def top(self) -> int:
-        if self.stack:
-            return self.stack[-1]
-        return None
-
-    def getMin(self) -> int:
-        if self.min_values:
-            return self.min_values[-1]
-        return None
-
-
-class MinStack:
-    def __init__(self):
-        self.values = []
-        # [[1,1], [0, 1]]
-        self.min_values = []
-        
-    def push(self, val: int) -> None:
-        self.values.append(val)
-        if not self.min_values or val < self.min_values[-1][0]:
-            self.min_values.append([val, 1])
-        elif val == self.min_values[-1][0]:
-            self.min_values[-1][1] += 1
-    
-    def pop(self) -> None:
-        if self.values:
-            val = self.values.pop()
-            if val == self.min_values[-1][0]:
-                self.min_values[-1][1] -= 1
-                if self.min_values[-1][1] == 0:
-                    self.min_values.pop()
-    
-    def top(self) -> int:
-        if self.values:
-            return self.values[-1]
-    
-    def getMin(self) -> int:
-        if self.min_values:
-            return self.min_values[-1][0]
   
     
 def main():
-    stack = MinStack()
-    # stack.push(5)
-    # stack.push(6)
-    # stack.push(-1)
-    # stack.push(8)
-    # stack.push(-88)
-    # print(stack.getMin())
-    # print(stack.top())
-    # print(stack.pop())
-    # print(stack.top())
-    # stack.pop()
-    # stack.pop()
-    # stack.pop()
-    # stack.pop()
-    # stack.pop()
-    # stack.pop()
-    print(stack.getMin())
-    #["MinStack","push","push","push","getMin","pop","getMin"]
-    #[[],[0],[1],[0],[],[],[]]
-    stack.push(0)
-    stack.push(1)
-    stack.push(0)
-    stack.push(0)
-
-    
-    print(stack.getMin())
-    stack.pop()
-    print(stack.getMin())
-    stack.push(-1)
-    stack.pop()
-    stack.pop()
-    stack.pop()
-    stack.pop()
-    stack.top()
-
+    solution = Solution()
+    s = ")()[{}]"
+    print(solution.isValid(s))
     
      
 if __name__ == "__main__":
--- Neetcode/Stack/ValidParenthesis.py
+++ Neetcode/Stack/ValidParenthesis.py
@@ -14,6 +14,20 @@ class Solution:
         #return stack == []
         return not stack
 
+class Solution2:
+    
+    #O(n) time | O(n) space, all characters in the string are opening brackets (like "(((([[[{{{"), and none of them are matched and popped. So all n characters end up on the stack.
+    def isValid(self, s: str) -> bool:
+        stack = []
+        map = {")":"(", "]":"[", "}":"{"}
+        for char in s:
+            if char not in map:
+                stack.append(char)
+            elif stack and map[char] == stack[-1]:
+                    stack.pop()
+            else:
+                return False
+        return len(stack) == 0
 
 
 def main():
