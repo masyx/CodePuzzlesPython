e2aff33ddcff4baf1fa76369b9717145bdef746a
Sergey Matveev <rfmatveev@gmail.com>
2025-03-27 23:06:04 -0500

    MinStack training

--- A_Training.py
+++ A_Training.py
@@ -1,4 +1,4 @@
-class MinStack:
+class MinStack1:
 
     def __init__(self):
         self.stack = []
@@ -24,6 +24,37 @@ class MinStack:
         if self.min_values:
             return self.min_values[-1]
         return None
+
+
+class MinStack:
+    def __init__(self):
+        self.values = []
+        # [[1,1], [0, 1]]
+        self.min_values = []
+        
+    def push(self, val: int) -> None:
+        self.values.append(val)
+        if not self.min_values or val < self.min_values[-1][0]:
+            self.min_values.append([val, 1])
+        elif val == self.min_values[-1][0]:
+            self.min_values[-1][1] += 1
+    
+    def pop(self) -> None:
+        if self.values:
+            val = self.values.pop()
+            if val == self.min_values[-1][0]:
+                self.min_values[-1][1] -= 1
+                if self.min_values[-1][1] == 0:
+                    self.min_values.pop()
+    
+    def top(self) -> int:
+        if self.values:
+            return self.values[-1]
+    
+    def getMin(self) -> int:
+        if self.min_values:
+            return self.min_values[-1][0]
+  
     
 def main():
     stack = MinStack()
@@ -42,20 +73,24 @@ def main():
     # stack.pop()
     # stack.pop()
     # stack.pop()
-    # print(stack.getMin())
+    print(stack.getMin())
     #["MinStack","push","push","push","getMin","pop","getMin"]
     #[[],[0],[1],[0],[],[],[]]
     stack.push(0)
     stack.push(1)
     stack.push(0)
     stack.push(0)
-    stack.push(0)
-    stack.push(0)
-    stack.push(0)
+
     
     print(stack.getMin())
     stack.pop()
     print(stack.getMin())
+    stack.push(-1)
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    stack.top()
 
     
      
