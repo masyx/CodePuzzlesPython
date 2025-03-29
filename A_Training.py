5757a44ab5b9bd67701c09132cd79bd45600f317
Sergey Matveev <rfmatveev@gmail.com>
2025-03-29 10:06:51 -0500

    ReversePolishNotation learning

--- A_Training.py
+++ A_Training.py
@@ -1,24 +1,42 @@
+from typing import List
+
 class Solution:
+    #                v
+    #i:  0   1   2   3   4
+    #  ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+    # stack = [3, 3]
+    # val_2 = 1, val_1 = 2
     
-    # stack: [")"]
-    def isValid(self, s: str) -> bool:
+    # O(n) time | O(m) space
+    def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        map = {")":"(", "]":"[", "}":"{"}
-        for char in s:
-            if char not in map:
-                stack.append(char)
-            elif stack and map[char] == stack[-1]:
-                    stack.pop()
+        operators = {"*", "/", "+", "-"}
+        for token in tokens:
+            if token not in operators:
+                stack.append(int(token))
             else:
-                return False
-        return len(stack) == 0
+                val_2 = stack.pop()
+                val_1 = stack.pop()
+                result = 0
+                if token == "+":
+                    result = val_1 + val_2
+                elif token == "-":
+                    result = val_1 - val_2
+                elif token == "*":
+                    result = val_1 * val_2
+                elif token == "/":
+                    result = int(val_1 / val_2)
+                stack.append(result)
+        return stack[0]
+            
+        
         
   
     
 def main():
     solution = Solution()
-    s = ")()[{}]"
-    print(solution.isValid(s))
+    tokens = ["18"]
+    print(solution.evalRPN(tokens))
     
      
 if __name__ == "__main__":
