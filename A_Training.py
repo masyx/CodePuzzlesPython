2fa7e104ae3f30869e69d5ebf2d7c3bc1c2eb870
Sergey Matveev <rfmatveev@gmail.com>
2025-04-06 19:14:07 -0500

    SearchMatrix training

--- A_Training.py
+++ A_Training.py
@@ -3,37 +3,32 @@ from typing import List, Optional
 import math
 
 class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        left, right = 0, rows * columns - 1
 
-    def encode(self, strs: List[str]) -> str:
-        def return_count(s):
-            count = str(len(s))
-            zeros_needed = 3 - len(count)
-            return "0" * zeros_needed + count
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // columns
+            column = mid % columns
+            if matrix[row][column] == target:
+                return True
+            elif target > matrix[row][column]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
 
-        res = ""
-        for s in strs:
-            res += return_count(s) + s
-        return res
-    # i = 3
-    #0123456789101112
-    #003cat004lo v e
-    def decode(self, s: str) -> List[str]:
-        res = []
-        i = 0
-        while i < len(s): 
-            length = int(s[i : i + 3])
-            word = s[i + 3: i + 3 + length]
-            res.append(word)
-            i += 3 + length
-        return res
+        
 
 def main():
-    strings = ["neet","code","love","you"]
+    matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    matrix = [[2]]
 
     solution = Solution()
-    s = solution.encode(strings)
-    print(s)
-    print(solution.decode(s))
+    res = solution.searchMatrix(matrix, 2)
+    print(res)
     
      
 if __name__ == "__main__":
