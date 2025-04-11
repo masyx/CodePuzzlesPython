e61e78c6106dceb8037929fbb884f4a9d973220c
Sergey Matveev <rfmatveev@gmail.com>
2025-04-10 22:02:12 -0500

    3Sum training hash table

--- A_Training.py
+++ A_Training.py
@@ -2,41 +2,49 @@ from typing import List
 from collections import defaultdict
 
 class Solution:
-    # O(n^2) space | O(n) space
-    def minimumOperations(self, nums: List[int]) -> int:
-        def is_unique(start) -> bool:
-            seen = set()
-            for num in nums[start:]:
-                if num in seen:
-                    return False
-                seen.add(num)
-            return True
-        
-        res = 0
-        for i in range(0, len(nums),  3):
-            if not is_unique(i):
-                res += 1
-        return res
-    #   i:    0 1 2 3 4 5 6 7 8
-    #nums:   [1,2,3,4,2,3,3,5,7]
-    def min_operations(self, nums):
-        res = 0
-        seen = set()
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] in seen:
-                res = i // 3 + 1
+    #nums:    [-1,0,1,2,-1,-4]
+    #
+    #   i:      0  1  2 3 4 5
+    #nums:    [-4,-1,-1,0,1,2]
+    # target = x + y + z => target - x = y + z => 0 - x = y + z => -x = y + z
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            curr_num = nums[i]
+            if curr_num > 0:
                 break
-            seen.add(nums[i])
+            if i > 0 and curr_num == nums[i - 1]:
+                continue
+            
+            self.two_sum(i + 1, nums, -curr_num, res)
         return res
+    # length = 3
+    #       v
+    #   0 1 2
+    # [-2,1,1]
+    # seen: 1
+    def two_sum(self, start, nums, target, res):
+        seen = set()
+        i = start
+        while i < len(nums):
+            curr_num = nums[i]            
+            compliment = target - curr_num
+            if compliment in seen:
+                res.append([nums[start - 1], compliment, curr_num])
+                while i + 1 < len(nums) and curr_num == nums[i + 1]:
+                    i += 1
+            seen.add(curr_num)
+            i += 1
+        return res
+        
         
     
 def main():
-    nums = [1,2,3,4,2,3,3,5,7]
-    nums = [3,3,3,3,3,3]
-    # nums = [1,2,3,4,5,6,7,8,9,10]
+    nums = [-1,0,1,2,-1,-4]
+    nums = [0,0,0,0]
     solution = Solution()
-    print(solution.minimumOperations(nums))
-    print(solution.min_operations(nums))
+    print(solution.threeSum(nums))
     
     
      
--- Neetcode/ArraysAndHashing/ThreeSum.py
+++ Neetcode/ArraysAndHashing/ThreeSum.py
@@ -68,7 +68,44 @@ def three_sum(numbers):
                     l += 1
     return result
 
+from typing import List
 
+class SolutionHashTable:
+    #nums:    [-1,0,1,2,-1,-4]
+    #
+    #   i:      0  1  2 3 4 5
+    #nums:    [-4,-1,-1,0,1,2]
+    # target = x + y + z => target - x = y + z => 0 - x = y + z => -x = y + z
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            curr_num = nums[i]
+            if curr_num > 0:
+                break
+            if i > 0 and curr_num == nums[i - 1]:
+                continue
+            
+            self.two_sum(i + 1, nums, -curr_num, res)
+        return res
+    # length = 3
+    #       v
+    #   0 1 2
+    # [-2,1,1]
+    # seen: 1
+    def two_sum(self, start, nums, target, res):
+        seen = set()
+        i = start
+        while i < len(nums):
+            curr_num = nums[i]            
+            compliment = target - curr_num
+            if compliment in seen:
+                res.append([nums[start - 1], compliment, curr_num])
+                while i + 1 < len(nums) and curr_num == nums[i + 1]:
+                    i += 1
+            seen.add(curr_num)
+            i += 1
+        return res
 
 
 if __name__ == "__main__":
