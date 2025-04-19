64dc9654c00437c9a8716e7c251063e460158a9c
Sergey Matveev <rfmatveev@gmail.com>
2025-04-18 22:34:04 -0500

    SearchInRotatedArray training

--- A_Training.py
+++ A_Training.py
@@ -1,4 +1,5 @@
-class Solution:
+from typing import List
+class SolutionBitManipulation:
     def hammingWeight_bf(self, n: int) -> int:
         count = 0
         for i in range(32):
@@ -46,11 +47,31 @@ class Solution:
             b = carry & mask
         return a if a <= MAX_INT else ~(a ^ mask)
         
-                    
+class Solution:
+    def search_rotated(self, nums: List[int], target: int):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            # left subarray is sorted
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            # right subarray is sorted
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
                 
 def main():
-    sol = Solution()
+    sol = SolutionBitManipulation()
     
+    # region BitManipulation testing
     n = 2 ** 31 - 1 # 2147483647
     print(f"Number of set bits in '{n}' is: {sol.hammingWeight(n)}")
     print()
@@ -66,7 +87,14 @@ def main():
     a, b = 3, 3
     print(f"Sum of {a} and {b} is: {sol.get_sum(a, b)}")
     
-     
+    # endregion
+
+    #  i    0 1 2 3 4 5 6 7 8 9
+    nums = [5,6,7,8,9,0,1,2,3,4]
+    s = Solution()
+    print(s.search_rotated(nums, 8))
+    
+    
 if __name__ == "__main__":
     main()
         
