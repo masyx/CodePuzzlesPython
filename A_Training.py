f224c689f3dbfec041ef69483f15b0de6369f2a4
Sergey Matveev <rfmatveev@gmail.com>
2025-05-16 23:12:01 -0500

    ClimbStairs learning

--- A_Training.py
+++ A_Training.py
@@ -1,138 +1,23 @@
 from typing import Optional
 from collections import deque
 
-
-class ListNode:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-        
-class LinkedList:
-    def __init__(self, head):
-        self.head = ListNode(head)
-        
-    def insert_at_end(self, val):
-        curr_node = self.head
-        previous = None
-        while curr_node:
-            previous = curr_node
-            curr_node = curr_node.next
-        previous.next = ListNode(val)
-    
-    def __str__(self):
-        curr = self.head
-        res = []
-        while curr:
-            res.append(str(curr.val))
-            curr = curr.next
-        return ",".join(res)
-    
-    def reverse(self):
-        curr = self.head
-        previous = None
-        while curr:
-            next_to_process = curr.next
-            curr.next = previous
-            previous = curr
-            curr = next_to_process
-        self.head = previous
-                
 class Solution:
-    # O(n) time | O(n) space
-    def reorderListRecursion(self, head: Optional[ListNode]) -> None:
+    # Brute Force: O(2^n) time |O(log n) space
+    def climbStairs(self, n: int) -> int:
+        def cs_dfs(count):
+            if count == n:
+                return 1
+            if count > n:
+                return 0
+            return cs_dfs(count + 1) + cs_dfs(count +2)
+        return cs_dfs(0)
 
-        def rec(root: ListNode, cur: ListNode) -> ListNode:
-            if not cur:
-                return root
 
-            root = rec(root, cur.next)
-            if not root:
-                return None
-
-            tmp = None
-            if root == cur or root.next == cur:
-                cur.next = None
-            else:
-                tmp = root.next
-                root.next = cur
-                cur.next = tmp
-
-            return tmp
-
-        head = rec(head, head.next)
-        
-    # O(n) time | O(1) space
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        second = slow.next
-        prev = slow.next = None
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
-
-        first, second = head, prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
-        
-    def mergeTwoLists(self, list1, list2):
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        
-        if list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
-    
-    def mergeTwoListsIterative(self, l1, l2):
-        dummy = node = ListNode(-1)
-        while l1 and l2:
-            if l1.val < l2.val:
-                node.next = l1
-                l1 = l1.next
-            else:
-                node.next = l2
-                l2 = l2.next
-            node = node.next
-        node.next = l1 or l2
-        return dummy.next
-    
-    def linkedListCycle(head: ListNode):
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
-        
         
 def main():
-    ll1 = LinkedList(1)
-    ll1.insert_at_end(2)
-    # ll1.insert_at_end(3)
-    # ll1.insert_at_end(4)
-    # ll1.insert_at_end(5)
-    #print(ll1)
-    
     sol = Solution()
-    ll2 = LinkedList(3)
-    head = curr = sol.mergeTwoListsIterative(ll1.head, ll2.head)
-    while curr:
-        print(curr.val, "->", end=" ")
-        curr = curr.next
+    print(sol.climbStairs(4))
+
     
     
 if __name__ == "__main__":
---
+++ Neetcode/Backtracking/ClimbingStairs.py
@@ -0,0 +1,90 @@
+'''
+70. Climbing Stairs
+Easy
+
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+1 <= n <= 45
+'''
+
+class Solution:
+    # Brute Force: O(2^n) time |O(n) space
+    def climbStairs_BF(self, n: int) -> int:
+        
+        def cs_dfs(count):
+            if count == n:
+                return 1
+            if count > n:
+                return 0
+            return cs_dfs(count + 1) + cs_dfs(count +2)
+        
+        return cs_dfs(0)
+    
+    def climbStairs_BF_2(self, n: int) -> int:
+        
+        def dfs(i):
+            if i >= n:
+                return i == n
+            # In Python, True == 1 and False == 0, so boolean values can be added like integers
+            return dfs(i + 1) + dfs(i + 2)
+        
+        return dfs(0)
+
+    # O(n) time | O(n) space
+    def climbStairs_DP_topDown(self, n):
+        cache = [-1] * n
+        def dp(i):
+            if i > n:
+                return 0
+            if i == n:
+                return 1
+            if cache[i] != -1:
+                return cache[i]
+            l = dp(i + 1)
+            r = dp(i + 2)
+            cache[i] = l + r
+            return cache[i]
+        
+        return dp(0)
+    
+    def climbStairs(self, n):
+        one, two = 1, 1
+        
+        for i in range(n - 1):
+            third = one + two # 5
+            one = two # 3
+            two = third # 5
+        return one
+        
+        
+def main():
+    sol = Solution()
+    print(sol.climbStairs_BF(8))
+    print(sol.climbStairs_BF_2(8))
+    print(sol.climbStairs_DP_topDown(8))
+
+    
+    
+if __name__ == "__main__":
+    main()
+        
