5594c73153bafc79f611336290f3e219003acafe
Sergey Matveev <rfmatveev@gmail.com>
2025-07-28 02:07:22 -0500

    KokoEatingBananas training

--- A_Training.py
+++ A_Training.py
@@ -1,36 +1,30 @@
+import math
 from typing import List, Optional
 from collections import deque
 
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix:
-            return False
-        rows = len(matrix)
-        columns = len(matrix[0])
-        l = 0
-        r = (rows * columns) - 1
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        min_speed = 1
+        max_speed = max(piles)
         
-        while l <= r:
-            mid = (l + r) // 2
-            
-            row = mid // columns
-            column = mid % columns
-            curr_num = matrix[row][column]
+        while min_speed <= max_speed:
+            mid_speed = (min_speed + max_speed) // 2
+            hours_to_finish = 0
+            for pile in piles:
+                hours_to_finish += math.ceil(pile / mid_speed)
 
-            if curr_num == target:
-                return True
-            elif target < curr_num:
-                r = mid - 1
+            if hours_to_finish > h:
+                min_speed = mid_speed + 1
             else:
-                l = mid + 1
-        
-        return False
+                max_speed = mid_speed - 1
+        return min_speed
         
 # n = 3       
 def main():
-    matrix =[[1]]
+    piles = [3,6,7,11]
+    h = 8
     sol = Solution()
-    print(sol.searchMatrix(matrix, 2))
+    print(sol.minEatingSpeed(piles, h))
 
 
 
