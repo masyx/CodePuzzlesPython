aa7040380213a208b0b4dcdc075ec3a8daaf37dd
Sergey Matveev <rfmatveev@gmail.com>
2025-08-02 16:33:12 -0500

    MaxTreeDepth training

--- A_Training.py
+++ A_Training.py
@@ -1,35 +1,83 @@
-import math
-from typing import List, Optional
-from collections import deque
+import collections
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        min_speed = 1
-        max_speed = max(piles)
+    def max_depth_dfs_rec(self, root):
+        if root is None:
+            return 0
         
-        while min_speed <= max_speed:
-            mid_speed = (min_speed + max_speed) // 2
-            hours_to_finish = 0
-            for pile in piles:
-                hours_to_finish += math.ceil(pile / mid_speed)
-
-            if hours_to_finish > h:
-                min_speed = mid_speed + 1
-            else:
-                max_speed = mid_speed - 1
-        return min_speed
+        left = self.max_depth_dfs_rec(root.left)
+        right = self.max_depth_dfs_rec(root.right)
         
-# n = 3       
+        return 1 + max(left, right)
+    
+    def max_depth_dfs_iter(self, root):
+        if root is None:
+            return 0
+        stack = [(root, 1)]
+        max_depth = 0
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+            
+            max_depth = max(max_depth, depth)
+        
+        return max_depth 
+            
+    
+    def max_depth_bfs_iter(self, root):
+        q = collections.deque()
+        if root:
+            q.append(root)
+        level = 0
+        
+        while q:
+            level += 1
+            for i in range(len(q)):
+                curr = q.popleft()
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+        return level
+             
 def main():
-    piles = [3,6,7,11]
-    h = 8
+    # Visual representation of the tree:
+    #         1
+    #       /   \
+    #      2     3
+    #     / \   /
+    #    4   5 6
+    #     \ 
+    #      9
+    
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.left.left.right = TreeNode(9)
+    
     sol = Solution()
-    print(sol.minEatingSpeed(piles, h))
+
+    print(f"Max tree depth is: {sol.max_depth_dfs_rec(root)}")
+    print(f"Max tree depth is: {sol.max_depth_dfs_iter(root)}")
+    print(f"Max tree depth is: {sol.max_depth_bfs_iter(root)}")
 
 
 
-    
-    
 if __name__ == "__main__":
     main()
         
