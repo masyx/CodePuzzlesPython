1f3d20acce32b54d2c59317b04a66a37ee717b83
Sergey Matveev <rfmatveev@gmail.com>
2025-09-15 23:39:39 -0500

    TimeMap learning

--- Neetcode/BinarySearch/TimeMap.py
+++ Neetcode/BinarySearch/TimeMap.py
@@ -1,3 +1,29 @@
+""" 981. Time Based Key-Value Store
+Medium
+
+Implement a time-based key-value data structure that supports:
+
+Storing multiple values for the same key at specified time stamps
+Retrieving the key's value at a specified timestamp
+Implement the TimeMap class:
+
+TimeMap() Initializes the object.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns the most recent value of key if set was previously called on it and the most recent timestamp for that key prev_timestamp is less than or equal to the given timestamp (prev_timestamp <= timestamp). If there are no values, it returns "".
+Note: For all calls to set, the timestamps are in strictly increasing order.
+
+Constraints:
+
+1 <= key.length, value.length <= 100
+key and value consist of lowercase English letters and digits.
+1 <= timestamp <= 107
+All the timestamps timestamp of set are strictly increasing.
+At most 2 * 105 calls will be made to set and get.
+
+"""
+
+
+
 from collections import defaultdict
 
 class TimeMap:
@@ -52,11 +78,7 @@ class TimeMap:
         return result
         
 
-
-                    
-    
 def main():
-    
     # Create an instance of the TimeMap
     tm = TimeMap()
 
